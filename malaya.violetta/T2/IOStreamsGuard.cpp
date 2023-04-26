f80376d3d9b3ded4886d912ae132e579ad106f06
Violetta Malaya <malaya.violetta@gmail.com>
2023-04-26 22:31:31 +0300

    added stream guard definition

---
+++ malaya.violetta/T2/IOStreamsGuard.cpp
@@ -0,0 +1,14 @@
+#include<ios>
+#include"IOStreamsGuard.hpp"
+malaya::IOStreamsGuard::IOStreamsGuard(std::basic_ios< char > & stream):
+  stream_(stream),
+  fill_(stream.fill()),
+  precision_(stream.precision()),
+  fmt_(stream.flags())
+{};
+malaya::IOStreamsGuard::~IOStreamsGuard()
+{
+  stream_.fill(fill_);
+  stream_.precision(precision_);
+  stream_.flags(fmt_);
+}
--- malaya.violetta/T2/IOStructs.cpp
+++ malaya.violetta/T2/IOStructs.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 #include"IOStructs.hpp"
 std::istream & malaya::operator>>(std::istream & in, DelimiterIO && dest)
 {
