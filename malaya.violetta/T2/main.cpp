5217904acc933efbdb6cba574b78ebf4810fa0c8
Violetta Malaya <malaya.violetta@gmail.com>
2023-04-27 10:32:10 +0300

    added input and output in main

--- malaya.violetta/T2/main.cpp
+++ malaya.violetta/T2/main.cpp
@@ -1,11 +1,31 @@
 #include<vector>
 #include<iostream>
+#include <iterator>
 #include"DataStruct.hpp"
 int main()
 {
   std::vector< malaya::DataStruct > data;
-  //добавить цикл со вводом
+  //std::copy(
+  //  std::istream_iterator< malaya::DataStruct >(std::cin),
+  //  std::istream_iterator< malaya::DataStruct >(),
+  //  std::back_inserter(data)
+  //);
+  while(std::cin) //возможно другое условие
+  {
+    std::copy(
+      std::istream_iterator< malaya::DataStruct >(std::cin),
+      std::istream_iterator< malaya::DataStruct >(),
+      std::back_inserter(data)
+    );
+  }
   //добавить сортировку
-  //добавить цикл с выводом
+  //написать компаратор
+  std::cout << "Data:\n";
+  std::copy(
+    std::begin(data),
+    std::end(data), // проверить
+    std::ostream_iterator< malaya::DataStruct >(std::cout, "\n")
+  );
+
   return 0;
 }
