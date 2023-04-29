c8cb20c4075ccfaa3579c8d23daad6dee4f73b6a
anton <antkumachev@gmail.com>
2023-04-29 22:10:33 +0300

    Ignore bad lines

--- kumachev.anton/T2/main.cpp
+++ kumachev.anton/T2/main.cpp
@@ -1,18 +1,28 @@
 #include <vector>
 #include <iostream>
-#include <iterator>
+#include <limits>
 #include <fstream>
 #include "data_struct.h"
 
 int main()
 {
   std::vector< T2::DataStruct > data;
+  auto backInserter = std::back_inserter(data);
   std::ifstream in("input.txt");
 
-  std::copy(std::istream_iterator< T2::DataStruct >(in),
-            std::istream_iterator< T2::DataStruct >{},
-            std::back_inserter(data)
-  );
+  while (!in.eof()) {
+    T2::DataStruct dataStruct;
+    in >> dataStruct;
+
+    if (!in.fail()) {
+      *backInserter = dataStruct;
+      ++backInserter;
+    }
+    else {
+      in.clear();
+      in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
 
   std::cout << data.size() << '\n';
   return 0;
