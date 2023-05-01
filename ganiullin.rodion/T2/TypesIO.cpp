5b2c0cdb8d468deca969ed799471a713a61db776
Coding_Seal <rodion2023@mail.ru>
2023-05-01 13:44:21 +0300

    Fixed format

---
+++ ganiullin.rodion/T2/TypesIO.cpp
@@ -0,0 +1,85 @@
+#include "TypesIO.h"
+#include <iomanip>
+
+namespace ganiullin {
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp)) {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, DoubleI&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    return in >> std::scientific >> dest.ref;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DoubleO&& dest) {
+    std::ostream::sentry sentry(out);
+    if (!sentry) {
+      return out;
+    }
+
+    double value = dest.val;
+    int exponent = 0;
+
+    if (value == 0) {
+      out << std::fixed << std::setprecision(1) << value << "e+" << exponent;
+    } else if (abs(value) < 1) {
+      while (abs(value) * 10 < 10) {
+        value *= 10;
+        exponent--;
+      }
+    } else {
+      while (abs(value) / 10 < 1) {
+        value /= 10;
+        exponent++;
+      }
+    }
+
+    if (value != 0) {
+      out << std::fixed << std::setprecision(1) << value << 'e' << exponent;
+    }
+    return out;
+  }
+
+  std::istream& operator>>(std::istream& in, StringIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+  }
+
+  std::istream& operator>>(std::istream& in, LabelIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    std::string data = "";
+    in >> data;
+    in.putback(data.back());
+    data.pop_back();
+    if (in && (data != dest.exp)) {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, ULongLongIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    return in >> DelimiterIO{'0'} >> DelimiterIO{'x'} >> std::hex >> dest.ref;
+  }
+}  // namespace ganiullin
--- ganiullin.rodion/T2/TypesIO.h
+++ ganiullin.rodion/T2/TypesIO.h
@@ -2,7 +2,6 @@
 #define TYPES_IO_H
 
 #include <string>
-#include "FormatGuard.h"
 
 namespace ganiullin {
   struct DelimiterIO {
@@ -28,83 +27,13 @@ namespace ganiullin {
     unsigned long long& ref;
   };
 
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    char c = '0';
-    in >> c;
-    if (in && (c != dest.exp)) {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, DoubleI&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, ULongLongIO&& dest);
 
-  std::istream& operator>>(std::istream& in, DoubleI&& dest) {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    return in >> std::scientific >> dest.ref;
-  }
+  std::ostream& operator<<(std::ostream& out, const DoubleO&& dest);
 
-  std::ostream& operator<<(std::ostream& out, const DoubleO&& dest) {
-    std::ostream::sentry sentry(out);
-    if (!sentry) {
-      return out;
-    }
-    ganiullin::iofmtguard fmtguard(out);
-
-    double value = dest.val;
-    int exponent = 0;
-    if (value == 0) {
-      out << std::setprecision(1) << value << 'e' << exponent;
-    } else if (abs(value) < 1) {
-      while (abs(value) * 10 < 10) {
-        value *= 10;
-        exponent--;
-      }
-    } else {
-      while (abs(value) / 10 < 1) {
-        value /= 10;
-        exponent++;
-      }
-    }
-    out << std::setprecision(1) << value << 'e' << exponent;
-
-    return out;
-  }
-
-  std::istream& operator>>(std::istream& in, StringIO&& dest) {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
-  }
-
-  std::istream& operator>>(std::istream& in, LabelIO&& dest) {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    std::string data = "";
-    in >> data;
-    in.putback(data.back());
-    data.pop_back();
-    if (in && (data != dest.exp)) {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
-
-  std::istream& operator>>(std::istream& in, ULongLongIO&& dest) {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    return in >> DelimiterIO{'0'} >> DelimiterIO{'x'} >> std::hex >> dest.ref;
-  }
 }  // namespace ganiullin
 #endif
