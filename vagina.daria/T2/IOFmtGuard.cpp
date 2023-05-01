64242f9fc072426f479c88e51c6ab914db8d4a2f
Vagina Daria <dvagina@inbox.ru>
2023-05-01 23:37:59 +0300

    Create IOFmtGuard files

---
+++ vagina.daria/T2/IOFmtGuard.cpp
@@ -0,0 +1,13 @@
+#include "IOFmtGuard.h"
+
+iofmtguard::iofmtguard(std::basic_ios< char >& s) :
+        s_(s),
+        fill_(s.fill()),
+        fmt_(s.flags())
+{}
+
+iofmtguard::~iofmtguard()
+{
+  s_.fill(fill_);
+  s_.flags(fmt_);
+}
