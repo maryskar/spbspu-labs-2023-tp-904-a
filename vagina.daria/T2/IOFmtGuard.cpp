3c3db1a7762dad384cac0f9bb5979a2684027557
Vagina Daria <dvagina@inbox.ru>
2023-05-02 21:43:16 +0300

    cg

--- vagina.daria/T2/IOFmtGuard.cpp
+++ vagina.daria/T2/IOFmtGuard.cpp
@@ -1,9 +1,9 @@
 #include "IOFmtGuard.h"
 
 vagina::iofmtguard::iofmtguard(std::basic_ios< char >& s) :
-        s_(s),
-        fill_(s.fill()),
-        fmt_(s.flags())
+  s_(s),
+  fill_(s.fill()),
+  fmt_(s.flags())
 {}
 
 vagina::iofmtguard::~iofmtguard()
