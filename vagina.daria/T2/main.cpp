98b4e66198cd247a7700ca5922abd3c67c4f63a1
Vagina Daria <dvagina@inbox.ru>
2023-05-02 20:24:23 +0300

    cg

--- vagina.daria/T2/main.cpp
+++ vagina.daria/T2/main.cpp
@@ -20,21 +20,21 @@
 
 int main()
 {
-  std::vector<DataStruct> data;
+  std::vector< DataStruct > data;
   while (!std::cin.eof())
   {
     std::cin.clear();
     std::copy(
-            std::istream_iterator< DataStruct >(std::cin),
-            std::istream_iterator< DataStruct >(),
-            std::back_inserter(data)
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data)
     );
   }
   std::sort(data.begin(), data.end(), comp);
   std::cout << "Data:\n";
   std::copy(std::begin(data),
-            std::end(data),
-            std::ostream_iterator< DataStruct >(std::cout, "\n")
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
   );
   return 0;
 }
