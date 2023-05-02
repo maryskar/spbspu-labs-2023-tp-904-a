dd58e9c14492a72d05497e0ca46dbe373a07bd77
Vagina Daria <dvagina@inbox.ru>
2023-05-02 23:26:11 +0300

    again main fixed

--- vagina.daria/T2/main.cpp
+++ vagina.daria/T2/main.cpp
@@ -17,7 +17,6 @@ int main()
     );
   }
   std::sort(data.begin(), data.end(), vagina::comp);
-  std::cout << "Data:\n";
   std::copy(std::begin(data),
     std::end(data),
     std::ostream_iterator< DataStruct >(std::cout, "\n")
