b066359a85175c8dd150b3b1b7087f2857688412
Coding_Seal <rodion2023@mail.ru>
2023-05-04 19:18:13 +0300

    Fixed iter

--- ganiullin.rodion/T2/main.cpp
+++ ganiullin.rodion/T2/main.cpp
@@ -5,27 +5,24 @@
 
 #include "DataStruct.h"
 
-constexpr size_t CHARS_TO_IGNORE = std::numeric_limits< std::streamsize >::max();
-
 int main()
 {
-  auto inIterBegin = std::istream_iterator< ganiullin::DataStruct >(std::cin);
-  auto inIterEnd = std::istream_iterator< ganiullin::DataStruct >();
+  using inIterBegin = std::istream_iterator< ganiullin::DataStruct >;
+  using inIterEnd = std::istream_iterator< ganiullin::DataStruct >;
   auto outIter = std::ostream_iterator< ganiullin::DataStruct >(std::cout, "\n");
   std::vector< ganiullin::DataStruct > data;
   ganiullin::Comparator comparator;
 
   while (!std::cin.eof()) {
-    std::copy(inIterBegin, inIterEnd, std::back_inserter(data));
+    std::copy(inIterBegin(std::cin), inIterEnd(), std::back_inserter(data));
 
     if (std::cin.fail()) {
       std::cin.clear();
-      std::cin.ignore(CHARS_TO_IGNORE, '\n');
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
 
   std::sort(data.begin(), data.end(), comparator);
-
   std::copy(std::begin(data), std::end(data), outIter);
 
   return 0;
