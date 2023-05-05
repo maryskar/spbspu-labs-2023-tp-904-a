82ad9117584c16b8a381c9b376d9c7667e469b40
Violetta Malaya <malaya.violetta@gmail.com>
2023-05-05 15:35:32 +0300

    fixed incorrect input checking

--- malaya.violetta/T2/main.cpp
+++ malaya.violetta/T2/main.cpp
@@ -3,13 +3,22 @@
 #include <iterator>
 #include <algorithm>
 #include <fstream>
+#include <limits>
 #include "DataStruct.hpp"
 int main()
 {
   //std::ifstream in("input.txt");
   std::vector< malaya::DataStruct > data;
-  std::copy(std::istream_iterator< malaya::DataStruct >(std::cin), std::istream_iterator< malaya::DataStruct >(),
-            std::back_inserter(data));
+  while(!std::cin.eof())
+  {
+    std::copy(std::istream_iterator< malaya::DataStruct >(std::cin), std::istream_iterator< malaya::DataStruct >(),
+              std::back_inserter(data));
+    if(!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
   std::sort(data.begin(), data.end(), malaya::DataComparator{});
   std::copy(std::begin(data), std::end(data),
             std::ostream_iterator< malaya::DataStruct >(std::cout, "\n"));
