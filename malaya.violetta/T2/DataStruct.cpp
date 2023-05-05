900a99bb0ea7175548431af761836c31e66f55c5
Violetta Malaya <malaya.violetta@gmail.com>
2023-05-05 22:57:05 +0300

    fixed output again

--- malaya.violetta/T2/DataStruct.cpp
+++ malaya.violetta/T2/DataStruct.cpp
@@ -165,6 +165,6 @@ std::ostream & malaya::operator<<(std::ostream & out, const DataStruct & data)
   IOStreamsGuard guard(out);
   out << "(" << ":key1 " << std::fixed << std::setprecision(1) << data.key1 << "d";
   out << ":key2 0" << std::oct << data.key2;
-  out << ":key3 \"" << data.key3 << "\")";
+  out << ":key3 \"" << data.key3 << "\":)";
   return out;
 }
