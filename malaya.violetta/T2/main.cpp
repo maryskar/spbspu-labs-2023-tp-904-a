85edb603827fa01ac11dbb13aa3f564531852fd9
Violetta Malaya <malaya.violetta@gmail.com>
2023-05-05 15:01:28 +0300

    fixed istream

--- malaya.violetta/T2/main.cpp
+++ malaya.violetta/T2/main.cpp
@@ -6,9 +6,9 @@
 #include "DataStruct.hpp"
 int main()
 {
-  std::ifstream in("input.txt");
+  //std::ifstream in("input.txt");
   std::vector< malaya::DataStruct > data;
-  std::copy(std::istream_iterator< malaya::DataStruct >(in), std::istream_iterator< malaya::DataStruct >(),
+  std::copy(std::istream_iterator< malaya::DataStruct >(std::cin), std::istream_iterator< malaya::DataStruct >(),
             std::back_inserter(data));
   std::sort(data.begin(), data.end(), malaya::DataComparator{});
   std::copy(std::begin(data), std::end(data),
