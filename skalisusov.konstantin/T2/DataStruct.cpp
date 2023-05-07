160040e8296141a729292c754c8b60816ecdba9a
Kikos1337 <skalisusov@inbox.ru>
2023-05-08 01:45:22 +0300

    body addition Double Literal format

--- skalisusov.konstantin/T2/DataStruct.cpp
+++ skalisusov.konstantin/T2/DataStruct.cpp
@@ -2,5 +2,10 @@
 
 std::istream &skalisusov::operator>>(std::istream &in, DoubleLiteralFormatIO &&dest)
 {
-
+  std::istream::sentry checkSentry(in);
+  if(!checkSentry)
+  {
+    return in;
+  }
+  return in >> dest.real_ >> CoverIO{"Dd"};
 }
