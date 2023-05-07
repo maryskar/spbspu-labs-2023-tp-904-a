4ad5138be7acff20ac723e5578b1430273ef2352
Kikos1337 <skalisusov@inbox.ru>
2023-05-07 22:58:38 +0300

    Create struct Data and class iofmtguard

--- skalisusov.konstantin/T2/DataStruct.h
+++ skalisusov.konstantin/T2/DataStruct.h
@@ -7,6 +7,22 @@
 
 namespace skalisusov
 {
-
+  struct Data: public std::__1::error_code
+  {
+    double key1;
+    double key2;
+    std::string key3;
+  };
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios< char > &s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
 }
 #endif //SPBSPU_LABS_2023_TP_904_A_DATASTRUCT_H
