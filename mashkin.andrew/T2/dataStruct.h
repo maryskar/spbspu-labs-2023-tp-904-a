a6527e1e9a4329f848efb95481b5acd055c72623
Машкин Андрей <beedzibood@gmail.com>
2023-05-07 15:53:59 +0300

    declared DataStruct and oper >> with DataStruct

---
+++ mashkin.andrew/T2/dataStruct.h
@@ -0,0 +1,17 @@
+#ifndef T2_DATASTRUCT_H
+#define T2_DATASTRUCT_H
+#include <iostream>
+#include <string>
+
+namespace mashkin
+{
+  struct DataStruct
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  std::istream& operator>>(std::istream& inp, DataStruct& data);
+}
+#endif
