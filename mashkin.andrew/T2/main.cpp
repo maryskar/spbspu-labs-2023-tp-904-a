503b467ac61efc46dca9972d6a52cf495fd16e67
Машкин Андрей <beedzibood@gmail.com>
2023-05-08 23:01:13 +0300

    added vector, iterator and get input from cin

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "dataStruct.h"
+
 int main()
 {
+  std::vector< mashkin::DataStruct > res;
+  std::vector< mashkin::DataStruct >::iterator iter;
+  iter = res.begin();
+  while (std::cin)
+  {
+    mashkin::DataStruct data;
+    std::cin >> data;
+    res.insert(iter, data);
+    ++iter;
+  }
   return 0;
 }
