53576ceb9aeb826019477c75381da609c816d05b
Машкин Андрей <beedzibood@gmail.com>
2023-05-09 20:44:48 +0300

    fixed output

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -18,9 +18,10 @@ int main()
       std::cin.clear();
       std::getline(std::cin, line);
     }
-    std::cout << data.key1 << " " << data.key2 << " " << data.key3 << "\n";
-    //res.insert(iter, data);
-    //++iter;
+    else
+    {
+      std::cout << data.key1 << " " << data.key2 << " " << data.key3 << "\n";
+    }
   }
   return 0;
 }
