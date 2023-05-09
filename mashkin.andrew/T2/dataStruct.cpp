187467c6c23119e99a902b0ca28f105ecfa6f7f3
Машкин Андрей <beedzibood@gmail.com>
2023-05-09 10:12:39 +0300

    added checking to open DataStruct (I mean "(:keyN")

--- mashkin.andrew/T2/dataStruct.cpp
+++ mashkin.andrew/T2/dataStruct.cpp
@@ -1,1 +1,23 @@
 #include "dataStruct.h"
+#include <iostream>
+#include <string>
+#include <ios>
+
+namespace mashkin
+{
+  std::istream& operator>>(std::istream& inp, DataStruct& data)
+  {
+    std::string var;
+    inp >> var;
+    if (var[0] == '(' && var[1] == ':')
+    {
+      var.erase(std::begin(var), std::begin(var) + 2);
+      std::cout << var;
+    }
+    else
+    {
+      inp.setstate(std::ios::failbit);
+      return inp;
+    }
+  }
+}
