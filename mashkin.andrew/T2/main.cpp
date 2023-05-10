8e84036229105b9bde4e3f0b2d6a81716ecf5fae
Машкин Андрей <beedzibood@gmail.com>
2023-05-10 23:24:40 +0300

    removed unnecessary lines

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -13,19 +13,12 @@ int main()
   {
     std::copy(std::istream_iterator< mashkin::DataStruct >(std::cin), std::istream_iterator< mashkin::DataStruct >(),
         std::back_inserter(res));
-    mashkin::DataStruct data;
-    std::cin >> data;
-    if (data.key3 == "error")
-    {
-      break;
-    }
     if (std::cin.fail())
     {
       std::string line;
       std::cin.clear();
       std::getline(std::cin, line);
     }
-    res.push_back(data);
   }
   std::sort(std::begin(res), std::end(res), comp);
   std::copy(std::begin(res), std::end(res), std::ostream_iterator< mashkin::DataStruct >(std::cout, "\n"));
