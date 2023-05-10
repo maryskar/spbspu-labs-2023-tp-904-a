96c89211b38470dcd551c541028a601fb18ddb52
Машкин Андрей <beedzibood@gmail.com>
2023-05-10 22:48:47 +0300

    deleted comments

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -11,8 +11,8 @@ int main()
   mashkin::Comparator comp;
   while (!std::cin.eof())
   {
-    /*std::copy(std::istream_iterator< mashkin::DataStruct >(std::cin), std::istream_iterator< mashkin::DataStruct >(),
-        std::back_inserter(res));*/
+    std::copy(std::istream_iterator< mashkin::DataStruct >(std::cin), std::istream_iterator< mashkin::DataStruct >(),
+        std::back_inserter(res));
     mashkin::DataStruct data;
     std::cin >> data;
     if (data.key3 == "error")
@@ -28,11 +28,6 @@ int main()
     res.push_back(data);
   }
   std::sort(std::begin(res), std::end(res), comp);
-  //::copy(std::begin(res), std::end(res), std::ostream_iterator< mashkin::DataStruct >(std::cout, "\n"));
-  // std::cout << data;
-  for (auto iter = res.begin(); iter < res.end(); iter++)
-  {
-    std::cout << *iter << "\n";
-  }
+  std::copy(std::begin(res), std::end(res), std::ostream_iterator< mashkin::DataStruct >(std::cout, "\n"));
   return 0;
 }
