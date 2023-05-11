8df5e6f75e638e7837c62a14b57ec0a5cd830939
Chemodurovart <chemodurovart@gmail.com>
2023-05-11 22:53:49 +0300

    added operators >> for IO structs

--- chemodurov.artem/T2/IO-structs.cpp
+++ chemodurov.artem/T2/IO-structs.cpp
@@ -1,5 +1,67 @@
-//
-// Created by Лоликонщик on 11.05.2023.
-//
-
 #include "IO-structs.hpp"
+#include <iostream>
+
+namespace chemodurov
+{
+  bool checkSentry(std::istream & in)
+  {
+    std::istream::sentry sentry(in);
+    return static_cast<bool>(sentry);
+  }
+
+  std::istream & operator>>(std::istream & in, DelimiterIO && dest)
+  {
+    if (!checkSentry(in))
+    {
+      return in;
+    }
+    char c = '\0';
+    in >> c;
+    if (in && c != std::tolower(dest.exp) && c != std::toupper(dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream & operator>>(std::istream & in, DoubleIO && dest)
+  {
+    if (!checkSentry(in))
+    {
+      return in;
+    }
+    return in >> std::scientific >> dest.ref;
+  }
+
+  std::istream & operator>>(std::istream & in, LongLongIO && dest)
+  {
+    if (!checkSentry(in))
+    {
+      return in;
+    }
+    return in >> dest.ref >> DelimiterIO{'l'} >> DelimiterIO{'l'};
+  }
+
+  std::istream & operator>>(std::istream & in, StringIO && dest)
+  {
+    if (!checkSentry(in))
+    {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+  }
+
+  std::istream & operator>>(std::istream & in, LabelIO && dest)
+  {
+    if (!checkSentry(in))
+    {
+      return in;
+    }
+    std::string data;
+    if ((in >> StringIO{data} && data != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+}
