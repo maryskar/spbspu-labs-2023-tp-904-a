805c99c44da3115fe4c0e1688187b6f930b1561e
Artem <artrom170@gmail.com>
2023-05-12 22:45:33 +0300

    FUCK! SHIT! istream_iterator: dereferencing only returns a copy of the most recently read object...

--- romanovich.artem/T2/main.cpp
+++ romanovich.artem/T2/main.cpp
@@ -7,14 +7,17 @@
 int main()
 {
   std::vector< romanovich::DataStruct > dsVector;
-  auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
+  using begin = std::istream_iterator< romanovich::DataStruct >;
   auto end = std::istream_iterator< romanovich::DataStruct >();
   auto target = std::back_inserter(dsVector);
   while (!std::cin.eof())
   {
-    std::copy(begin, end, target);
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    std::copy(begin(std::cin), end, target);
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
   }
   romanovich::Comparator Comp;
   std::sort(dsVector.begin(), dsVector.end(), Comp);
