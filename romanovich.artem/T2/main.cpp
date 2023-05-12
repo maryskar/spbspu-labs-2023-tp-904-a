30e3c64223d9c178dd6f7e1a5b11e3003aca1c25
Artem <artrom170@gmail.com>
2023-05-12 14:26:12 +0300

    Revert "checkout"
    This reverts commit 2c862724ccc494eafc99d637ee7e5fc50f7a5f42.

--- romanovich.artem/T2/main.cpp
+++ romanovich.artem/T2/main.cpp
@@ -2,25 +2,42 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <limits>
 #include "datastruct.h"
 int main()
 {
   std::vector< romanovich::DataStruct > ds;
-  auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
+  /*auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
   auto end = std::istream_iterator< romanovich::DataStruct >();
   auto target = std::back_inserter(ds);
   while (!std::cin.eof())
   {
     std::copy(begin, end, target);
-    if (!std::cin)
+    if (std::cin.fail())
     {
       std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::string dummy;
+      std::getline(std::cin, dummy);
+    }
+  }*/
+  std::back_insert_iterator< decltype(ds) > backInserter = std::back_inserter(ds);
+  while (!std::cin.eof())
+  {
+    romanovich::DataStruct dataStruct;
+    std::cin >> dataStruct;
+    if (!std::cin.fail())
+    {
+      *backInserter = dataStruct;
+      ++backInserter;
+    }
+    else
+    {
+      std::cin.clear();
+      std::string dummy;
+      std::getline(std::cin, dummy);
     }
   }
   romanovich::Comparator Comp;
   std::sort(ds.begin(), ds.end(), Comp);
-  std::copy(ds.begin(), ds.end(), std::ostream_iterator< romanovich::DataStruct >(std::cout, "\n"));
+  std::copy(ds.begin(), ds.end(), std::ostream_iterator< romanovich::DataStruct >(std::cout));
   return 0;
 }
