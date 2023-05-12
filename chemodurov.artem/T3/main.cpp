5389b5ec247f93d5367e5ff164b5a15cf4e5183b
Chemodurovart <chemodurovart@gmail.com>
2023-05-12 22:31:28 +0300

    added some logic for reading in main

--- chemodurov.artem/T3/main.cpp
+++ chemodurov.artem/T3/main.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
 {
+  if (argc != 2)
+  {
+    std::cerr << "Not correct number of CML args\n";
+    return 1;
+  }
+  std::ifstream ifstream(argv[1]);
+  if (!ifstream.is_open())
+  {
+    std::cerr << "The file doesn't open\n";
+    return 1;
+  }
+
   return 0;
 }
