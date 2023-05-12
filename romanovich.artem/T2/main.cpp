b87ef379554b209a5400fea214865d64ffa4bc5d
Artem <artrom170@gmail.com>
2023-05-12 11:38:51 +0300

    step back 2

--- romanovich.artem/T2/main.cpp
+++ romanovich.artem/T2/main.cpp
@@ -6,10 +6,19 @@
 int main()
 {
   std::vector< romanovich::DataStruct > ds;
-  auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
+  /*auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
   auto end = std::istream_iterator< romanovich::DataStruct >();
   auto target = std::back_inserter(ds);
-  //std::copy(begin, end, target);
+  while (!std::cin.eof())
+  {
+    std::copy(begin, end, target);
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::string dummy;
+      std::getline(std::cin, dummy);
+    }
+  }*/
   std::back_insert_iterator< decltype(ds) > backInserter = std::back_inserter(ds);
   while (!std::cin.eof())
   {
