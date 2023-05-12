978d7cee7870afc7e9ca246bbe05d78a3e362d51
Chemodurovart <chemodurovart@gmail.com>
2023-05-12 08:48:37 +0300

    added reading in main

--- chemodurov.artem/T2/main.cpp
+++ chemodurov.artem/T2/main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include "data-struct.hpp"
+
 int main()
 {
+  std::vector< chemodurov::DataStrct > data;
+  std::copy(
+    std::istream_iterator< chemodurov::DataStrct >(std::cin),
+    std::istream_iterator< chemodurov::DataStrct >(),
+    std::back_inserter(data)
+  );
+  std::copy(data.begin(), data.end(), std::ostream_iterator< chemodurov::DataStrct >(std::cout, "\n"));
   return 0;
 }
