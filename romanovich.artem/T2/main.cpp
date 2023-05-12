29c5773e8329dee4ee3937ead2d53a6e7dcf3e58
Artem <artrom170@gmail.com>
2023-05-12 17:38:18 +0300

    loop

--- romanovich.artem/T2/main.cpp
+++ romanovich.artem/T2/main.cpp
@@ -2,24 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <limits>
 #include "datastruct.h"
-bool isValid(const romanovich::DataStruct &)
-{
-  if (std::cin.fail())
-  {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    return false;
-  }
-  return true;
-}
 int main()
 {
   std::vector< romanovich::DataStruct > dsVector;
   auto begin = std::istream_iterator< romanovich::DataStruct >(std::cin);
   auto end = std::istream_iterator< romanovich::DataStruct >();
   auto target = std::back_inserter(dsVector);
-  std::copy_if(begin, end, target, isValid);
+  while (!std::cin.eof())
+  {
+    std::copy(begin, end, target);
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
   romanovich::Comparator Comp;
   std::sort(dsVector.begin(), dsVector.end(), Comp);
   std::copy(dsVector.begin(), dsVector.end(), std::ostream_iterator< romanovich::DataStruct >(std::cout));
