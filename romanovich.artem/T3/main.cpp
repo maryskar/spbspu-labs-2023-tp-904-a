eb2735fe933fca5931d8f5e72f31f3f4daae7d2c
Artem <artrom170@gmail.com>
2023-05-14 12:22:42 +0300

    input from file

---
+++ romanovich.artem/T3/main.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "polygon.h"
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "No file provided.\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "Cannot open file.\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons;
+  size_t pointsCount;
+  while (input >> pointsCount)
+  {
+    if (input.fail())
+    {
+      input.clear();
+      input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      continue;
+    }
+    std::vector< Point > points;
+    for (size_t i = 0; i < pointsCount; i++)
+    {
+      Point point{};
+      input >> point;
+      //std::cout << point.x << " " << point.y << "\n";
+      if (input.fail())
+      {
+        input.clear();
+        input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        break;
+      }
+      points.push_back(point);
+    }
+    if (points.size() == pointsCount)
+    {
+      polygons.emplace_back(points);
+    }
+  }
+  for (const auto &polygon: polygons)
+  {
+    for (size_t i = 0; i < polygon.pointsCount(); ++i)
+    {
+      std::cout << polygon.getPoint(i).x << " " << polygon.getPoint(i).y << "\n";
+    }
+    std::cout << "\n";
+  }
+  return 0;
+}
