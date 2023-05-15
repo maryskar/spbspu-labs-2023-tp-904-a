01d120f8064c37f76bdd49111ac212cc5128564b
Машкин Андрей <beedzibood@gmail.com>
2023-05-15 15:32:50 +0300

    removed comments

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -9,21 +9,17 @@
 int main()
 {
   std::vector< mashkin::DataStruct > res;
-  //mashkin::DataStruct data;
   mashkin::Comparator comp;
   using inpIter = std::istream_iterator< mashkin::DataStruct >;
   using outIter = std::ostream_iterator< mashkin::DataStruct >;
   constexpr auto maxSize = std::numeric_limits< std::streamsize >::max();
   while (!std::cin.eof())
   {
-    //std::cin >> data;
     std::copy(inpIter(std::cin), inpIter(), std::back_inserter(res));
     if (std::cin.fail())
     {
-      // std::string line;
       std::cin.clear();
       std::cin.ignore(maxSize, '\n');
-      // std::getline(std::cin, line);
     }
   }
   std::sort(std::begin(res), std::end(res), comp);
