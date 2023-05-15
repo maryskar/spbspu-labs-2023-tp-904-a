bf24171d17d2f9a88a6d36cf5de2e980aafcae71
Машкин Андрей <beedzibood@gmail.com>
2023-05-15 15:28:09 +0300

    improved std::cin.ignore

--- mashkin.andrew/T2/main.cpp
+++ mashkin.andrew/T2/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <string>
 #include <vector>
 #include "dataStruct.h"
@@ -12,16 +13,17 @@ int main()
   mashkin::Comparator comp;
   using inpIter = std::istream_iterator< mashkin::DataStruct >;
   using outIter = std::ostream_iterator< mashkin::DataStruct >;
+  constexpr auto maxSize = std::numeric_limits< std::streamsize >::max();
   while (!std::cin.eof())
   {
     //std::cin >> data;
     std::copy(inpIter(std::cin), inpIter(), std::back_inserter(res));
     if (std::cin.fail())
     {
-      //std::string line;
+      // std::string line;
       std::cin.clear();
-      std::cin.ignore();
-      //std::getline(std::cin, line);
+      std::cin.ignore(maxSize, '\n');
+      // std::getline(std::cin, line);
     }
   }
   std::sort(std::begin(res), std::end(res), comp);
