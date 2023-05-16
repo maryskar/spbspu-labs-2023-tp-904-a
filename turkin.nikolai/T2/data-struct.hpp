d94ab7d03c720ece84d4f5781a8515b786ed9c70
Lagushka1984 <nikolai.turkin.main@gmail.com>
2023-05-16 23:05:34 +0300

    deleted comments

--- turkin.nikolai/T2/data-struct.hpp
+++ turkin.nikolai/T2/data-struct.hpp
@@ -1,24 +1,21 @@
 #ifndef DATA_STRUCT_HPP
 #define DATA_STRUCT_HPP
 
+#include <istream>
 #include <string>
 
 namespace turkin
 {
   struct DataStruct
   {
-    double key1;
-    std::string key2;
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
   };
 
   struct DelimiterIO
   {
     char exp;
-  };
-
-  struct DoubleIO
-  {
-    double & ref;
   };
 
   struct StringIO
@@ -30,6 +27,9 @@ namespace turkin
   {
     std::string exp;
   };
+
+  struct ULL10
+  {};
 
   class iofmtguard
   {
@@ -44,9 +44,9 @@ namespace turkin
   };
 
   std::istream & operator>>(std::istream & in, DelimiterIO && dest);
-  std::istream & operator>>(std::istream & in, DoubleIO && dest);
   std::istream & operator>>(std::istream & in, StringIO && dest);
   std::istream & operator>>(std::istream & in, LabelIO && dest);
+  std::istream & operator>>(std::istream & in, ULL10 && dest);
   std::istream & operator>>(std::istream & in, DataStruct & dest);
   std::ostream & operator<<(std::ostream & out, const DataStruct & dest);
 }
