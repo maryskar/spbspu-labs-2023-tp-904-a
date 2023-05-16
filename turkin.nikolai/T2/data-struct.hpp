ee6be5917a6774944001087d6af2ff44253f88c9
Lagushka1984 <nikolai.turkin.main@gmail.com>
2023-05-17 00:18:51 +0300

    also moved

--- turkin.nikolai/T2/data-struct.hpp
+++ turkin.nikolai/T2/data-struct.hpp
@@ -12,43 +12,6 @@ namespace turkin
     unsigned long long key2;
     std::string key3;
   };
-
-  struct DelimiterIO
-  {
-    char exp;
-  };
-
-  struct StringIO
-  {
-    std::string & ref;
-  };
-
-  struct LabelIO
-  {
-    std::string exp;
-  };
-
-  struct ULL10
-  {};
-
-  class iofmtguard
-  {
-  public:
-    iofmtguard(std::basic_ios< char > & s);
-    ~iofmtguard();
-  private:
-    std::basic_ios< char > & s_;
-    char fill_;
-    std::streamsize precision_;
-    std::basic_ios< char >::fmtflags fmt_;
-  };
-
-  std::istream & operator>>(std::istream & in, DelimiterIO && dest);
-  std::istream & operator>>(std::istream & in, StringIO && dest);
-  std::istream & operator>>(std::istream & in, LabelIO && dest);
-  std::istream & operator>>(std::istream & in, ULL10 && dest);
-  std::istream & operator>>(std::istream & in, DataStruct & dest);
-  std::ostream & operator<<(std::ostream & out, const DataStruct & dest);
 }
 
 #endif
