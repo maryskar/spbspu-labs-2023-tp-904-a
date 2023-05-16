c8168174bd46497b56665cdf5f6b70d33cbc90f0
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-05-17 00:13:14 +0300

    add explicit to class constructor

--- shelkovnikov.dmitrii/T2/io.h
+++ shelkovnikov.dmitrii/T2/io.h
@@ -27,7 +27,7 @@ namespace dimkashelk
   class iofmtguard
   {
   public:
-    iofmtguard(std::basic_ios< char > &s);
+    explicit iofmtguard(std::basic_ios< char > &s);
     ~iofmtguard();
   private:
     std::basic_ios< char > &s_;
