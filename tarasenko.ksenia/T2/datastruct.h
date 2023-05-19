fd344813c50ec898dccb4a792744724a72822087
xeniaTa <kseniatarasenko1@gmail.com>
2023-05-19 15:41:48 +0300

    start datastruct

---
+++ tarasenko.ksenia/T2/datastruct.h
@@ -0,0 +1,13 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+#include <string>
+namespace tarasenko
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+#endif
