669b37008c8506771732a79878d18b6505b10715
Kikos1337 <skalisusov@inbox.ru>
2023-05-20 16:10:22 +0300

    adding struct types

--- skalisusov.konstantin/T2/StructTypes.h
+++ skalisusov.konstantin/T2/StructTypes.h
@@ -1,12 +1,38 @@
 #ifndef SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
 #define SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
 
-
-
 #include <sstream>
 #include <string>
 #include <cassert>
 #include <iomanip>
 
+namespace skalisusov
+{
+  struct CoverIO
+  {
+    std::string cover_;
+  };
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct DoubleLiteralFormatIO
+  {
+    double &dublit_;
+  };
+  struct DoubleSciencificFormatI
+  {
+    double &dubscienI_;
+  };
+  struct DoubleSciencificFormatO
+  {
+    const double &dubscienO_;
+  };
+  struct StringIO
+  {
+    std::string &str_;
+  };
+}
 
 #endif //SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
