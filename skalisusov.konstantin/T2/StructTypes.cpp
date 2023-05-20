d13be9995996f4b689db2cb0cda5128b58b47bd1
Kikos1337 <skalisusov@inbox.ru>
2023-05-20 18:45:10 +0300

    adding operator input for DoubleSciencificFotmatI and DoubleSciencificFotmatO

--- skalisusov.konstantin/T2/StructTypes.cpp
+++ skalisusov.konstantin/T2/StructTypes.cpp
@@ -41,5 +41,49 @@ namespace skalisusov
     return in >> dest.dublit_ >> DelimiterIO{ 'd' };
   }
 
+  std::istream &operator>>(std::istream &in, DoubleSciencificFormatI &&dest)
+  {
+    std::istream::sentry CheckSentry(in);
+    if(!CheckSentry)
+    {
+      return in;
+    }
+    return in >> std::scientific >> dest.dubscienI_;
+  }
+
+  std::ostream &operator<<(std::ostream &out, DoubleSciencificFormatO dest)
+  {
+    std::ostream::sentry CheckSentry(out);
+    if(!CheckSentry)
+    {
+      return out;
+    }
+    double number = dest.dubscienO_;
+    int tenDegree = 0;
+    if(number < 1)
+    {
+      while(number < 10)
+      {
+        if(number > 1)
+        {
+          break;
+        }
+        else
+        {
+          number *= 10;
+          tenDegree --;
+        }
+      }
+    }
+    else
+    {
+      while(number > 10)
+      {
+        number /=10;
+        tenDegree ++;
+      }
+    }
+    return out << std::fixed << std::setprecision(1)  << number << (tenDegree > 0 ? "e+" : "e") << tenDegree;
+  }
 
 }
