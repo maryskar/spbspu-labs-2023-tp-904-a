1b9d4a8b3bbe5d9eb38d84275312a42e2ac6a658
Kikos1337 <skalisusov@inbox.ru>
2023-05-20 04:37:08 +0300

    add in iofmtguard cpp

--- skalisusov.konstantin/T2/iofmtguard.cpp
+++ skalisusov.konstantin/T2/iofmtguard.cpp
@@ -1,5 +1,14 @@
-//
-// Created by Konstantine Skalisusov on 20.05.2023.
-//
-
 #include "iofmtguard.h"
+iofmtguard::iofmtguard(std::basic_ios< char > &s) :
+        s_(s),
+        fill_(s.fill()),
+        precision_(s.precision()),
+        fmt_(s.flags())
+{}
+
+iofmtguard::~iofmtguard()
+{
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
