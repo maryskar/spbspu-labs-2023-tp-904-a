d2be61b52e6834bb591e0d22501ec295b718cfab
Kikos1337 <skalisusov@inbox.ru>
2023-05-20 19:04:23 +0300

    add operator for DataStruct

--- skalisusov.konstantin/T2/DataStruct.cpp
+++ skalisusov.konstantin/T2/DataStruct.cpp
@@ -1,2 +1,19 @@
 #include "DataStruct.h"
-
+namespace skalisusov
+{
+  std::ostream &operator<<(std::ostream &out, const DataStruct &dest)
+  {
+    std::ostream::sentry CheckSentry(out);
+    if(!CheckSentry)
+    {
+      return out;
+    }
+    iofmtguard iofmtguard(out);
+    out << "(";
+    out << ":key1 " << std::fixed << std::setprecision(1) << dest.key1_ << 'd';
+    out << ":key2 " << DoubleSciencificFormatO{dest.key2_};
+    out << ":key3 " << '"' << dest.key3_ << '"';
+    out << ":)";
+    return out;
+  }
+}
