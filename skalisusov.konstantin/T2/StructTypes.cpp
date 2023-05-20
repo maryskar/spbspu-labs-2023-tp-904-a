930472e21077c3df7c4dae9ba0b9caffdf104848
Kikos1337 <skalisusov@inbox.ru>
2023-05-20 17:57:04 +0300

    adding operator input for CoverIO

--- skalisusov.konstantin/T2/StructTypes.cpp
+++ skalisusov.konstantin/T2/StructTypes.cpp
@@ -17,5 +17,20 @@ namespace skalisusov
     }
     return in;
   }
+  std::istream &operator>>(std::istream &in, CoverIO &&dest)
+  {
+    std::istream::sentry CheckSentry(in);
+    if(!CheckSentry)
+    {
+      return in;
+    }
+    std::string cover = "";
+    if((in >> StringIO{cover}) && (cover != dest.cover_))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
 
 }
