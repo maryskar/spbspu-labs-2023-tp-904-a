183c904493a475db77ac264aa1822af7fc2063c2
root <root@DESKTOP-CG1KQKN>
2023-05-22 20:32:59 +0300

    added data struct

---
+++ azheganova.faina/T2/datastruct.h
@@ -0,0 +1,14 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCH_H
+#include <string>
+
+namespace azheganova
+{
+  struct DataStruct
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+#endif
