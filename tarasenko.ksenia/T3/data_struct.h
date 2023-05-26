9635c32fb7dc5267f1359fb3f7d8b5c08051bd97
xeniaTa <kseniatarasenko1@gmail.com>
2023-05-26 12:13:10 +0300

    add structs data

---
+++ tarasenko.ksenia/T3/data_struct.h
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+#include <vector>
+
+namespace tarasenko
+{
+  struct Point
+  {
+    int x, y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+}
+#endif
