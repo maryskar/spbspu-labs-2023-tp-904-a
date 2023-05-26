d420f4fc66087140dfe5c50e6de3ec01963fd543
Chemodurovart <chemodurovart@gmail.com>
2023-05-26 16:31:30 +0300

    fixed ignore

--- chemodurov.artem/T2/main.cpp
+++ chemodurov.artem/T2/main.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <limits>
 #include "data-struct.hpp"
 
 int main()
 {
+  constexpr auto max_size = std::numeric_limits< std::streamsize >::max();
   std::vector< chemodurov::DataStruct > data;
   while (!std::cin.eof())
   {
     if (std::cin.fail())
     {
       std::cin.clear();
-      std::string line;
-      std::getline(std::cin, line);
+      std::cin.ignore(max_size, '\n');
     }
     using istr_it_data_t = std::istream_iterator< chemodurov::DataStruct >;
     std::copy(istr_it_data_t(std::cin), istr_it_data_t(), std::back_inserter(data));
