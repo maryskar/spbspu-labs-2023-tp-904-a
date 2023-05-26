e29555ad4b5451281aff3632f5656ee62809ce71
root <root@DESKTOP-CG1KQKN>
2023-05-26 14:55:26 +0300

    added iofmtguard

---
+++ azheganova.faina/T2/iofmtguard.h
@@ -0,0 +1,20 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCH_H
+#include <iostream>
+
+namespace azheganova
+{
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios< char > &s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
+
+#endif
