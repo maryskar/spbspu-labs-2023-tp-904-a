fd81192b6dacbb8719b5e016bff4b693e7897a3c
Litvin Sofia <sofialitvin823@gmail.com>
2023-05-28 15:30:50 +0300

    deleted ns

--- litvin.sofia/T2/main.cpp
+++ litvin.sofia/T2/main.cpp
@@ -4,24 +4,20 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
-namespace litvin
-{
-  int main()
+int main(){
+  using inIter = std::istream_iterator< litvin::DataStruct >;
+  using outIter = std::ostream_iterator< litvin::DataStruct >;
+  std::vector <litvin::DataStruct> data;
+  while (!std::cin.eof())
   {
-    using inIter = std::istream_iterator< litvin::DataStruct >;
-    using outIter = std::ostream_iterator< litvin::DataStruct >;
-    std::vector <litvin::DataStruct> data;
-    while (!std::cin.eof())
+    std::copy(inIter(std::cin), inIter(), std::back_inserter(data));
+    if (!std::cin)
     {
-      std::copy(inIter(std::cin), inIter(), std::back_inserter(data));
-      if (!std::cin)
-      {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      }
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
-    std::sort(data.begin(), data.end(), litvin::Compare{});
-    std::copy(data.begin(), data.end(), outIter(std::cout, "\n"));
-    return 0;
   }
+  std::sort(data.begin(), data.end(), litvin::Compare{});
+  std::copy(data.begin(), data.end(), outIter(std::cout, "\n"));
+  return 0;
 }
