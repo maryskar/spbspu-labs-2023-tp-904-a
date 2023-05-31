8a4f16558cdb030be0f2b5e18b01af2f95e0f86e
Maxim <maks.timofeev9834@gmail.com>
2023-05-31 17:11:08 +0300

    add datastruct without special type

--- timofeev.maxim/T2/DataStruct.h
+++ timofeev.maxim/T2/DataStruct.h
@@ -1,5 +1,14 @@
-
-#ifndef T2_DATASTRUCT_H
-#define T2_DATASTRUCT_H
-
-#endif // T2_DATASTRUCT_H
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+#include <iostream>
+#include <string>
+namespace timofeev
+{
+  struct DataStruct
+  {
+    /*type 1*/ key1;
+    /*type 1*/ key2;
+    std::string key3;
+  };
+}
+#endif
