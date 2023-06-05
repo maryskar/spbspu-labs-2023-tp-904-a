9260520bc0745f8d33c4cf92bebd6980b82b4845
xeniaTa <kseniatarasenko1@gmail.com>
2023-06-05 11:45:57 +0300

    fix commands and call

--- tarasenko.ksenia/T3/call.h
+++ tarasenko.ksenia/T3/call.h
@@ -21,13 +21,13 @@ namespace tarasenko
   void call(const std::string& command1, std::vector< Polygon >& p, std::istream& in, std::ostream& out)
   {
     Commands commands;
-    if (commands.find(command1))
+    std::string command2 = "";
+    in >> command2;
+    try
     {
-      std::string command2 = "";
-      in >> command2;
-      try
+      if (!isdigit(command2[0]))
       {
-        if (!isdigit(command2[0]))
+        if (commands.findIn(command1 + " " + command2, 3))
         {
           std::function< double(const std::vector< Polygon >&) > command;
           commands.get(command1 + " " + command2, command);
@@ -35,19 +35,30 @@ namespace tarasenko
         }
         else
         {
+          std::function< size_t(const std::vector< Polygon >&) > command;
+          commands.get(command1 + " " + command2, command);
+          out << command(p) << "\n";
+        }
+      }
+      else
+      {
+        if (commands.findIn(command1, 1))
+        {
           std::function< double(const std::vector< Polygon >&, const size_t&) > command;
           commands.get(command1, command);
           auto n = std::stoull(command2);
           out << std::fixed << std::setprecision(1) << command(p, n) << "\n";
         }
-      }
-      catch (const std::exception&)
-      {
-        out << outMessageInvalidCommand << "\n";
-        readTrash(in);
+        else
+        {
+          std::function< size_t(const std::vector< Polygon >&, const size_t&) > command;
+          commands.get(command1, command);
+          auto n = std::stoull(command2);
+          out << command(p, n) << "\n";
+        }
       }
     }
-    else
+    catch (const std::exception&)
     {
       out << outMessageInvalidCommand << "\n";
       readTrash(in);
--- tarasenko.ksenia/T3/commands.h
+++ tarasenko.ksenia/T3/commands.h
@@ -8,31 +8,54 @@
 
 namespace tarasenko
 {
+  using namespace std::placeholders;
+
   class Commands
   {
   public:
    Commands():
      names(),
      type_1(),
-     type_2()
+     type_2(),
+     type_3(),
+     type_4()
    {
-     names = {"AREA", "MAX", "MIN", "COUNT", "INFRAME", "RIGHTSHAPES"};
+     names = {
+       {"AREA"},
+       {"COUNT"},
+       {"AREA EVEN", "AREA ODD", "AREA MEAN", "MAX AREA", "MIN AREA"},
+       {"MAX VERTEXES", "MIN VERTEXES", "COUNT EVEN", "COUNT ODD"},
+       {"INFRAME", "RIGHTSHAPES"}
+     };
 
      type_1.insert(std::make_pair("AREA", &getAreaWithEqualNumVerts));
-     //type_1.insert(std::make_pair("COUNT", &getAreaWithEqualNumVerts));
+     type_2.insert(std::make_pair("COUNT", &getNumWithEqualNumVerts));
 
-     type_2.insert(std::make_pair("AREA EVEN", &getAreaEven));
-     type_2.insert(std::make_pair("AREA ODD", &getAreaOdd));
-     type_2.insert(std::make_pair("AREA MEAN", &getAreaMean));
-     type_2.insert(std::make_pair("MAX AREA", &getMaxArea));
-     type_2.insert(std::make_pair("MIN AREA", &getMinArea));
-     type_2.insert(std::make_pair("MAX VERTEXES", &getMaxVerts));
-     type_2.insert(std::make_pair("MIN VERTEXES", &getMinVerts));
+     type_3.insert(std::make_pair("AREA EVEN", &getAreaEven));
+     type_3.insert(std::make_pair("AREA ODD", &getAreaOdd));
+     type_3.insert(std::make_pair("AREA MEAN", &getAreaMean));
+     type_3.insert(std::make_pair("MAX AREA", &getMaxArea));
+     type_3.insert(std::make_pair("MIN AREA", &getMinArea));
+
+     type_4.insert(std::make_pair("MAX VERTEXES", &getMaxVerts));
+     type_4.insert(std::make_pair("MIN VERTEXES", &getMinVerts));
+     type_4.insert(std::make_pair("COUNT EVEN", &getNumEven));
+     type_4.insert(std::make_pair("COUNT ODD", &getNumOdd));
    }
 
    bool find(const std::string& name)
    {
-     return std::find(names.begin(), names.end(), name) != names.end();
+     auto cond = std::bind(std::equal_to< std::string >{}, _1, name);
+     auto in_any_of = [&](const auto& in_vector) {
+       return std::any_of(in_vector.begin(), in_vector.end(), cond);
+     };
+     return std::any_of(names.begin(), names.end(), in_any_of);
+   }
+
+   bool findIn(const std::string& name, size_t type)
+   {
+     size_t i = type - 1;
+     return std::find(names[i].begin(), names[i].end(), name) != names[i].end();
    }
 
    void get(const std::string& key, std::function< double(const std::vector< Polygon >&, const size_t&) >& command)
@@ -47,7 +70,7 @@ namespace tarasenko
      }
    }
 
-   void get(const std::string& key, std::function< double(const std::vector< Polygon >&) >& command)
+   void get(const std::string& key, std::function< size_t(const std::vector< Polygon >&, const size_t&) >& command)
    {
      try
      {
@@ -59,10 +82,36 @@ namespace tarasenko
      }
    }
 
+   void get(const std::string& key, std::function< double(const std::vector< Polygon >&) >& command)
+   {
+     try
+     {
+       command = type_3.at(key);
+     }
+     catch (const std::out_of_range&)
+     {
+       throw std::out_of_range("Not found");
+     }
+   }
+
+   void get(const std::string& key, std::function< size_t(const std::vector< Polygon >&) >& command)
+   {
+     try
+     {
+       command = type_4.at(key);
+     }
+     catch (const std::out_of_range&)
+     {
+       throw std::out_of_range("Not found");
+     }
+   }
+
   private:
-   std::vector< std::string > names;
+   std::vector< std::vector< std::string > > names;
    std::map< std::string, std::function< double(const std::vector< Polygon >&, const size_t&) > > type_1;
-   std::map< std::string, std::function< double(const std::vector< Polygon >&) > > type_2;
+   std::map< std::string, std::function< size_t(const std::vector< Polygon >&, const size_t&) > > type_2;
+   std::map< std::string, std::function< double(const std::vector< Polygon >&) > > type_3;
+   std::map< std::string, std::function< size_t(const std::vector< Polygon >&) > > type_4;
   };
 }
 #endif
