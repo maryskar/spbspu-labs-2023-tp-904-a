c4a92b870dd2ac538f22a4e66607e04efc8bd6de
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-07 13:40:24 +0300

    create polygon

---
+++ shelkovnikov.dmitrii/common/polygon.h
@@ -0,0 +1,12 @@
+#ifndef SPBSPU_LABS_2023_TP_904_POLYGON_HPP
+#define SPBSPU_LABS_2023_TP_904_POLYGON_HPP
+#include <vector>
+#include "point.h"
+namespace dimkashelk
+{
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+}
+#endif
