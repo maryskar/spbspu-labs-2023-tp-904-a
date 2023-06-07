5f19a735f7367a3a034bd36d9af68f38948286a7
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-07 19:03:49 +0300

    add getFilteredPolygons

--- shelkovnikov.dmitrii/common/commands.cpp
+++ shelkovnikov.dmitrii/common/commands.cpp
@@ -7,16 +7,24 @@
 namespace
 {
   typedef dimkashelk::Polygon polygon;
+  typedef std::vector< polygon > v_polygon;
   bool isEven(const polygon &polygon)
   {
     return polygon.points.size() % 2 == 0;
   }
-  void printArea(const std::vector< polygon > &filtered, std::ostream &out)
+  void printArea(const v_polygon &filtered, std::ostream &out)
   {
     std::vector< double > filtered_area;
     std::transform(filtered.begin(), filtered.end(), std::back_inserter(filtered_area), dimkashelk::getArea);
     dimkashelk::iofmtguard iofmtguard(out);
     out << std::setprecision(1) << std::accumulate(filtered_area.begin(), filtered_area.end(), 0.0);
+  }
+  template< typename UnaryOperation >
+  v_polygon getFilteredPolygons(const v_polygon &pol, UnaryOperation op)
+  {
+    v_polygon result;
+    std::transform(pol.begin(), pol.end(), std::back_inserter(result), op);
+    return result;
   }
 }
 void dimkashelk::printAreaEven(const std::vector< Polygon > &polygon, std::ostream &out)
