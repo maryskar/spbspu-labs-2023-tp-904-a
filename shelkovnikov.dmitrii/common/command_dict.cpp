4db79c5d374247656c1769fb16ca8f49e96dabe0
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-07 18:40:13 +0300

    add commands to command_dict

--- shelkovnikov.dmitrii/common/command_dict.cpp
+++ shelkovnikov.dmitrii/common/command_dict.cpp
@@ -1,7 +1,20 @@
 #include "command_dict.h"
+#include "commands.h"
 dimkashelk::comm_dict_t dimkashelk::createCommDict()
 {
   dimkashelk::comm_dict_t res{};
-  //
+  res.dic1["AREA EVEN"] = printAreaEven;
+  res.dic1["AREA ODD"] = printAreaOdd;
+  res.dic1["AREA MEAN"] = printAreaMean;
+  res.dic2["AREA"] = printAreaNumOfVertex;
+  res.dic1["MAX AREA"] = printMaxArea;
+  res.dic1["MAX VERTEXES"] = printMaxVertex;
+  res.dic1["MIN AREA"] = printMinArea;
+  res.dic1["MIN VERTEXES"] = printMinVertex;
+  res.dic1["COUNT EVEN"] = printCountEven;
+  res.dic1["COUNT ODD"] = printCountOdd;
+  res.dic2["COUNT"] = printCountNumOfVertex;
+  res.dic3["INTERSECTIONS"] = printIntersections;
+  res.dic3["SAME"] = printSame;
   return res;
 }
