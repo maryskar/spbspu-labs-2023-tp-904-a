a6c95efe06acb4bec8031257504726946d1d5561
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-07 15:25:16 +0300

    add check parameters

--- shelkovnikov.dmitrii/T3/main.cpp
+++ shelkovnikov.dmitrii/T3/main.cpp
@@ -1,5 +1,17 @@
-#include <io.h>
-int main()
+#include <iostream>
+#include <fstream>
+int main(int argc, char * argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << "Check arguments\n";
+    return 1;
+  }
+  std::ifstream in(argv[1]);
+  if (!in)
+  {
+    std::cerr << "File not open\n";
+    return 2;
+  }
   return 0;
 }
