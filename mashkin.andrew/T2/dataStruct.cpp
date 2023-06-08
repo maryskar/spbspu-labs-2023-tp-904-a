fb22516fe1829717bbecb7e6e09d4ded81094a1f
Машкин Андрей <beedzibood@gmail.com>
2023-06-08 23:46:03 +0300

    transmit to common directory input structs that I need to use in T3

--- mashkin.andrew/T2/dataStruct.cpp
+++ mashkin.andrew/T2/dataStruct.cpp
@@ -6,6 +6,7 @@
 #include <ios>
 #include <iostream>
 #include <string>
+#include <InputStructs.h>
 
 namespace mashkin
 {
@@ -47,79 +48,6 @@ namespace mashkin
     }
     res = "0b0" + res;
     return res;
-  }
-
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    char c = '0';
-    in >> c;
-    if (in && (c != dest.exp))
-    {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
-
-  std::istream& operator>>(std::istream& in, DoubleIO&& dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    std::string dblStr;
-    std::getline(in, dblStr, ':');
-    if (dblStr.find("e") == std::string::npos)
-    {
-      in.setstate(std::ios::failbit);
-      return in;
-    }
-    dest.ref = stod(dblStr);
-    return in;
-  }
-
-  std::istream& operator>>(std::istream& in, StringIO&& dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
-    return in >> DelimiterIO{':'};
-  }
-
-  std::istream& operator>>(std::istream& in, UllIO&& dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    std::string var;
-    std::getline(in, var, ':');
-    if (var.substr(0, 2) != "0b")
-    {
-      in.setstate(std::ios::failbit);
-      return in;
-    }
-    dest.ref = std::bitset< 64 >(var.substr(2)).to_ullong();
-    return in;
-  }
-
-  std::istream& operator>>(std::istream& in, LabelIO&& dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    return in >> dest.exp;
   }
 
   std::istream& operator>>(std::istream& in, DataStruct& dest)
--- mashkin.andrew/T2/dataStruct.h
+++ mashkin.andrew/T2/dataStruct.h
@@ -12,31 +12,6 @@ namespace mashkin
     std::string key3;
   };
 
-  struct DelimiterIO
-  {
-    char exp;
-  };
-
-  struct DoubleIO
-  {
-    double& ref;
-  };
-
-  struct StringIO
-  {
-    std::string& ref;
-  };
-
-  struct LabelIO
-  {
-    std::string& exp;
-  };
-
-  struct UllIO
-  {
-    unsigned long long& ref;
-  };
-
   class iofmtguard
   {
   public:
@@ -55,11 +30,6 @@ namespace mashkin
     bool operator()(const DataStruct& first, const DataStruct& second);
   };
 
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-  std::istream& operator>>(std::istream& in, DoubleIO&& dest);
-  std::istream& operator>>(std::istream& in, StringIO&& dest);
-  std::istream& operator>>(std::istream& in, LabelIO&& dest);
-  std::istream& operator>>(std::istream& in, UllIO&& dest);
   std::istream& operator>>(std::istream& inp, DataStruct& data);
   std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 }
