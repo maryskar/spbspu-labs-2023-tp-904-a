7584e1155d51fec6060321560604b9db605dc802
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-08 18:08:12 +0300

    update main

--- shelkovnikov.dmitrii/T3/main.cpp
+++ shelkovnikov.dmitrii/T3/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 #include <fstream>
-#include <map>
-int main(int argc, char * argv[])
+#include <vector>
+#include <limits>
+#include <iterator>
+#include "polygon.h"
+#include "command_dict.h"
+std::ostream &outInvalidCommandMessage(std::ostream &out)
+{
+  out << "<INVALID COMMAND>";
+  return out;
+}
+std::string inputCommand(std::istream &in)
+{
+  std::string res;
+  in >> res;
+  if (!in)
+  {
+    throw std::runtime_error("Nothing to input");
+  }
+  if (res == "INTERSECTIONS" || res == "SAME")
+  {
+    return res;
+  }
+  std::string temp;
+  in >> temp;
+  if (!in)
+  {
+    throw std::invalid_argument("Invalid command");
+  }
+  res += ' ' + temp;
+  return res;
+}
+void doCommand(std::vector< dimkashelk::Polygon > &data,
+  dimkashelk::comm_dict_t allCommands,
+  std::string &command,
+  std::istream &in,
+  std::ostream &out)
+{
+  try
+  {
+    allCommands.dic3.at(command)(data, out, in);
+  }
+  catch (const std::out_of_range &e)
+  {}
+  try
+  {
+    allCommands.dic1.at(command)(data, out);
+  }
+  catch (const std::out_of_range &e)
+  {}
+  auto spaceIndex = command.find(' ');
+  auto index = std::stoull(command.substr(spaceIndex));
+  allCommands.dic2.at(command.substr(0, spaceIndex))(data, out, index);
+}
+int main(int argc, char *argv[])
 {
   if (argc != 2)
   {
@@ -14,6 +66,35 @@ int main(int argc, char * argv[])
     std::cerr << "File not open\n";
     return 2;
   }
-
+  constexpr auto max_size = std::numeric_limits< std::streamsize >::max();
+  using input_iter = std::istream_iterator< dimkashelk::Polygon >;
+  std::vector< dimkashelk::Polygon > data;
+  while (in)
+  {
+    std::copy(input_iter(in), input_iter(), std::back_inserter(data));
+    if (!in)
+    {
+      in.clear();
+      in.ignore(max_size, '\n');
+    }
+  }
+  auto commands = dimkashelk::createCommDict();
+  while (std::cin)
+  {
+    try
+    {
+      auto command = inputCommand(std::cin);
+      doCommand(data, commands, command, std::cin, std::cout);
+    }
+    catch (const std::logic_error &e)
+    {
+      outInvalidCommandMessage(std::cout) << '\n';
+      std::cin.ignore(max_size, '\n');
+    }
+    catch (const std::runtime_error &e)
+    {
+      break;
+    }
+  }
   return 0;
 }
