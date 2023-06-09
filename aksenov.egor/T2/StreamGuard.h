e1f443457090bc581b85ff1693156c4e524ea786
aksenovEA <aksenov200477@gmail.com>
2023-06-09 03:26:59 +0300

    add StreamGuard.h

---
+++ aksenov.egor/T2/StreamGuard.h
@@ -0,0 +1,16 @@
+#ifndef STREAMGUARD_H
+#define STREAMGUARD_H
+#include <iomainp>
+namespace aksenov {
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios<char> &s);
+    ~StreamGuard();
+  private:
+    std::basic_ios<char> &s_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags flags_;
+  };
+}
+#endif
--- aksenov.egor/T2/main.cpp
+++ aksenov.egor/T2/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main()
 {
-  std::cout << "Init commit" << "\n";
+  std::cout << "Init commit";
 }
