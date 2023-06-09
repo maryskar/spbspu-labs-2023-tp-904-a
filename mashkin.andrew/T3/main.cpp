d9cb95726f087b6b78caf371baf8bed0c3fdc3e9
Машкин Андрей <beedzibood@gmail.com>
2023-06-09 15:13:22 +0300

    added checking to file and getting data from inputFile

--- mashkin.andrew/T3/main.cpp
+++ mashkin.andrew/T3/main.cpp
@@ -1,4 +1,38 @@
-int main()
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <limits>
+#include <fstream>
+#include "polygon.h"
+
+int main(int argc, char** argv)
 {
+  if (argc > 2)
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
+  std::ifstream inpFile;
+  if (argc == 2)
+  {
+    inpFile.open(argv[1]);
+    if (!inpFile.is_open())
+    {
+      std::cerr << "File isn't open\n";
+      return 1;
+    }
+  }
+  std::vector< mashkin::Polygon > res;
+  using inpIter = std::istream_iterator< mashkin::Polygon >;
+  constexpr auto maxSize = std::numeric_limits< std::streamsize >::max();
+  while (!inpFile.eof())
+  {
+    std::copy(inpIter(inpFile), inpIter(), std::back_inserter(res));
+    if (inpFile.fail())
+    {
+      inpFile.clear();
+      inpFile.ignore(maxSize, '\n');
+    }
+  }
   return 0;
 }
