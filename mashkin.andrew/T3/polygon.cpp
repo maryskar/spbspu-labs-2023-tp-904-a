14dfa963c91721e27f87058c40db9eebe0969564
Машкин Андрей <beedzibood@gmail.com>
2023-06-09 15:31:34 +0300

    improved operator>> for Polygon and defined operator>> for Point

--- mashkin.andrew/T3/polygon.cpp
+++ mashkin.andrew/T3/polygon.cpp
@@ -7,6 +7,18 @@
 
 namespace mashkin
 {
+  std::istream& operator>>(std::istream& inp, Point& point)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
+    {
+      return inp;
+    }
+    inp >> DelimiterIO{'('} >> point.x >> DelimiterIO{';'};
+    inp >> point.y >> DelimiterIO{')'};
+    return inp;
+  }
+
   std::istream& operator>>(std::istream& inp, Polygon& dest)
   {
     std::istream::sentry sentry(inp);
@@ -14,9 +26,15 @@ namespace mashkin
     {
       return inp;
     }
+    Polygon input;
     size_t numOfPoints;
     inp >> numOfPoints;
     using inpIter = std::istream_iterator< mashkin::Point >;
-    std::copy_n(inpIter(inp), numOfPoints, std::back_inserter(dest.points));
+    std::copy_n(inpIter(inp), numOfPoints, std::back_inserter(input.points));
+    if (inp)
+    {
+      dest = input;
+    }
+    return inp;
   }
 }
