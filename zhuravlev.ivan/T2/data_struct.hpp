6c1ab515de4eb5fb8dd207914251a18f1f6aee77
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-06-09 16:02:35 +0300

    added scopeguard and data structs

---
+++ zhuravlev.ivan/T2/data_struct.hpp
@@ -0,0 +1,21 @@
+#ifndef SPBSPU_LABS_2023_TP_904_A_DATA_STRUCT_HPP
+#define SPBSPU_LABS_2023_TP_904_A_DATA_STRUCT_HPP
+#include <string>
+
+namespace zhuravlev
+{
+  struct Data
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  struct DataComparator
+  {
+    bool operator()(const Data& lhs, const Data& rhs);
+  };
+}
+
+
+#endif
--- zhuravlev.ivan/T2/main.cpp
+++ zhuravlev.ivan/T2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "iofmtguard.hpp"
 
 int main()
 {
