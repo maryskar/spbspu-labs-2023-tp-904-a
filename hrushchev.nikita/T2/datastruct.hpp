d8e5677f0e9cd9707e31f8526dbcc0c154274681
Shuzamada <shuzamada@inbox.ru>
2023-06-09 21:34:32 +0300

    datastruct >>

--- hrushchev.nikita/T2/datastruct.hpp
+++ hrushchev.nikita/T2/datastruct.hpp
@@ -9,6 +9,8 @@ namespace hrushchev
 		std::complex< double > key2;
 		std::string key3;
 	};
-	
+
 	std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+	std::istream& operator>>(std::istream& in, DataStruct& data);
+
 }
