5eb29635773bbf1b2e24636f6216da5b1fbc916f
Машкин Андрей <beedzibood@gmail.com>
2023-06-09 15:25:38 +0300

    defined operator>> for Polygon

--- mashkin.andrew/T3/polygon.cpp
+++ mashkin.andrew/T3/polygon.cpp
@@ -1,2 +1,22 @@
 #include "polygon.h"
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <iterator>
 #include <inputStructs.h>
+
+namespace mashkin
+{
+  std::istream& operator>>(std::istream& inp, Polygon& dest)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
+    {
+      return inp;
+    }
+    size_t numOfPoints;
+    inp >> numOfPoints;
+    using inpIter = std::istream_iterator< mashkin::Point >;
+    std::copy_n(inpIter(inp), numOfPoints, std::back_inserter(dest.points));
+  }
+}
