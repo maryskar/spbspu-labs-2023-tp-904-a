a40bf5dc69a61a251e015c0b7085caff6c570635
Shuzamada <shuzamada@inbox.ru>
2023-06-10 17:51:31 +0300

    delimiterio >>

---
+++ hrushchev.nikita/T2/iotypes.cpp
@@ -0,0 +1,20 @@
+#include "iotypes.hpp"
+
+namespace hrushchev
+{
+  std::istream& operator>>(std::istream &in, DelimiterIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+}
