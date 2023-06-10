63e690a10035ba062f16ad728781e59085ef9d87
Vagina Daria <dvagina@inbox.ru>
2023-06-10 22:31:32 +0300

    moved typesIO

--- vagina.daria/T3/polygon.cpp
+++ vagina.daria/T3/polygon.cpp
@@ -1,6 +1,7 @@
 #include "polygon.h"
+#include "../common/TypesIO.h"
 
-std::istream& operator>>(std::istream& in, Point& dest)
+std::istream& vagina::operator>>(std::istream& in, Point& dest)
 {
   std::istream::sentry sentry(in);
   if (!sentry)
@@ -8,21 +9,5 @@ std::istream& operator>>(std::istream& in, Point& dest)
     return in;
   }
   in >> DelimiterIO{ '(' } >> dest.x >> DelimiterIO{ ';' } >> dest.y >> DelimiterIO{ ')' };
-  return in;
-}
-
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  char delim = '0';
-  in >> delim;
-  if (in && (delim != dest.symbol))
-  {
-    in.setstate(std::ios::failbit);
-  }
   return in;
 }
