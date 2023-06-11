f5e8819f90b9fa81744352b8332e1101e9ffdb9c
root <root@DESKTOP-CG1KQKN>
2023-06-11 22:37:52 +0300

    fixed mistakes

--- azheganova.faina/T2/iofmtguard.h
+++ azheganova.faina/T2/iofmtguard.h
@@ -7,7 +7,7 @@ namespace azheganova
   class iofmtguard
   {
   public:
-    iofmtguard(std::basic_ios< char > & s);
+    explicit iofmtguard(std::basic_ios< char > & s);
     ~iofmtguard();
   private:
     std::basic_ios< char > & s_;
--- azheganova.faina/T2/main.cpp
+++ azheganova.faina/T2/main.cpp
@@ -1,20 +1,25 @@
-#include <vector>
+#include <deque>
 #include <iterator>
 #include <algorithm>
+#include <limits>
 #include "datastruct.h"
 
 int main()
 {
   using ist_iter = std::istream_iterator< azheganova::DataStruct >;
-  std::vector< azheganova::DataStruct > data;
+  using ost_iter = std::ostream_iterator< azheganova::DataStruct >;
+  std::deque< azheganova::DataStruct > data;
   while (!std::cin.eof())
   {
-    std::cin.clear();
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
     std::copy(ist_iter(std::cin), ist_iter(), std::back_inserter(data));
   }
   std::sort(data.begin(), data.end(), azheganova::compareValues);
-  std::copy(std::begin(data), std::end(data), std::ostream_iterator< azheganova::DataStruct >(std::cout, "\n")
-  );
+  std::copy(std::begin(data), std::end(data), ost_iter(std::cout, "\n"));
   return 0;
 }
 
