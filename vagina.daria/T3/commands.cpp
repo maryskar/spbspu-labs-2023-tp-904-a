05e3e00bfd4029d9aa5249d456f4031059c88a8c
Vagina Daria <dvagina@inbox.ru>
2023-06-12 19:01:51 +0300

    fix area

--- vagina.daria/T3/commands.cpp
+++ vagina.daria/T3/commands.cpp
@@ -14,10 +14,8 @@ bool vagina::isCountOfVertexes(const Polygon& pol, std::size_t param)
 void vagina::areaEven(const std::vector< Polygon >& dest, std::ostream& out)
 {
   std::vector< Polygon > tmp (dest.size());
-  std::copy_if(dest.begin(), dest.end(), tmp.begin(),
-    [&](Polygon i) { return isEven(i); });
-  std::size_t count = std::count_if(dest.begin(), dest.end(),
-    [&](Polygon i) { return isEven(i); });
+  std::copy_if(dest.begin(), dest.end(), tmp.begin(), isEven);
+  std::size_t count = std::count_if(dest.begin(), dest.end(), isEven);
   std::vector< double > tmpS(count);
   std::transform(tmp.begin(), tmp.end(), std::back_inserter(tmpS), getArea);
   out << std::setprecision(1) << std::accumulate(tmpS.begin(), tmpS.end(), 0.0) << "\n";
