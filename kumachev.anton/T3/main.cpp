e525620aa8409ac1a22b8c614503313a8b1a71a0
anton <antkumachev@gmail.com>
2023-06-13 22:51:35 +0300

    Polygon IO

--- kumachev.anton/T3/main.cpp
+++ kumachev.anton/T3/main.cpp
@@ -1,7 +1,36 @@
-#include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <iterator>
 #include "polygon.h"
+#include "polygon_io.h"
 
-int main()
+int main(int argc, char **argv)
 {
+  using streamsize_limits = std::numeric_limits< std::streamsize >;
+  using istream_iterator = std::istream_iterator< kumachev::Polygon >;
+
+  if (argc != 2) {
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+
+  if (!file.is_open()) {
+    return 1;
+  }
+
+  std::vector< kumachev::Polygon > shapes;
+  auto backInserter = std::back_inserter(shapes);
+
+  while (!file.eof()) {
+    std::copy(istream_iterator(file), istream_iterator(), backInserter);
+
+    if (file.fail()) {
+      file.clear();
+      file.ignore(streamsize_limits::max(), '\n');
+    }
+  }
+
   return 0;
 }
---
+++ kumachev.anton/T3/polygon_io.cpp
@@ -0,0 +1,71 @@
+#include "polygon_io.h"
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+namespace kumachev {
+  std::istream &operator>>(std::istream &istream, CharIO &&character)
+  {
+    std::istream::sentry sentry(istream);
+
+    if (!sentry) {
+      return istream;
+    }
+
+    char c = '\0';
+    istream >> c;
+
+    if (c != character.value) {
+      istream.setstate(std::ios::failbit);
+    }
+
+    return istream;
+  }
+
+  std::istream &operator>>(std::istream &istream, Point &point)
+  {
+    std::istream::sentry sentry(istream);
+
+    if (!sentry) {
+      return istream;
+    }
+
+    Point value{ 0, 0 };
+    istream >> CharIO{ '(' } >> value.x >> CharIO{ ';' } >> value.y;
+    istream >> CharIO{ ')' };
+
+    if (istream) {
+      point = value;
+    }
+
+    return istream;
+  }
+
+  std::istream &operator>>(std::istream &istream, Polygon &polygon)
+  {
+    using istream_iterator = std::istream_iterator< Point >;
+    std::istream::sentry sentry(istream);
+
+    if (!sentry) {
+      return istream;
+    }
+
+    size_t vertexCount = 0;
+    istream >> vertexCount;
+
+    if (!istream || vertexCount < 3) {
+      istream.setstate(std::ios::failbit);
+      return istream;
+    }
+
+    Polygon value;
+    auto backInserter = std::back_inserter(value.points);
+    std::copy_n(istream_iterator(istream), vertexCount, backInserter);
+
+    if (istream) {
+      std::swap(polygon, value);
+    }
+
+    return istream;
+  }
+}
