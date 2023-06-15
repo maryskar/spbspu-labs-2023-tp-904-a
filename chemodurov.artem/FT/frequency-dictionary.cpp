4596bf2928baa0052e85cfd7e88f7d97a410d75f
Chemodurovart <chemodurovart@gmail.com>
2023-06-15 09:53:50 +0300

    copy frequency-dictionary from FA and change to standard

---
+++ chemodurov.artem/FT/frequency-dictionary.cpp
@@ -0,0 +1,67 @@
+#include "frequency-dictionary.hpp"
+#include <iostream>
+#include <algorithm>
+
+namespace chemodurov
+{
+  bool isAlphabet(char c)
+  {
+    return !std::isalpha(static_cast< unsigned char >(c));
+  }
+
+  char toLower(char c)
+  {
+    return static_cast< char >(std::tolower(c));
+  }
+
+  Word::Word(const std::string & str_word):
+   data_(str_word)
+  {
+    auto it = std::find_if_not(data_.begin(), data_.end(), isAlphabet);
+    data_.erase(data_.begin(), it);
+    auto rit = std::find_if_not(data_.rbegin(), data_.rend(), isAlphabet);
+    if (rit != data_.rend())
+    {
+      data_.erase(rit.base() - 1, data_.end());
+    }
+    if (!data_.empty() && !isAlphabet(data_[0]))
+    {
+      data_.erase(0, 1);
+    }
+    if (data_.empty())
+    {
+      throw std::invalid_argument("Empty word");
+    }
+    std::transform(data_.begin(), data_.end(), data_.begin(), toLower);
+  }
+
+  std::string Word::getWord() const
+  {
+    return data_;
+  }
+
+  bool operator<(const Word & lhs, const Word & rhs)
+  {
+    return lhs.getWord() < rhs.getWord();
+  }
+
+  std::ostream & operator<<(std::ostream & out, const chemodurov::Word & word)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    return out << word.getWord();
+  }
+
+  std::ostream & operator<<(std::ostream & out, const std::pair< const Word, size_t > & pair)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    return out << pair.first << ' ' << pair.second;
+  }
+}
---
+++ chemodurov.artem/FT/frequency-dictionary.hpp
@@ -0,0 +1,22 @@
+#ifndef FA_FREQUENCY_DICTIONARY_HPP
+#define FA_FREQUENCY_DICTIONARY_HPP
+#include <map>
+
+namespace chemodurov
+{
+  class Word
+  {
+   public:
+    explicit Word(const std::string & str_word);
+    std::string getWord() const;
+   private:
+    std::string data_;
+  };
+  bool operator<(const Word & lhs, const Word & rhs);
+  std::ostream & operator<<(std::ostream & out, const Word & word);
+  std::ostream & operator<<(std::ostream & out, const std::pair< const Word, size_t > & pair);
+  using FreqDict = std::map< Word, size_t >;
+  using DictWithFreqDicts = std::map< std::string, FreqDict >;
+}
+
+#endif
