d814480aef68e2549b8d9adc235c9800d85df48b
Shuzamada <shuzamada@inbox.ru>
2023-07-06 09:04:22 +0300

    try fix cg 49

--- hrushchev.nikita/T2/iotypes.cpp
+++ hrushchev.nikita/T2/iotypes.cpp
@@ -1,55 +1,52 @@
 #include "iotypes.hpp"
 
-namespace hrushchev
+std::istream& hrushchev::operator>>(std::istream& in, DelimiterIO&& dest)
 {
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  std::istream::sentry sentry(in);
+  if (!sentry)
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    char c = '0';
-    in >> c;
-    if (in && (c != dest.exp))
-    {
-      in.setstate(std::ios::failbit);
-    }
     return in;
   }
-
-  std::istream& operator>>(std::istream& in, UnsignedLongLongIO&& dest)
+  char c = '0';
+  in >> c;
+  if (in && (c != dest.exp))
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    return in >> dest.num;
+    in.setstate(std::ios::failbit);
   }
+  return in;
+}
 
-  std::istream& operator>>(std::istream& in, ComplexIO&& dest)
+std::istream& hrushchev::operator>>(std::istream& in, UnsignedLongLongIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    double real = 0.0;
-    double imag = 0.0;
-    in >> DelimiterIO{'#'} >> DelimiterIO{'c'} >> DelimiterIO{'('};
-    in >> real >> imag >> DelimiterIO{')'};
-    dest.cmp = std::complex< double >(real, imag);
     return in;
   }
+  return in >> dest.num;
+}
 
-  std::istream& operator>>(std::istream& in, StringIO&& dest)
+std::istream& hrushchev::operator>>(std::istream& in, ComplexIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    return std::getline(in >> DelimiterIO{'"'}, dest.str, '"');
+    return in;
   }
+  double real = 0.0;
+  double imag = 0.0;
+  in >> DelimiterIO{'#'} >> DelimiterIO{'c'} >> DelimiterIO{'('};
+  in >> real >> imag >> DelimiterIO{')'};
+  dest.cmp = std::complex< double >(real, imag);
+  return in;
+}
+
+std::istream& hrushchev::operator>>(std::istream& in, StringIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return std::getline(in >> DelimiterIO{'"'}, dest.str, '"');
 }
