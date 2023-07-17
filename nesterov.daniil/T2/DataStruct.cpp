5737b671329948756896f5c84051983a5ea2d17f
daniil.nesterov <imdnv@yandex.ru>
2023-07-17 13:35:26 +0300

    T2 first version

---
+++ nesterov.daniil/T2/DataStruct.cpp
@@ -0,0 +1,85 @@
+#include "DataStruct.h"
+#include "IOStructs.h"
+#include "IOFmtGuard.h"
+#include <iostream>
+#include <iomanip>
+
+namespace nesterov
+{
+  std::istream &operator>>(std::istream &in, DataStruct &dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    DataStruct input;
+    {
+      using sep = DelimiterIO;
+      using label = LabelIO;
+      using str = StringIO;
+      using hex = HexIO;
+      using rat = RationalIO;
+
+      std::string key;
+      in >> sep{'('} >> sep{':'};
+
+      for (size_t i = 0; i < 3; i++)
+      {
+        in >> label{key};
+        if (key.size() < 4)
+        {
+          return in;
+        }
+        if (key.substr(3) == "1")
+        {
+          in >> hex{input.key1};
+        }
+        else if (key.substr(3) == "2")
+        {
+          in >> rat{input.key2};
+        }
+        else if (key.substr(3) == "3")
+        {
+          in >> str{input.key3};
+        }
+        in >> sep{':'};
+      }
+      in >> sep{')'};
+    }
+    if (in)
+    {
+      dest = input;
+    }
+    return in;
+  }
+
+  std::ostream &operator<<(std::ostream &out, const DataStruct &src)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    IOFmtGuard fmtguard(out);
+    out << "(:";
+    out << "key1 " << "0x" << std::hex << std::uppercase << src.key1 << ":" << std::resetiosflags(std::ios_base::hex);
+    out << "key2 " << "(:N " << src.key2.first << ":D " << src.key2.second << ":):";
+    out << "key3 " << '"' << src.key3 << '"';
+    out << ":)";
+    return out;
+  }
+
+  bool DataStructComparator::operator()(const DataStruct &lhs, const DataStruct &rhs) const
+  {
+    if (lhs.key1 != rhs.key1)
+    {
+      return lhs.key1 < rhs.key1;
+    }
+    if (lhs.key2 != rhs.key2)
+    {
+      return lhs.key2.first / lhs.key2.second < rhs.key2.first / rhs.key2.second;
+    }
+    return lhs.key3 < rhs.key3;
+  }
+}
---
+++ nesterov.daniil/T2/IOStructs.cpp
@@ -0,0 +1,80 @@
+#include "IOStructs.h"
+#include <iostream>
+#include <iomanip>
+
+namespace nesterov
+{
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream &operator>>(std::istream &in, DoubleIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> dest.ref >> DelimiterIO{'d'};
+  }
+
+  std::istream &operator>>(std::istream &in, StringIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+  }
+
+  std::istream &operator>>(std::istream &in, LabelIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    std::string data = "";
+    if ((in >> data) && (data.find("key") != 0))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    dest.exp = data;
+    return in;
+  }
+
+  std::istream &operator>>(std::istream &in, HexIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> std::hex >> dest.ref >> std::resetiosflags(std::ios_base::hex);
+  }
+
+  std::istream &operator>>(std::istream &in, RationalIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> DelimiterIO{'('} >> DelimiterIO{':'} >> DelimiterIO{'N'} >> dest.ref.first
+              >> DelimiterIO{':'} >> DelimiterIO{'D'} >> dest.ref.second >> DelimiterIO{':'}
+              >> DelimiterIO{')'};
+  }
+}
