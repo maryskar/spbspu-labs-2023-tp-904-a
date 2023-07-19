edc23af82c610810e2d30183e6ed2c14ae38e0a6
root <root@DESKTOP-CG1KQKN>
2023-07-19 15:08:31 +0300

    added commands

---
+++ azheganova.faina/T3/commands.h
@@ -0,0 +1,20 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+namespace azheganova
+{
+  void getAreaEven();
+  void getAreaOdd();
+  void getAreaMean();
+  void getAreaNumOfVertexes();
+  void getMaxArea();
+  void getAreaVertexes();
+  void getMinArea();
+  void getMinVerexes();
+  void countEven();
+  void countOdd();
+  void countNumOfVertexes();
+  void getRmecho();
+  void getRightshapes();
+}
+#endif
