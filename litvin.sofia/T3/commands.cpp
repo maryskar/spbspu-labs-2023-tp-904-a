907da7c3c133ab69dc6957562aab90f873b0ce6c
Litvin Sofia <sofialitvin823@gmail.com>
2023-07-24 16:02:35 +0500

    added areLinesIntersected fn

--- litvin.sofia/T3/commands.cpp
+++ litvin.sofia/T3/commands.cpp
@@ -3,6 +3,7 @@
 #include <numeric>
 #include <algorithm>
 #include <functional>
+#include <math.h>
 #include "polygon.hpp"
 namespace litvin
 {
@@ -21,8 +22,7 @@ namespace litvin
     using namespace std::placeholders;
     auto getAreaIfEven = std::bind(calcAreaIf, _1, isEven);
     std::transform(data.cbegin(), data.cend(), std::back_inserter(areas), getAreaIfEven);
-    double res = std::accumulate(areas.cbegin(), areas.cend(), 0.0);
-    return res;
+    return std::accumulate(areas.cbegin(), areas.cend(), 0.0);
   }
   double getOddArea(const v_pol & data)
   {
@@ -37,7 +37,7 @@ namespace litvin
     std::vector< double > areas(data.size());
     std::transform(data.cbegin(), data.cend(), std::back_inserter(areas), calcArea);
     double sum = std::accumulate(areas.cbegin(), areas.cend(), 0.0);
-    return sum / areas.size();
+    return sum / static_cast<double>(areas.size());
   }
   bool hasQuantityOfVertexes(const Polygon & pol, size_t num)
   {
@@ -53,8 +53,7 @@ namespace litvin
     using namespace std::placeholders;
     auto calcAreaNVertexes = std::bind(calcAreaIfNVertexes, _1, number_of_vertexes);
     std::transform(data.cbegin(), data.cend(), std::back_inserter(areas), calcAreaNVertexes);
-    double res = std::accumulate(areas.cbegin(), areas.cend(), 0.0);
-    return res;
+    return std::accumulate(areas.cbegin(), areas.cend(), 0.0);
   }
   double getMaxOrMinAreaOrVertexes(const v_pol & data, bool isTheGreatest, bool isArea)
   {
@@ -77,7 +76,7 @@ namespace litvin
   {
     if (!data.empty())
     {
-      return getMaxOrMinAreaOrVertexes(data, 1, 1);
+      return getMaxOrMinAreaOrVertexes(data, true, true);
     }
     throw std::invalid_argument("For max area must be at least one polygon\n");
   }
@@ -85,7 +84,7 @@ namespace litvin
   {
     if (!data.empty())
     {
-      getMaxOrMinAreaOrVertexes(data, 1, 0);
+      getMaxOrMinAreaOrVertexes(data, true, false);
     }
     throw std::invalid_argument("For max vertexes must be at least one polygon\n");
   }
@@ -93,7 +92,7 @@ namespace litvin
   {
     if (!data.empty())
     {
-      return getMaxOrMinAreaOrVertexes(data, 0, 1);
+      return getMaxOrMinAreaOrVertexes(data, false, true);
     }
     throw std::invalid_argument("For min area must be at least one polygon\n");
   }
@@ -101,14 +100,13 @@ namespace litvin
   {
     if (!data.empty())
     {
-      return getMaxOrMinAreaOrVertexes(data, 0, 0);
+      return getMaxOrMinAreaOrVertexes(data, false, false);
     }
     throw std::invalid_argument("For min vertexes must be at least one polygon\n");
   }
   size_t countIf(bool (* predicate)(const Polygon & pol), const v_pol & data)
   {
-    size_t count = std::count_if(data.cbegin(), data.cend(), predicate);
-    return count;
+    return std::count_if(data.cbegin(), data.cend(), predicate);
   }
   size_t countEven(const v_pol & data)
   {
@@ -120,12 +118,54 @@ namespace litvin
   }
   bool countIfNVertexes(const Polygon & pol, size_t num)
   {
-    return hasQuantityOfVertexes(pol, num) ? true : false;
+    return hasQuantityOfVertexes(pol, num);
   }
   size_t countIfNOfVertexesIs(const v_pol & data, size_t number_of_vertexes)
   {
     using namespace std::placeholders;
     auto hasNVertexes = std::bind(countIfNVertexes, _1, number_of_vertexes);
     return std::count_if(data.cbegin(), data.cend(), hasNVertexes);
+  }
+  bool areLinesIntersected(const Point & l1_p1, const Point & l1_p2, const Point & l2_p1, const Point & l2_p2)
+  {
+    double x1 = l1_p1.x;
+    double y1 = l1_p1.y;
+    double x2 = l1_p2.x;
+    double y2 = l1_p2.y;
+    double x3 = l2_p1.x;
+    double y3 = l2_p1.y;
+    double y4 = l2_p2.y;
+    double x4 = l2_p2.x;
+    double denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    if (denominator == 0) {
+      return false;
+    }
+    double intersectX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denominator;
+    double intersectY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denominator;
+    if (intersectX >= std::min(x1, x2) && intersectX <= std::max(x1, x2) &&
+        intersectY >= std::min(y1, y2) && intersectY <= std::max(y1, y2) &&
+        intersectX >= std::min(x3, x4) && intersectX <= std::max(x3, x4) &&
+        intersectY >= std::min(y3, y4) && intersectY <= std::max(y3, y4)) {
+      return true;
+    }
+    return false;
+  }
+  size_t getNumberOfIntersections(const v_pol & data, const Polygon & pol)
+  {
+    size_t count = 0;
+    if (data.empty())
+    {
+      return count;
+    }
+    return count;
+  }
+  size_t getNumberOfSameFigures(const v_pol & data, const Polygon & pol)
+  {
+    size_t count = 0;
+    if (data.empty())
+    {
+      return count;
+    }
+    return count;
   }
 }
