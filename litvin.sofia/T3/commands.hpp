ffb10221d44854dc6d54f53b36a1dcbee9d480e9
Litvin Sofia <sofialitvin823@gmail.com>
2023-07-26 22:31:48 +0500

    modified typenames

--- litvin.sofia/T3/commands.hpp
+++ litvin.sofia/T3/commands.hpp
@@ -5,14 +5,14 @@
 #include "polygon.hpp"
 namespace litvin
 {
-  using name1 = void (*)(const std::vector< Polygon > & data, std::ostream & out);
-  using name2 = void (*)(const std::vector<Polygon> & data, const Polygon & pol, std::ostream & out);
-  using name3 = void (*)(const std::vector< Polygon > & data, size_t num, std::ostream & out);
+  using signature_type_1 = void (*)(const std::vector< Polygon > & data, std::ostream & out);
+  using signature_type_2 = void (*)(const std::vector<Polygon> & data, const Polygon & pol, std::ostream & out);
+  using signature_type_3 = void (*)(const std::vector< Polygon > & data, size_t num, std::ostream & out);
   struct commandDict
   {
-    std::map< std::string, name1 > dict1;
-    std::map< std::string, name2 > dict2;
-    std::map< std::string, name3 > dict3;
+    std::map< std::string, signature_type_1 > dict1;
+    std::map< std::string, signature_type_2 > dict2;
+    std::map< std::string, signature_type_3 > dict3;
   };
 }
 #endif
