3241f50bd817c355b97e178b3d95b812e70afadd
root <root@DESKTOP-CG1KQKN>
2023-08-03 21:33:23 +0300

    added commands

--- azheganova.faina/T3/commands.cpp
+++ azheganova.faina/T3/commands.cpp
@@ -1,1 +1,20 @@
 #include "commands.h"
+
+azheganova::Commands createCommands()
+{
+  azheganova::Commands res;
+  res.dict_1.insert({ "AREA EVEN", azheganova::getAreaEven });
+  res.dict_1.insert({ "AREA ODD", azheganova::getAreaOdd });
+  res.dict_1.insert({ "AREA MEAN", azheganova::getAreaMean });
+  res.dict_1.insert({ "MAX AREA", azheganova::getMaxArea });
+  res.dict_1.insert({ "MAX VERTEXES", azheganova::getMaxVertexes });
+  res.dict_1.insert({ "MIN AREA", azheganova::getMinArea });
+  res.dict_1.insert({ "MIN VERTEXES", azheganova::getMinVertexes });
+  res.dict_1.insert({ "COUNT EVEN", azheganova::countEven });
+  res.dict_1.insert({ "COUNT ODD", azheganova::countOdd });
+  res.dict_2.insert({ "AREA", azheganova::getAreaNumOfVertexes });
+  res.dict_2.insert({ "COUNT", azheganova::countNumOfVertexes });
+  res.dict_3.insert({ "RMECHO", azheganova::getRmecho });
+  res.dict_3.insert({ "RIGHTSHAPES", azheganova::getRightshapes });
+  return res;
+}
--- azheganova.faina/T3/commands.h
+++ azheganova.faina/T3/commands.h
@@ -1,20 +1,34 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include <iostream>
+#include <map>
+#include "datastructs.h"
 
 namespace azheganova
 {
-  void getAreaEven();
-  void getAreaOdd();
-  void getAreaMean();
-  void getAreaNumOfVertexes();
-  void getMaxArea();
-  void getAreaVertexes();
-  void getMinArea();
-  void getMinVerexes();
-  void countEven();
-  void countOdd();
-  void countNumOfVertexes();
-  void getRmecho();
-  void getRightshapes();
+    using commands1 = void (*) (const std::vector< Polygon > & polygon, std::ostream & out);
+    using commands2 = void (*) (const std::vector< Polygon > & polygon, size_t num, std::ostream & out);
+    using commands3 = void(*)(const std::vector< Polygon > & polygon, std::ostream & out);
+    struct Commands
+    {
+      std::map< std::string, commands1 > dict_1;
+      std::map< std::string, commands2 > dict_2;
+      std::map< std::string, commands3 > dict_3;
+    };
+    Commands createCommands();
+    void getAreaEven(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getAreaOdd(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getAreaMean(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getAreaNumOfVertexes(const std::vector< Polygon > & polygon, size_t num, std::ostream & out);
+    void getMaxArea(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getMaxVertexes(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getMinArea(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getMinVertexes(const std::vector< Polygon > & polygon, std::ostream & out);
+    void countEven(const std::vector< Polygon > & polygon, std::ostream & out);
+    void countOdd(const std::vector< Polygon > & polygon, std::ostream & out);
+    void countNumOfVertexes(const std::vector< Polygon > & polygon, size_t num, std::ostream & out);
+    void getRmecho(const std::vector< Polygon > & polygon, std::ostream & out);
+    void getRightshapes(const std::vector< Polygon > & polygon, std::ostream & out);
 }
+
 #endif
