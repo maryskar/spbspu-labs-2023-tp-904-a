732923b444e7fa9209bdc83b8cbc83e108ab7960
Gulyaev Ilya <ilya.gulyaev2016@gmail.com>
2023-08-04 17:51:29 +0300

    Lab T2 fixed

--- gulyaev.ilya/T2/main.cpp
+++ gulyaev.ilya/T2/main.cpp
@@ -13,7 +13,6 @@ int main()
     using i_iter = std::istream_iterator< gulyaev::DataStruct >;
     std::copy(i_iter(std::cin), i_iter(), std::back_inserter(data));
   }
-  std::cout << "Data:\n";
   std::sort(std::begin(data), std::end(data), gulyaev::isLess);
   std::copy(std::begin(data), std::end(data),
     std::ostream_iterator< gulyaev::DataStruct >(std::cout, "\n"));
