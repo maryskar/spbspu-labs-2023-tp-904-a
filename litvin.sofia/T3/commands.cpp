cc3574e467650ebf53f0a9733a6720daa0346a98
Litvin Sofia <sofialitvin823@gmail.com>
2023-08-05 14:43:45 +0500

    file problems

--- litvin.sofia/T3/commands.cpp
+++ litvin.sofia/T3/commands.cpp
@@ -289,55 +289,7 @@ void litvin::runCommand(const std::vector< Polygon > & data, const command_dicts
     in >> polygon >> DelimiterIO{'\n'};
     if (!in)
     {
-<<<<<<< HEAD
       throw std::invalid_argument("Invalid command parameter");
-=======
-      std::string parameter = " ";
-      in >> parameter;
-      if (!in)
-      {
-        throw std::invalid_argument("Invalid command parameter");
-      }
-      command_name = command_name + " " + parameter;
-    }
-    return command_name;
-  }
-  void command_dicts::executeCommand(const std::string & cmd, const std::vector< Polygon > & data,
-                                     std::ostream & out) const
-  {
-    signature_type_1 function = dict1.at(cmd);
-    function(data, out);
-  }
-  void command_dicts::executeCommand(const std::string & cmd, const std::vector< Polygon > & data,
-                                     const Polygon & pol, std::ostream & out) const
-  {
-    signature_type_2 function = dict2.at(cmd);
-    function(data, pol, out);
-  }
-  void command_dicts::executeCommand(const std::string & cmd, const std::vector< Polygon > & data, size_t num,
-                                     std::ostream & out) const
-  {
-    signature_type_3 function = dict3.at(cmd);
-    function(data, num, out);
-  }
-  void runCommand(const v_pol & data, const cmd_d & dicts, std::string & cmd, std::ostream & out, std::istream & in)
-  {
-    if (cmd == "INTERSECTIONS" || cmd == "SAME")
-    {
-      Polygon polygon;
-      in >> polygon >> DelimiterIO{'\n'};
-      if (!in)
-      {
-        throw std::invalid_argument("Invalid command parameter");
-      }
-      try
-      {
-        dicts.executeCommand(cmd, data, polygon, out);
-        return;
-      }
-      catch (const std::out_of_range & error)
-      {}
->>>>>>> 880f1d3243424a0f8694527c44ff1fbb0edba5bd
     }
     try
     {
