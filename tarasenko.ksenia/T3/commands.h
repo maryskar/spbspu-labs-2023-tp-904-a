a873f12138123162f24212865b370b9ee83abc73
xeniaTa <kseniatarasenko1@gmail.com>
2023-08-06 18:58:41 +0300

    rm get

--- tarasenko.ksenia/T3/commands.h
+++ tarasenko.ksenia/T3/commands.h
@@ -8,7 +8,6 @@
 #include <map>
 #include <functional>
 #include <message.h>
-#include "data_struct.h"
 #include "funcs_for_commands.h"
 
 namespace tarasenko
@@ -64,17 +63,13 @@ namespace tarasenko
      {
        if (findIn(command1, 4))
        {
-         std::function< size_t(const std::vector< Polygon >&) > command;
-         get(command1, command);
-         out << command(data) << "\n";
+         out << type_4.at(command1)(data) << "\n";
        }
        else if (findIn(command1, 5))
        {
          Polygon polygon;
          getFrameRect(in, polygon);
-         std::function< bool(const std::vector< Polygon >&, const Polygon&) > command;
-         get(command1, command);
-         out << (command(data, polygon) ? "<TRUE>" : "<FALSE>") << "\n";
+         out << (type_5.at(command1)(data, polygon) ? "<TRUE>" : "<FALSE>") << "\n";
        }
        else
        {
@@ -85,32 +80,23 @@ namespace tarasenko
          {
            if (findIn(command1 + " " + command2, 3))
            {
-             std::function< double(const std::vector< Polygon >&) > command;
-             get(command1 + " " + command2, command);
-             out << std::fixed << std::setprecision(1) << command(data) << "\n";
+             out << std::fixed << std::setprecision(1) << type_3.at(command1 + " " + command2)(data) << "\n";
            }
            else
            {
-             std::function< size_t(const std::vector< Polygon >&) > command;
-             get(command1 + " " + command2, command);
-             out << command(data) << "\n";
+             out << type_4.at(command1 + " " + command2)(data) << "\n";
            }
          }
          else
          {
+           auto n = std::stoull(command2);
            if (findIn(command1, 1))
            {
-             std::function< double(const std::vector< Polygon >&, const size_t&) > command;
-             get(command1, command);
-             auto n = std::stoull(command2);
-             out << std::fixed << std::setprecision(1) << command(data, n) << "\n";
+             out << std::fixed << std::setprecision(1) << type_1.at(command1)(data, n) << "\n";
            }
            else
            {
-             std::function< size_t(const std::vector< Polygon >&, const size_t&) > command;
-             get(command1, command);
-             auto n = std::stoull(command2);
-             out << command(data, n) << "\n";
+             out << type_2.at(command1)(data, n) << "\n";
            }
          }
        }
@@ -125,76 +111,11 @@ namespace tarasenko
    bool find(const std::string& name)
    {
      auto cond = std::bind(std::equal_to< std::string >{}, _1, name);
-     auto in_any_of = [&](const auto& in_vector) {
+     auto in_any_of = [&](const auto& in_vector)
+     {
        return std::any_of(in_vector.begin(), in_vector.end(), cond);
      };
      return std::any_of(names.begin(), names.end(), in_any_of);
-   }
-
-   bool findIn(const std::string& name, size_t type)
-   {
-     size_t i = type - 1;
-     return std::find(names[i].begin(), names[i].end(), name) != names[i].end();
-   }
-
-   void get(const std::string& key, std::function< double(const std::vector< Polygon >&, const size_t&) >& command)
-   {
-     try
-     {
-       command = type_1.at(key);
-     }
-     catch (const std::out_of_range&)
-     {
-       throw std::out_of_range("Not found");
-     }
-   }
-
-   void get(const std::string& key, std::function< size_t(const std::vector< Polygon >&, const size_t&) >& command)
-   {
-     try
-     {
-       command = type_2.at(key);
-     }
-     catch (const std::out_of_range&)
-     {
-       throw std::out_of_range("Not found");
-     }
-   }
-
-   void get(const std::string& key, std::function< double(const std::vector< Polygon >&) >& command)
-   {
-     try
-     {
-       command = type_3.at(key);
-     }
-     catch (const std::out_of_range&)
-     {
-       throw std::out_of_range("Not found");
-     }
-   }
-
-   void get(const std::string& key, std::function< size_t(const std::vector< Polygon >&) >& command)
-   {
-     try
-     {
-       command = type_4.at(key);
-     }
-     catch (const std::out_of_range&)
-     {
-       throw std::out_of_range("Not found");
-     }
-   }
-
-   void get(const std::string& key, std::function< bool(const std::vector< Polygon >&, const Polygon&) >& command)
-   {
-     try
-     {
-       command = type_5.at(key);
-     }
-     catch (const std::out_of_range&)
-     {
-       throw std::out_of_range("Not found");
-     }
    }
 
   private:
@@ -204,6 +125,13 @@ namespace tarasenko
    std::map< std::string, std::function< double(const std::vector< Polygon >&) > > type_3;
    std::map< std::string, std::function< size_t(const std::vector< Polygon >&) > > type_4;
    std::map< std::string, std::function< bool(const std::vector< Polygon >&, const Polygon&) > > type_5;
+   bool findIn(const std::string& name, size_t type);
   };
+
+  bool tarasenko::Commands::findIn(const std::string& name, size_t type)
+  {
+    size_t i = type - 1;
+    return std::find(names[i].begin(), names[i].end(), name) != names[i].end();
+  }
 }
 #endif
