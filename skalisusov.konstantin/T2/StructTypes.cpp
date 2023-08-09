4e09823eb2ceacf62a7eb053ae333b27ee219744
Kikos1337 <skalisusov@inbox.ru>
2023-08-09 07:28:01 +0300

    fix StructTypes.cpp

--- skalisusov.konstantin/T2/StructTypes.cpp
+++ skalisusov.konstantin/T2/StructTypes.cpp
@@ -1,68 +1,89 @@
 #include "StructTypes.h"
+#include <iomanip>
 
-namespace skalisusov
+std::istream & skalisusov::operator>>(std::istream &in, LabelIO &&dest)
 {
-  std::istream &operator>>(std::istream &in, DelimiterIO &&dest)
+  std::istream::sentry CheckSentry(in);
+  if(!CheckSentry)
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-      return in;
-    }
-    char c = '0';
-    in >> c;
-    if (in && (c != dest.exp))
-    {
-      in.setstate(std::ios::failbit);
-    }
     return in;
   }
-  std::istream &operator>>(std::istream &in, CoverIO &&dest)
+  std::string str = "";
+  std::getline(in, str, 'y');
+  str += 'y';
+  if (in && (str != dest.label_))
   {
-    std::istream::sentry CheckSentry(in);
-    if(!CheckSentry)
-    {
-      return in;
-    }
-    std::string cover = "";
-    if((in >> StringIO{cover}) && (cover != dest.cover_))
-    {
-      in.setstate(std::ios::failbit);
-    }
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+std::istream & skalisusov::operator>>(std::istream &in, DelimiterIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
     return in;
   }
-  std::istream &operator>>(std::istream &in, DoubleLiteralFormatIO &&dest)
+  char delim = '0';
+  in >> delim;
+  if (in && (delim != dest.exp))
   {
-    std::istream::sentry CheckSentry(in);
-    if (!CheckSentry)
-    {
-      return in;
-    }
-    return in >> dest.dublit_ >> DelimiterIO{ 'd' };
+    in.setstate(std::ios::failbit);
   }
-  std::istream &operator>>(std::istream &in, DoubleSciencificFormatI &&dest)
+  return in;
+}
+std::istream & skalisusov::operator>>(std::istream &in, StringIO &&dest)
+{
+  std::istream::sentry CheckSentry(in);
+  if(!CheckSentry)
   {
-    std::istream::sentry CheckSentry(in);
-    if(!CheckSentry)
-    {
-      return in;
-    }
-    return in >> std::scientific >> dest.dubscienI_;
+    return in;
   }
-  std::ostream &operator<<(std::ostream &out, DoubleSciencificFormatO dest)
+  return std::getline(in >> DelimiterIO{'"'},dest.str_,'"');
+}
+std::istream & skalisusov::operator>>(std::istream &in, DoubleLiteralFormatIO &&dest)
+{
+  std::istream::sentry CheckSentry(in);
+  if (!CheckSentry)
   {
-    std::ostream::sentry CheckSentry(out);
-    if(!CheckSentry)
-    {
-      return out;
-    }
-    double number = dest.dubscienO_;
-    int tenDegree = 0;
+    return in;
+  }
+  return in >> dest.dublit_ >> DelimiterIO{ 'd' };
+}
+std::istream & skalisusov::operator>>(std::istream &in, DoubleSciencificFormatI &&dest)
+{
+  std::istream::sentry CheckSentry(in);
+  if(!CheckSentry)
+  {
+    return in;
+  }
+  in >> std::scientific >> dest.dubscienI_;
+  if (in && (dest.dubscienI_ == 0))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+std::ostream & skalisusov::operator<<(std::ostream &out, const DoubleSciencificFormatO dest)
+{
+  std::ostream::sentry CheckSentry(out);
+  if(!CheckSentry)
+  {
+    return out;
+  }
+  double number = dest.dubscienO_;
+  int tenDegree = 0;
+  if(number == 0)
+  {
+    tenDegree = 0;
+  }
+  else
+  {
     if(number < 1)
     {
-      while(number < 10)
+      while(number < 1)
       {
-        if(number > 1)
+        if(number >= 1)
         {
           break;
         }
@@ -75,21 +96,12 @@ namespace skalisusov
     }
     else
     {
-      while(number > 10)
+      while(number >= 10)
       {
         number /=10;
         tenDegree ++;
       }
     }
-    return out << std::fixed << std::setprecision(1)  << number << (tenDegree > 0 ? "e+" : "e") << tenDegree;
   }
-  std::istream &operator>>(std::istream &in, skalisusov::StringIO &&dest)
-  {
-    std::istream::sentry CheckSentry(in);
-    if(!CheckSentry)
-    {
-      return in;
-    }
-    return std::getline(in >> DelimiterIO{'"'},dest.str_,'"');
-  }
+  return out << std::fixed << std::setprecision(1)  << number << (tenDegree > 0 ? "e+" : "e") << tenDegree;
 }
