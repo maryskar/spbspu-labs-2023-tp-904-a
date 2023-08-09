178a995676311c85e258feb216efc7b8fba4108f
Kikos1337 <skalisusov@inbox.ru>
2023-08-09 07:26:43 +0300

    fix StructTypes.h

--- skalisusov.konstantin/T2/StructTypes.h
+++ skalisusov.konstantin/T2/StructTypes.h
@@ -1,22 +1,21 @@
 #ifndef SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
 #define SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
-#include <iostream>
-#include <sstream>
 #include <string>
-#include <cassert>
-#include <iomanip>
 
 namespace skalisusov
 {
-  struct CoverIO
+  struct LabelIO
   {
-    std::string cover_;
+    std::string label_;
   };
   struct DelimiterIO
   {
     char exp;
   };
-
+  struct StringIO
+  {
+    std::string &str_;
+  };
   struct DoubleLiteralFormatIO
   {
     double &dublit_;
@@ -29,16 +28,13 @@ namespace skalisusov
   {
     const double &dubscienO_;
   };
-  struct StringIO
-  {
-    std::string &str_;
-  };
-  std::istream &operator>>(std::istream &in, CoverIO &&dest);
-  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
-  std::istream &operator>>(std::istream &in, DoubleLiteralFormatIO &&dest);
-  std::istream &operator>>(std::istream &in, DoubleSciencificFormatI &&dest);
-  std::ostream &operator<<(std::ostream &out, const DoubleSciencificFormatO dest);
-  std::istream &operator>>(std::istream &in, StringIO &&dest);
+
+  std::istream & operator>>(std::istream &in, LabelIO &&dest);
+  std::istream & operator>>(std::istream &in, DelimiterIO &&dest);
+  std::istream & operator>>(std::istream &in, DoubleLiteralFormatIO &&dest);
+  std::istream & operator>>(std::istream &in, DoubleSciencificFormatI &&dest);
+  std::ostream & operator<<(std::ostream &out, const DoubleSciencificFormatO dest);
+  std::istream & operator>>(std::istream &in, StringIO &&dest);
 }
 
 #endif //SPBSPU_LABS_2023_TP_904_A_STRUCTTYPES_H
