c1e4b426fedcf5a0e388521b9d4556f0a53462b6
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-08-11 00:39:05 +0300

    correction in main

--- chulkov.leontii/T2/main.cpp
+++ chulkov.leontii/T2/main.cpp
@@ -1,24 +1,25 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <string>
 #include <vector>
-#include <limits>
+
 #include "data.h"
 
 int main() {
   std::vector< chulkov::Data > resl;
   chulkov::Comparator comp;
-  using inpIter = std::istream_iterator< chulkov::Data >;
+  using istIter = std::istream_iterator< chulkov::Data >;
   while (!std::cin.eof()) {
-    std::copy_n(inpIter(std::cin), 3, std::back_inserter(resl));
+    std::copy_n(istIter(std::cin), 3, std::back_inserter(resl));
     if (std::cin.fail()) {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
-  using outIter = std::ostream_iterator< chulkov::Data >;
+  using ostIter = std::ostream_iterator< chulkov::Data >;
   std::sort(std::begin(resl), std::end(resl), comp);
-  std::copy(std::begin(resl), std::end(resl), outIter(std::cout, "\n"));
+  std::copy(std::begin(resl), std::end(resl), ostIter(std::cout, "\n"));
   return 0;
 }
