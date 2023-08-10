5893d03f584b2e1fb764b00e3c2d82b46361318e
aksenovEA <aksenov200477@gmail.com>
2023-08-10 21:20:50 +0300

    add data type and iterators

--- aksenov.egor/T2/main.cpp
+++ aksenov.egor/T2/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <vector>
+#include <iterator>
+#include "DataStruct.h"
 int main()
 {
+  std::vector< aksenov::DataStruct > data;
+  using inputIter = std::istream_iterator< aksenov::DataStruct >;
+  using outputIter = std::ostream_iterator< aksenov::DataStruct >;
   std::cout << "Init commit";
 }
