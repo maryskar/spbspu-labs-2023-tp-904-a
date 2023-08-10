c18e4b35cc54ec28d959d427f019de82e1671444
aksenovEA <aksenov200477@gmail.com>
2023-08-10 21:06:35 +0300

    DataStruct.h update

--- aksenov.egor/T2/DataStruct.h
+++ aksenov.egor/T2/DataStruct.h
@@ -1,6 +1,13 @@
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
+#include <string>
+#include <complex>
 namespace aksenov {
-
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
 }
 #endif
