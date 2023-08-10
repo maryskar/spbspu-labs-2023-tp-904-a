88c533e3e2c51b3df8b38ca1c6ccff72bad1c534
aksenovEA <aksenov200477@gmail.com>
2023-08-10 21:51:10 +0300

    trying to make comparator as a function

---
+++ aksenov.egor/T2/Comparator.cpp
@@ -0,0 +1,16 @@
+#include "Comparator.h"
+bool aksenov::comparator(const aksenov::DataStruct &lhs, const aksenov::DataStruct &rhs)
+{
+  if (lhs.key1 == rhs.key1)
+  {
+    if (lhs.key2 == rhs.key2)
+    {
+      return lhs.key3.length() < rhs.key3.length();
+    }
+    return abs(lhs.key2) < abs(rhs.key2);
+  }
+  else
+  {
+    return abs(lhs.key1) < abs(rhs.key2);
+  }
+}
---
+++ aksenov.egor/T2/Comparator.h
@@ -0,0 +1,8 @@
+#ifndef T2_COMPARATOR_H
+#define T2_COMPARATOR_H
+#include "DataStruct.h"
+namespace aksenov
+{
+  bool comparator(const DataStruct& lhs, const DataStruct& rhs);
+}
+#endif //T2_COMPARATOR_H
