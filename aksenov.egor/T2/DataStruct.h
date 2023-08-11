cd9cee9835c73e3f80cd5a9d6421076b18f12f1d
aksenovEA <aksenov200477@gmail.com>
2023-08-11 18:49:29 +0300

    add operators

--- aksenov.egor/T2/DataStruct.h
+++ aksenov.egor/T2/DataStruct.h
@@ -10,7 +10,32 @@ namespace aksenov {
     std::complex< double > key2;
     std::string key3;
   };
-  std::istream &operator>>(std::istream &in, DataStruct &dst);
-  std::ostream &operator<<(std::ostream &out, const DataStruct &dst);
+
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct DoubleIO
+  {
+    double &ref;
+  };
+
+  struct StringIO
+  {
+    std::string &ref;
+  };
+
+  struct LabelIO
+  {
+    std::string exp;
+  };
+
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+  std::istream &operator>>(std::istream &in, DoubleIO &&dest);
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
+  std::istream &operator>>(std::istream &in, LabelIO &&dest);
+  std::istream &operator>>(std::istream &in, DataStruct &dest);
+  std::ostream &operator<<(std::ostream &out, const DataStruct &dest);
 }
 #endif
