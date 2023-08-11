a3a76c15cb111dff705434d12b0238253f005b47
Kikos1337 <skalisusov@inbox.ru>
2023-08-12 02:02:08 +0300

    fix  DoubleSciencificFormatO

--- skalisusov.konstantin/T2/StructTypes.h
+++ skalisusov.konstantin/T2/StructTypes.h
@@ -33,7 +33,7 @@ namespace skalisusov
   std::istream & operator>>(std::istream &in, DelimiterIO &&dest);
   std::istream & operator>>(std::istream &in, DoubleLiteralFormatIO &&dest);
   std::istream & operator>>(std::istream &in, DoubleSciencificFormatI &&dest);
-  std::ostream & operator<<(std::ostream &out, const DoubleSciencificFormatO dest);
+  std::ostream & operator<<(std::ostream &out, const DoubleSciencificFormatO &&dest);
   std::istream & operator>>(std::istream &in, StringIO &&dest);
 }
 
