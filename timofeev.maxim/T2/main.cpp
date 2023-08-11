7c482ea3959aee33a2ac7fab48c185219a0e92be
Caspio <maks.timofeev9834@gmail.com>
2023-08-11 23:53:37 +0400

    delete useless parts of template from main

--- timofeev.maxim/T2/main.cpp
+++ timofeev.maxim/T2/main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <cassert>
 #include <iterator>
 #include <vector>
-#include <iomanip>
+#include "comparator.h"
+#include "DataStruct.h"
 int main()
 {
-  using nspace::Data;
 
   std::vector< Data > data;
-  std::istringstream iss("{ \"key1\": 1.0d, \"key2\": \"Let madness release you\" }");
+  std::copy( std::istream_iterator< Data >(iss), std::istream_iterator< Data >(), std::back_inserter(data));
 
-  std::copy(
-      std::istream_iterator< Data >(iss),
-      std::istream_iterator< Data >(),
-      std::back_inserter(data)
-  );
-
-  std::cout << "Data:\n";
-  std::copy(
-      std::begin(data),
-      std::end(data),
-      std::ostream_iterator< Data >(std::cout, "\n")
-  );
+  std::copy( std::begin(data), std::end(data), std::ostream_iterator< Data >(std::cout, "\n") );
 
   return 0;
 }
