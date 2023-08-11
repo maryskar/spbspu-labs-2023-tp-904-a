5365356337be49a6437238297b7eef2fbdb3b80f
Caspio <maks.timofeev9834@gmail.com>
2023-08-11 23:55:27 +0400

    add DelimetrIO

--- timofeev.maxim/T2/DataStruct.cpp
+++ timofeev.maxim/T2/DataStruct.cpp
@@ -6,3 +6,21 @@ std::istream &operator>>(std::istream &in, StringIO &&dest);
 std::istream &operator>>(std::istream &in, LabelIO &&dest);
 std::istream &operator>>(std::istream &in, DataStruct &dest);
 std::ostream &operator<<(std::ostream &out, const DataStruct &dest);*/
+namespace timofeev
+{
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+      return in;
+    }
+}
