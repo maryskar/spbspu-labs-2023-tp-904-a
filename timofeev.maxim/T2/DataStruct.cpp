946501466bf37d2002708722e1cabe7e50aa8d7a
Caspio <maks.timofeev9834@gmail.com>
2023-08-12 00:08:51 +0400

    add Input and Output from template with libraries

--- timofeev.maxim/T2/DataStruct.cpp
+++ timofeev.maxim/T2/DataStruct.cpp
@@ -1,4 +1,6 @@
 #include "DataStruct.h"
+#include <iomanip>
+#include "IofGuard.h"
 /*std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
 std::istream &operator>>(std::istream &in, DoubleIO &&dest);
 std::istream &operator>>(std::istream &in, ComplexIO &&dest);
@@ -34,4 +36,50 @@ namespace timofeev
     return std::getline(in >> DelimiterIO {'"'}, dest.ref, '"');
   }
 
+
+
+
+
+
+
+    std::istream &operator>>(std::istream &in, Data &dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        Data input;
+        {
+            using sep = DelimiterIO;
+            using label = LabelIO;
+            using dbl = DoubleIO;
+            using str = StringIO;
+            in >> sep{ '{' };
+            in >> label{ "key1" } >> sep{ ':' } >> dbl{ input.key1 };
+            in >> sep{ ',' };
+            in >> label{ "key2" } >> sep{ ':' } >> str{ input.key2 };
+            in >> sep{ '}' };
+        }
+        if (in)
+        {
+            dest = input;
+        }
+        return in;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const Data &src)
+    {
+        std::ostream::sentry sentry(out);
+        if (!sentry)
+        {
+            return out;
+        }
+        iofmtguard fmtguard(out);
+        out << "{ ";
+        out << "\"key1\": " << std::fixed << std::setprecision(1) << src.key1 << "d, ";
+        out << "\"key2\": " << src.key2;
+        out << " }";
+        return out;
+    }
 }
