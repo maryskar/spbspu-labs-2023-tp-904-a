e82032a7ff1c380bcf82309162dba272bbf8fc13
Kikos1337 <skalisusov@inbox.ru>
2023-08-12 17:32:45 +0300

    fix sciencific format 1

--- skalisusov.konstantin/T2/DataStruct.cpp
+++ skalisusov.konstantin/T2/DataStruct.cpp
@@ -56,7 +56,7 @@ std::ostream & skalisusov::operator<<(std::ostream &out, const DataStruct &dest)
   out << "(";
   out << ":key1 " << std::fixed << std::setprecision(1) << dest.key1_ << 'd';
   out << ":key2 " << DoubleSciencificFormatO{dest.key2_};
-  out << ":key3 " << std::quoted(dest.key3_);
+  out << ":key3 " << '"' << dest.key3_ << '"';
   out << ":)";
   return out;
 }
