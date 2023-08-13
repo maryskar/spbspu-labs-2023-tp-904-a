67ec8a0c892ad0517f574801e0e2b6afcc095434
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-08-14 01:41:55 +0300

    fix in main

--- chulkov.leontii/T2/main.cpp
+++ chulkov.leontii/T2/main.cpp
@@ -11,7 +11,7 @@ int main() {
   chulkov::Comparator comp;
   using istIter = std::istream_iterator< chulkov::Data >;
   while (!std::cin.eof()) {
-    std::copy_n(istIter(std::cin), 3, std::back_inserter(resl));
+    std::copy(istIter(std::cin), istIter(), std::back_inserter(resl));
     if (std::cin.fail()) {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
