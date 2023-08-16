32ef6360fef4566abb9b6dd45aef25a19555a56a
Caspio <maks.timofeev9834@gmail.com>
2023-08-17 00:38:01 +0400

    polygon.h add operator>>

---
+++ timofeev.maxim/T3/polygon.cpp
@@ -0,0 +1,15 @@
+#include "polygon.h"
+namespace timofeev
+{
+  std::istream &operator>>(std::istream &in, Polygon & dest)
+  {
+
+  }
+
+  std::istream &operator>>(std::istream &in, Point &dest)
+  {
+
+  }
+
+}
+
