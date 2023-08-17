050333a8259434d9159b724f3b93af93f6bdcaeb
Caspio <maks.timofeev9834@gmail.com>
2023-08-17 17:49:29 +0400

    add Dictionary

---
+++ timofeev.maxim/T3/dictionary.cpp
@@ -0,0 +1,14 @@
+#include "dictionary.h"
+#include
+namespace timofeev
+{
+  void dictionary(dict& command)
+  {
+    command["AREA"] = ;
+    command["MAX"] = ;
+    command["MIN"] = ;
+    command["CUNT"] = ;
+    command["RECTS"] = ;
+    command["RIGHTSHAPES"] = ;
+  }
+}
