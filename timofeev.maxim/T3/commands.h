0d7010fd9f4fd0cbc997a7a771bc98160efb8f3c
Caspio <maks.timofeev9834@gmail.com>
2023-08-17 23:31:25 +0400

    dictionary header update

--- timofeev.maxim/T3/commands.h
+++ timofeev.maxim/T3/commands.h
@@ -1,12 +1,15 @@
 #ifndef T3_COMMANDS_H
 #define T3_COMMANDS_H
+#include <istream>
+#include <vector>
+#include "polygon.h"
 namespace timofeev
 {
-  void doAreaCommands( );
-  void doMaxCommands( );
-  void doMinCommands( );
-  void doCountCommands( );
-  void doRectsCommand( );
-  void doRSCommand( );
+  void doAreaCommands(std::istream &in, const std::vector< Polygon >& res);
+  void doMaxCommands(std::istream &in, const std::vector< Polygon >& res);
+  void doMinCommands(std::istream &in, const std::vector< Polygon >& res);
+  void doCountCommands(std::istream &in, const std::vector< Polygon >& res);
+  void doRectsCommand(std::istream &in, const std::vector< Polygon >& res);
+  void doRSCommand(std::istream &in, const std::vector< Polygon >& res);
 }
 #endif //T3_COMMANDS_H
