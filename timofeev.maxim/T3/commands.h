16643d9ff0d47427a7225bef62ec457f52a3125b
Caspio <maks.timofeev9834@gmail.com>
2023-08-17 22:39:27 +0400

    add commands

---
+++ timofeev.maxim/T3/commands.h
@@ -0,0 +1,12 @@
+#ifndef T3_COMMANDS_H
+#define T3_COMMANDS_H
+namespace timofeev
+{
+  void doAreaCommands( );
+  void doMaxCommands( );
+  void doMinCommands( );
+  void doCountCommands( );
+  void doRectsCommand( );
+  void doRSCommand( );
+}
+#endif //T3_COMMANDS_H
