e2c43be7526ddbb6a7cdb3e1d54ae4be421fbd52
Caspio <maks.timofeev9834@gmail.com>
2023-08-17 23:03:36 +0400

    add doAreaCommands

--- timofeev.maxim/T3/commands.cpp
+++ timofeev.maxim/T3/commands.cpp
@@ -1,5 +1,35 @@
 #include "commands.h"
+#include <istream>
+#include <exception>
+#include "helpFunctions.h"
+#include "../common/IofGuard.h"
 namespace timofeev
 {
+  void doAreaCommands (std::istream &in, )
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return;
+    }
+    std::string secPart;
+    in >> secPart;
+    if (is_number(secPart))
+    {
+      //do smth with vertexes
+    }
+    else if (secPart == "EVEN")
+    {
+      doEven();
+    }
+    else if (secPart == "ODD")
+    {
+      doOdd();
+    }
+    else
+    {
+      throw std::logic_error("Error");
+    }
+  }
 
 }
