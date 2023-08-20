4932278628c318cbc2a5ac448befaa29eab8a23f
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-08-20 23:36:14 +0300

    added implementation of operator>> for Delimiter

--- zhuravlev.ivan/T2/IO.cpp
+++ zhuravlev.ivan/T2/IO.cpp
@@ -2,6 +2,18 @@
 #include "iofmtguard.hpp"
 #include <iomanip>
 
-namespace zhuravlev
+std::istream& operator>>(std::istream& in, zhuravlev::DelimiterIO&& dest)
 {
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  char c = '0';
+  in >> c;
+  if (in && c != dest.expected)
+  {
+    in.setstate(std::ios::failbit());
+  }
+  return in;
 }
