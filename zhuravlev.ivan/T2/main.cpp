6b7a98466a59c609c41ca1c9d369eed087216b3c
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-08-20 18:50:01 +0300

    addded loop for input and check stream status

--- zhuravlev.ivan/T2/main.cpp
+++ zhuravlev.ivan/T2/main.cpp
@@ -13,5 +13,15 @@ int main()
   using in_data_iter = std::istream_iterator< data_t >;
   using out_data_iter = std::ostream_iterator< data_t >;
   std::deque< data_t > data;
+  do
+  {
+    if(std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(max_stream_size, '\n');
+    }
+   std::copy(in_data_iter(std::cin), out_data_iter(), std::back_inserter(data));
+  }
+  while(!std::cin.eof());
   return 0;
 }
