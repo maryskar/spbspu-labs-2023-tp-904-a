6fa59943e8bd647f656e5e12dcb7a6d3b36996f2
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-08-20 18:44:59 +0300

    added using to make clear and deque

--- zhuravlev.ivan/T2/main.cpp
+++ zhuravlev.ivan/T2/main.cpp
@@ -2,9 +2,16 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <deque>
 #include "data_struct.hpp"
 
 int main()
 {
+  const std::streamsize max_stream_size = std::numeric_limits< std::streamsize >::max();
+  using data_t = zhuravlev::DataStruct;
+  using in_data_iter = std::istream_iterator< data_t >;
+  using out_data_iter = std::ostream_iterator< data_t >;
+  std::deque< data_t > data;
   return 0;
 }
