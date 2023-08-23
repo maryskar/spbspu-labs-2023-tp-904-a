2e16a55dff23fa2f97de83cc1ac5ed8de224eb58
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2023-08-23 21:49:38 +0300

    trying

--- zhuravlev.ivan/T2/IO.cpp
+++ zhuravlev.ivan/T2/IO.cpp
@@ -4,65 +4,65 @@
 #include <string>
 #include <bitset>
 
-using namespace zhuravlev;
-
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+namespace zhuravlev
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
   {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && dest.expected)
+    {
+      in.setstate(std::ios::failbit);
+    }
     return in;
   }
-  char c = '0';
-  in >> c;
-  if (in && c != dest.expected)
-  {
-    in.setstate(std::ios::failbit);
-  }
-  return in;
-}
 
-std::istream& operator>>(std::istream& in, HexIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
+  std::istream& operator>>(std::istream& in, HexIO&& dest)
   {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> DelimiterIO{ '0' } >> DelimiterIO{ 'x' } >> std::hex >> dest.ref;
+  }
+
+  std::istream& operator>>(std::istream& in, BinIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    std::bitset< sizeof(unsigned long long ) > value(dest.ref);
+    return in >> DelimiterIO{ '0' } >> DelimiterIO{ 'b' } >> value;
+  }
+  std::istream& operator>>(std::istream& in, StringIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+  }
+  std::istream &operator>>(std::istream &in, LabelIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    std::string data = "";
+    if ((in >> StringIO{ data }) && (data != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
     return in;
   }
-  return in >> DelimiterIO{ '0' } >> DelimiterIO{ 'x' } >> std::hex >> dest.ref;
-}
-
-std::istream& operator>>(std::istream& in, BinIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  std::bitset< sizeof(unsigned long long) > value(dest.ref);
-  return in >> DelimiterIO{ '0' } >>  DelimiterIO{ 'b' } >>value;
-}
-
-std::istream& operator>>(std::istream& in, StringIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
-}
-
-std::istream& operator>>(std::istream& in, LabelIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  for (size_t i = 0; i < dest.exp.length(); i++)
-  {
-    in >> dest.exp[i];
-  }
-  return in;
 }
--- zhuravlev.ivan/T2/data_struct.cpp
+++ zhuravlev.ivan/T2/data_struct.cpp
@@ -1,18 +1,17 @@
-#include "data_struct.hpp"
 #include <iomanip>
 #include <vector>
 #include <iterator>
 #include "iofmtguard.hpp"
 #include "IO.hpp"
 
-using namespace zhuravlev;
-bool compareData(const DataStruct& lhs, const DataStruct& rhs)
+namespace zhuravlev
 {
-  return (lhs.key1 != rhs.key1) ? (lhs.key1 < rhs.key1): (lhs.key2 != rhs.key2) ? (lhs.key2 < rhs.key2): (lhs.key3.size() < rhs.key3.size());
-}
 
-std::istream& operator>>(std::istream& in, DataStruct& dest)
-{
+  bool compareData(const DataStruct& lhs, const DataStruct& rhs)
+  {
+    return (lhs.key1 != rhs.key1) ? (lhs.key1 < rhs.key1): (lhs.key2 != rhs.key2) ? (lhs.key2 < rhs.key2): (lhs.key3.size() < rhs.key3.size());
+  }
+  std::istream& operator>>(std::istream& in, DataStruct& dest)
   {
     std::istream::sentry sentry(in);
     if (!sentry)
@@ -23,15 +22,16 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
     {
       using sep = DelimiterIO;
       using label = LabelIO;
-      using bin = BinIO;
-      using hex = HexIO;
+      using binll = BinIO;
+      using hexll = HexIO;
       using str = StringIO;
       in >> sep{ '(' };
-      in >> label{ "key1" } >> sep{ ':' } >> BinIO{ input.key1 };
-      in >> sep{ ',' };
-      in >> label{ "key2" } >> sep{ ':' } >> HexIO{ input.key2 };
-      in >> sep{ ',' };
-      in >> label{ "key3" } >> sep{ ':' } >> StringIO{ input.key3 };
+      in >> sep{ ':' } >> label{ "key1" } >> binll{ input.key1 };
+      in >> sep{ ' ' };
+      in >> sep{ ':' } >> label{ "key2" } >> hexll{ input.key2 };
+      in >> sep{ ' ' };
+      in >> sep{ ':' } >> label{ "key3" } >> str{ input.key3 };
+      in >> sep{ ' ' };
       in >> sep{ ')' };
     }
     if (in)
@@ -40,20 +40,4 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
     }
     return in;
   }
-}
-
-std::ostream& operator<<(std::ostream& out, const DataStruct& src)
-{
-  std::ostream::sentry sentry(out);
-  if (!sentry)
-  {
-    return out;
-  }
-  iofmtguard fmtguard(out);
-  out << "(";
-  out << ":key1 0b" << src.key1;
-  out << ":key2 0x" << std::hex << std::uppercase << src.key2;
-  out << ":key3 \"" << src.key3;
-  out << "\":)";
-  return out;
 }
