f6594a553bdc24867b06da294102ac959269ca9a
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-25 12:19:48 +0500

    Add operator>>() with DoubleIO and CharIO

--- fesenko.ivan/T2/iotypes.cpp
+++ fesenko.ivan/T2/iotypes.cpp
@@ -14,3 +14,22 @@ std::istream &fesenko::operator>>(std::istream &in, DelimiterIO &&dest)
   }
   return in;
 }
+
+std::istream &fesenko::operator>>(std::istream &in, DoubleIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  return in >> std::scientific >> dest.ref;
+}
+
+std::istream &fesenko::operator>>(std::istream &in, CharIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  char c = '\'';
+  return in >> DelimiterIO{ c } >> dest.ref >> DelimiterIO{ c };
+}
