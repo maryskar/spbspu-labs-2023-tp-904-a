a74f7ae29c38dc9e71deb579f1d1d4ecd098a8c1
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-26 18:33:05 +0500

    Fixes in iotypes

--- fesenko.ivan/T2/iotypes.cpp
+++ fesenko.ivan/T2/iotypes.cpp
@@ -30,7 +30,8 @@ std::istream &fesenko::operator>>(std::istream &in, CharIO &&dest)
   if (!sentry) {
     return in;
   }
-  char c = '\'';
+  int quoteCode = 39;
+  char c = quoteCode;
   return in >> DelimiterIO{ c } >> dest.ref >> DelimiterIO{ c };
 }
 
@@ -40,7 +41,8 @@ std::istream &fesenko::operator>>(std::istream &in, StringIO &&dest)
   if (!sentry) {
     return in;
   }
-  char c ='\"';
+  int doubleQuoteCode = 34;
+  char c = doubleQuoteCode;
   return std::getline(in >> DelimiterIO{ c }, dest.ref, c);
 }
 
@@ -50,9 +52,8 @@ std::istream &fesenko::operator>>(std::istream &in, LabelIO &&dest)
   if (!sentry) {
     return in;
   }
-  std::string data = "";
-  if ((in >> StringIO{ data }) && (data != dest.exp)) {
-    in.setstate(std::ios::failbit);
+  for (size_t i = 0; i < dest.exp.length(); i++) {
+    in >> DelimiterIO{ dest.exp[i] };
   }
   return in;
 }
