26fd2b02fa9a391095cb21f294411a4fa96dc204
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-27 13:45:25 +0500

    Add number of arguments checker

--- fesenko.ivan/T3/main.cpp
+++ fesenko.ivan/T3/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-
+  if (argc != 2)
+  {
+    std::cerr << "Invalid number of arguments\n";
+    return 2;
+  }
+  std::ifstream in(argv[1]);
+  if (!in)
+  {
+    std::cerr << "Can`t open the file\n";
+    return 2;
+  }
 }
