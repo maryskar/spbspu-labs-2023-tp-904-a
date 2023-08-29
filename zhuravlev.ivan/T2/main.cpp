9b9b6a7713720e006dbe7f2f4537f9b437350980
zzzhura <i.zhurravlev@gmail.com>
2023-08-29 14:29:32 +0300

    changed input construction

--- zhuravlev.ivan/T2/main.cpp
+++ zhuravlev.ivan/T2/main.cpp
@@ -12,16 +12,15 @@ int main()
   using in_data_iter = std::istream_iterator< zhuravlev::DataStruct >;
   using out_data_iter = std::ostream_iterator< zhuravlev::DataStruct >;
   std::deque< zhuravlev::DataStruct > data;
-  do
+  while (!std::cin.eof())
   {
-    if(std::cin.fail())
+    std::copy(in_data_iter(std::cin), in_data_iter(), std::back_inserter(data));
+    if (!std::cin)
     {
       std::cin.clear();
       std::cin.ignore(max_stream_size, '\n');
     }
-    std::copy(in_data_iter(std::cin), in_data_iter(), std::back_inserter(data));
   }
-  while(!std::cin.eof());
   std::sort(data.begin(), data.end(), zhuravlev::compareData);
   std::copy(std::begin(data), std::end(data), out_data_iter(std::cout, "\n"));
   return 0;
