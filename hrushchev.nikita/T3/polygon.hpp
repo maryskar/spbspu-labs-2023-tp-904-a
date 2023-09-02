c25b0673e1aa455d97513bbd733cbce94e25d586
Shuzamada <shuzamada@inbox.ru>
2023-09-02 03:07:45 +0300

    start operator>>

---
+++ hrushchev.nikita/T3/polygon.hpp
@@ -0,0 +1,24 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <cstddef>
+#include <vector>
+#include <istream>
+
+namespace hrushchev
+{
+  struct Point
+  {
+    size_t x_;
+    size_t y_;
+  };
+  std::istream & operator>>(std::istream & in, Point & dest);
+
+  struct Polygon
+  {
+    std::vector< Point > points_;
+  };
+  std::istream & operator>>(std::istream & in, Polygon & dest);
+}
+
+#endif
