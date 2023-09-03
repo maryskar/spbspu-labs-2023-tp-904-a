5453087b161b04b95ff2647365feadb2e05953c6
Ivan31122003 <Ivan31122003@mail.ru>
2023-09-03 22:15:33 +0300

    fix acc

--- shestakov.ivan/T2/data_struct.cpp
+++ shestakov.ivan/T2/data_struct.cpp
@@ -70,8 +70,7 @@ namespace shestakov
       return out;
     }
     iofmtguard fmtguard(out);
-    out << "(";
-    out << ":key1 " << src.key1;
+    out << "(:key1 \'" << src.key1 << "\'";
     out << ":key2 #c" << std::fixed << std::setprecision(1) << src.key2;
     out << ":key3 \"" << src.key3 << "\":)";
     return out;
