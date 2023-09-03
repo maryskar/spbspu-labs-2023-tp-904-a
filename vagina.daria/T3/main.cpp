9d499e9f403480ba623e2b98598c58559436c13f
Vagina Daria <dvagina@inbox.ru>
2023-09-04 00:19:11 +0300

    try this

--- vagina.daria/T3/main.cpp
+++ vagina.daria/T3/main.cpp
@@ -20,7 +20,7 @@ int main(int argNum, char* argv[])
     std::cerr << "Error: file could not be opened." << '\n';
     return 1;
   }
-  constexpr auto max_size = std::numeric_limits< std::streamsize >::max();
+  /*constexpr auto max_size = std::numeric_limits< std::streamsize >::max();
   std::vector< vagina::Polygon > polygon;
   using istream_it = std::istream_iterator< vagina::Polygon >;
   while (!in.eof())
@@ -49,6 +49,42 @@ int main(int argNum, char* argv[])
     catch (const std::runtime_error & e)
     {
       break;
+    }
+    if(!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }*/
+  using inIter = std::istream_iterator< vagina::Polygon >;
+  std::vector< vagina::Polygon > data;
+  while (!in.eof())
+  {
+    std::copy(inIter(in), inIter(), std::back_inserter(data));
+    if (!in)
+    {
+      in.clear();
+      in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  vagina::DictionaryOfCommands comms;
+  while (!std::cin.eof())
+  {
+    try
+    {
+      std::string command;
+      command = vagina::readCommand(std::cin);
+      vagina::doCommand(command, comms, data, std::cin, std::cout);
+    }
+    catch (const std::runtime_error &)
+    {
+      break;
+    }
+    catch (const std::logic_error &)
+    {
+      vagina::messageInvalidCommand(std::cout);
+      std::cout << '\n';
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
     if(!std::cin)
     {
