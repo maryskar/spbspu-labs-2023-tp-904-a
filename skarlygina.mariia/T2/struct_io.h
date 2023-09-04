cd154f4ab463a984caa438529b04a3f02f466c6f
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 20:24:55 +0300

    io structure

--- skarlygina.mariia/T2/struct_io.h
+++ skarlygina.mariia/T2/struct_io.h
@@ -1,6 +1,32 @@
 #ifndef STRUCT_IO_H
 #define STRUCT_IO_H
-
 #include <string>
+
+struct delimiter_sep
+{
+  char sep;
+};
+
+struct delimiter_io
+{
+  char exp;
+};
+
+struct string_io
+{
+  std::string& ref;
+};
+
+struct DBL_sciIO
+{
+  double& ref;
+};
+
+struct ULL_hexIO
+{
+  unsigned long long& ref;
+};
+
+
 
 #endif
