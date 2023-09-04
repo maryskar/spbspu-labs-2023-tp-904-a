1b8430824334e9c568128c2e0b7a763973e910c4
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 22:08:35 +0300

    base iofmt guard class

---
+++ skarlygina.mariia/T2/iofmt_guard.h
@@ -0,0 +1,32 @@
+#ifndef IOFMT_GUARD_H
+#define IOFMT_GUARD_H
+#include <ios>
+
+class Iofmtguard
+{
+public:
+  Iofmtguard(std::basic_ios< char >& st);
+  ~Iofmtguard();
+private:
+  std::basic_ios< char >& st_;
+  char fill_;
+  std::streamsize precision_;
+  std::basic_ios< char >::fmtflags fmt_;
+};
+
+Iofmtguard::Iofmtguard(std::basic_ios< char >& st) :
+  st_(st),
+  fill_(st.fill()),
+  precision_(st.precision()),
+  fmt_(st.flags())
+{}
+
+Iofmtguard::~Iofmtguard()
+{
+  st_.fill(fill_);
+  st_.precision(precision_);
+  st_.flags(fmt_);
+}
+
+#endif // !IOFMT_GUARD_H
+
