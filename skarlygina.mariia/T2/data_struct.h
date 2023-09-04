a33f557ab134f2e586a7f6dc90ac13c88bbc3882
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 19:21:19 +0300

    structure h complete

--- skarlygina.mariia/T2/data_struct.h
+++ skarlygina.mariia/T2/data_struct.h
@@ -1,18 +1,17 @@
 #ifndef DATA_STRUCT_H
 #define DATA_STRUCT_H
-#include <complex>
 #include <string>
 
 namespace structure {
   struct DataStruct {
-    long long key1;
-    std::complex< double > key2;
+    double key1;
+    long long key2;
     std::string key3;
   };
   std::ostream& operator<<(std::ostream& os, const DataStruct& data);
   std::istream& operator>>(std::istream& in, DataStruct& src);
-  size_t getDataStructSize();
+  bool operator<(const DataStruct& left, const DataStruct& right);
 }
 
-#endif // !DATA_STRUCT_H
+#endif
 
