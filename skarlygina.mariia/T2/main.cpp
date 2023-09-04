75aafa0ed5f14053cc3eb0f679644fcb42856cf9
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 20:45:37 +0300

    ttt

--- skarlygina.mariia/T2/main.cpp
+++ skarlygina.mariia/T2/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 #include "data_struct.h"
 
 int main()
 {
   std::vector< structure::DataStruct > data;
-  std::cout << "hi";
+  using i_iter = std::istream_iterator< structure::DataStruct >;
+  using o_iter = std::ostream_iterator< structure::DataStruct >;
+  while (!std::cin.eof())
+  {
+    std::copy(i_iter(std::cin), i_iter(), std::back_inserter(data));
+    if (std::cin.rdstate() == std::ios::failbit)
+    {
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cin.clear();
+    }
+  }
 }
