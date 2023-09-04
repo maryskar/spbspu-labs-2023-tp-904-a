6c8ff0a17f1c7af8d0d1dd6d804a937252a94aa0
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 18:57:20 +0300

    fixed merge

--- skarlygina.mariia/T2/data_struct.h
+++ skarlygina.mariia/T2/data_struct.h
@@ -2,10 +2,17 @@
 #define DATA_STRUCT_H
 #include <complex>
 #include <string>
-struct DataStruct {
-  long long key1;
-  std::complex< double > key2;
-  std::string key3;
-};
+
+namespace structure {
+  struct DataStruct {
+    long long key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+  std::ostream& operator<<(std::ostream& os, const DataStruct& data);
+  std::istream& operator>>(std::istream& in, DataStruct& src);
+  size_t getDataStructSize();
+}
+
 #endif // !DATA_STRUCT_H
 
