78e586b0c082c206a1ad61c76b833c4ce66d352a
natavdeeva <scarletname@yandex.ru>
2023-09-04 14:15:49 +0300

    created DataStruct

---
+++ avdeeva.natalya/T2/DataStruct.h
@@ -0,0 +1,13 @@
+#ifndef SPBSPU_LABS_2023_TP_904_A_DATASTRUCT_H
+#define SPBSPU_LABS_2023_TP_904_A_DATASTRUCT_H
+#include <iostream>
+#include <string>
+namespace avdeeva
+{
+  struct DataStruct {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+#endif
