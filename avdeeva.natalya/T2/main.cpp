b5b3b47998cbbfd02c0273efa3e5e3a797e7717f
natavdeeva <scarletname@yandex.ru>
2023-09-05 00:26:03 +0300

    output using iterator std::sort and std::copy

--- avdeeva.natalya/T2/main.cpp
+++ avdeeva.natalya/T2/main.cpp
@@ -1,10 +1,15 @@
 #include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <algorithm>
 #include "DataStruct.h"
 int main()
 {
   std::deque< avdeeva::DataStruct > data;
   avdeeva::Comparator comparator;
   using inIter = std::istream_iterator< avdeeva::DataStruct >;
+  using outIter = std::ostream_iterator< avdeeva::DataStruct >;
   while (!std::cin.eof())
   {
     std::copy(inIter(std::cin), inIter(), std::back_inserter(data));
@@ -14,5 +19,7 @@ int main()
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
+  std::sort(data.begin(), data.end(), comparator);
+  std::copy(std::begin(data), std::end(data), outIter(std::cout, "\n"));
   return 0;
 }
