3af4f46670b1517cedf126e8e83f75e98a5d1e72
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-04 18:44:41 +0300

    data struct

---
+++ skarlygina.mariia/T2/data_struct.h
@@ -0,0 +1,11 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+#include <complex>
+#include <string>
+struct DataStruct {
+  long long key1;
+  std::complex< double > key2;
+  std::string key3;
+};
+#endif // !DATA_STRUCT_H
+
