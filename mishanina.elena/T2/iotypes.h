10d0134e19904dd6ad19c59723bf14e430a51078
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-05 11:38:30 +0300

    implementation iotypes

--- mishanina.elena/T2/iotypes.h
+++ mishanina.elena/T2/iotypes.h
@@ -1,6 +1,32 @@
 #ifndef IOTYPES_H
 #define IOTYPES_H
-
-
-
+#include <string>
+namespace mishanina
+{
+  struct DelimiterIO
+  {
+    char exp;
+  };
+  struct LongLongIO
+  {
+    long long &ref;
+  };
+  struct UnsignedLongLongIO
+  {
+    unsigned long long &ref;
+  };
+  struct StringIO
+  {
+    std::string &ref;
+  };
+  struct LabelIO
+  {
+    std::string exp;
+  };
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+  std::istream &operator>>(std::istream &in, LongLongIO &&dest);
+  std::istream &operator>>(std::istream &in, UnsignedLongLongIO &&dest);
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
+  std::istream &operator>>(std::istream &in, LabelIO &&dest);
+}
 #endif
