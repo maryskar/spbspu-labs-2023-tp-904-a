36954458e117c02718465d25972f95794037a783
Кончев Максим Денисович <konchev.md@edu.spbstu.ru>
2023-09-07 22:27:42 +0300

    fill DataStruct

--- konchev.maksim/T2/DataStruct.cpp
+++ konchev.maksim/T2/DataStruct.cpp
@@ -1,1 +1,17 @@
 #include "DataStruct.h"
+std::ifstream &operator>>(std::ifstream &in, DataStruct &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+}
+std::ofstream &operator<<(std::ofstream &out, const DataStruct &data)
+{
+  std::ofstream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+}
--- konchev.maksim/T2/DataStruct.h
+++ konchev.maksim/T2/DataStruct.h
@@ -1,3 +1,13 @@
 #ifndef KONCHEV_MAKSIM_DATASTRUCT_H
 #define KONCHEV_MAKSIM_DATASTRUCT_H
-#endif //KONCHEV_MAKSIM_DATASTRUCT_H
+#include <string>
+#include <fstream>
+struct DataStruct
+{
+  long long key1;
+  unsigned long long key2;
+  std::string key3;
+};
+std::ifstream &operator>>(std::ifstream &in, DataStruct &&data);
+std::ofstream &operator<<(std::ofstream &out, const DataStruct &data);
+#endif
