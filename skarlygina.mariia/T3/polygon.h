83abb79c8c2c72429b0f138a3552c5e949ba31a9
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-08 01:33:53 +0300

    first structures

---
+++ skarlygina.mariia/T3/polygon.h
@@ -0,0 +1,17 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+#include <vector>
+
+namespace skarlygina
+{
+  struct Point
+  {
+    size_t x, y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+}
+#endif
