be5af9e282d546ccd666fdb424d2228c58369823
Кончев Максим Денисович <konchev.md@edu.spbstu.ru>
2023-09-07 23:22:53 +0300

    realization iotypes

--- konchev.maksim/common/iotypes.cpp
+++ konchev.maksim/common/iotypes.cpp
@@ -1,1 +1,59 @@
 #include "iotypes.h"
+using namespace konchev;
+std::ifstream &operator>>(std::ifstream &in, delimiter &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  char s = '0';
+  in >> s;
+  if (in && s != data.exp)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+std::ifstream &operator>>(std::ifstream &in, label &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  for (std::size_t i = 0; i < data.str.length(); i++)
+  {
+    in >> delimiter{data.str[i]};
+  }
+  return in;
+}
+std::ifstream &operator>>(std::ifstream &in, LongLong &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  in >> data.ref >> delimiter{'l'} >> delimiter{'l'};
+  return in;
+}
+std::ifstream &operator>>(std::ifstream &in, uLongLong &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  in >> std::hex >> data.ref;
+  return in;
+}
+std::ifstream &operator>>(std::ifstream &in, String &&data)
+{
+  std::ifstream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return std::getline(in >> delimiter{'"'}, data.ref, '"');
+}
--- konchev.maksim/common/iotypes.h
+++ konchev.maksim/common/iotypes.h
@@ -1,4 +1,33 @@
 #ifndef KONCHEV_MAKSIM_IOTYPES_H
 #define KONCHEV_MAKSIM_IOTYPES_H
-
+#include <string>
+#include <fstream>
+namespace konchev
+{
+  struct delimiter
+  {
+    char exp;
+  };
+  struct label
+  {
+    std::string str;
+  };
+  struct LongLong
+  {
+    long long &ref;
+  };
+  struct uLongLong
+  {
+    unsigned long long &ref;
+  };
+  struct String
+  {
+    std::string &ref;
+  };
+  std::ifstream &operator>>(std::ifstream &in, delimiter &&data);
+  std::ifstream &operator>>(std::ifstream &in, label &&data);
+  std::ifstream &operator>>(std::ifstream &in, LongLong &&data);
+  std::ifstream &operator>>(std::ifstream &in, uLongLong &&data);
+  std::ifstream &operator>>(std::ifstream &in, String &&data);
+}
 #endif
