06001f02c770c2a04ff26c3dede7732d0798e942
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-09 01:26:10 +0300

    opeartors point new polygon

--- skarlygina.mariia/T3/point.cpp
+++ skarlygina.mariia/T3/point.cpp
@@ -1,2 +1,32 @@
 #include "point.h"
-#include "struct_io.h"
+#include <iostream>
+#include <struct_io.h>
+
+std::istream& skarlygina::operator>>(std::istream& in, Point& point)
+{
+  std::istream::sentry sent(in);
+  if (!sent)
+  {
+    return in;
+  }
+  int x = 0;
+  int y = 0;
+  in >> delimiter_IO_t{'('} >> x >> delimiter_IO_t{';'} >> y >> delimiter_IO_t{')'};
+  if (in)
+  {
+    point = {x, y};
+  }
+  return in;
+}
+
+std::ostream& skarlygina::operator<<(std::ostream& out, const Point& point)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  out << '(' << point.x << ';' << point.y << ')';
+  return out;
+}
+
--- skarlygina.mariia/T3/point.h
+++ skarlygina.mariia/T3/point.h
@@ -1,5 +1,6 @@
 #ifndef POINT_H
 #define POINT_H
+#include <ios>
 namespace skarlygina
 {
   struct Point
