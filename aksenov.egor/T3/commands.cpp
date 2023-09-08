b89f5df7ea21ae9a3c657404e94a0ababbd76900
aksenovEA <aksenov200477@gmail.com>
2023-09-09 01:43:21 +0300

    doMinVertexes()

--- aksenov.egor/T3/commands.cpp
+++ aksenov.egor/T3/commands.cpp
@@ -124,6 +124,19 @@ namespace aksenov
     std::copy(minElementIter, minElementIter + 1, out);
   }
 
+  void doMinVertexes(const std::vector< Polygon > &pol)
+  {
+    std::vector< Polygon > pols = pol;
+    std::vector< size_t > amountsOfVertexes;
+    for (const Polygon &polygon : pols)
+    {
+      amountsOfVertexes.push_back(polygon.points.size());
+    }
+    auto minVertIter = std::min_element(amountsOfVertexes.begin(), amountsOfVertexes.end());
+    auto out = std::ostream_iterator< size_t > (std::cout, "\n");
+    std::copy(minVertIter, minVertIter + 1, out);
+  }
+
   void doMin(std::istream &inp, const std::vector< Polygon > &pol)
   {
     std::string command = "";
