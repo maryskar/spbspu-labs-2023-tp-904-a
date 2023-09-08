26574b5745793d9f0233393af2a6c75f314cd58f
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-08 08:34:46 +0300

    fix operator

--- mishanina.elena/T2/DataStruct.cpp
+++ mishanina.elena/T2/DataStruct.cpp
@@ -2,86 +2,88 @@
 #include <iomanip>
 #include <iotypes.h>
 #include <iofmtguard.h>
-
-std::ostream &mishanina::printLL(std::ostream &out, const long long &data)
-{
-  out << ":key1 " << static_cast< long long >(data) << "ll";
-  return out;
-}
-
-std::ostream &mishanina::printULL(std::ostream &out, const unsigned long long &data)
-{
-  out << ":key2 " << static_cast< unsigned long long >(data) << "ull";
-  return out;
-}
-
-std::istream &mishanina::operator>>(std::istream &in, DataStruct &data)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
+//#include "../common/iotypes.h"
+//#include "../common/iofmtguard.h"
+  std::ostream &mishanina::operator<<(std::ostream &out, const long long &data)
   {
-    return in;
+    out << ":key1 ";
+    out.operator<<(data) << "ll";
+    return out;
   }
-  DataStruct dataStruct;
-  std::size_t number = 0;
-  in >> DelimiterIO{'('} >> DelimiterIO{':'};
-  for (size_t i = 0; i < 3; i++)
+  std::ostream &mishanina::operator<<(std::ostream &out, const unsigned long long &data)
   {
-    in >> LabelIO{"key"};
-    in >> number;
-    if (number == 1)
-    {
-      in >> LongLongIO{dataStruct.key1} >> DelimiterIO{':'};
-    }
-    else if (number == 2)
-    {
-      in >> UnsignedLongLongIO{dataStruct.key2} >> DelimiterIO{':'};
-    }
-    else if (number == 3)
-    {
-      in >> StringIO{dataStruct.key3} >> DelimiterIO{':'};
-    }
-    if (!in)
+    out << ":key2 ";
+    out.operator<<(data) << "ull";
+    return out;
+  }
+  std::istream &mishanina::operator>>(std::istream &in, DataStruct &data)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
     {
       return in;
     }
+    DataStruct dataStruct;
+    std::size_t number = 0;
+    in >> DelimiterIO{'('} >> DelimiterIO{':'};
+    for (size_t i = 0; i < 3; i++)
+    {
+      in >> LabelIO{"key"};
+      in >> number;
+      if (number == 1)
+      {
+        in >> LongLongIO{dataStruct.key1} >> DelimiterIO{':'};
+      }
+      else if (number == 2)
+      {
+        in >> UnsignedLongLongIO{dataStruct.key2} >> DelimiterIO{':'};
+      }
+      else if (number == 3)
+      {
+        in >> StringIO{dataStruct.key3} >> DelimiterIO{':'};
+      }
+      if (!in)
+      {
+        return in;
+      }
+    }
+    in >> DelimiterIO{')'};
+    if (in)
+    {
+      data = dataStruct;
+    }
+    return in;
   }
-  in >> DelimiterIO{')'};
-  if (in)
+  std::ostream &mishanina::operator<<(std::ostream &out, const DataStruct &data)
   {
-    data = dataStruct;
-  }
-  return in;
-}
-
-std::ostream &mishanina::operator<<(std::ostream &out, const DataStruct &data)
-{
-  using namespace mishanina;
-  std::ostream::sentry sentry(out);
-  if (!sentry)
-  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    out << '(';
+//    out << data.key1;
+//    out << data.key2;
+    mishanina::operator<<(out, data.key1);
+    mishanina::operator<<(out, data.key2);
+//    printLL(out, data.key1);
+//    printULL(out, data.key2);
+    out << ":key3 " << std::quoted(data.key3, '"');
+    out << ":)";
     return out;
   }
-  out << '(';
-  printLL(out,data.key1);
-  printULL(out,data.key2);
-  out << ":key3 " << std::quoted(data.key3, '"');
-  out << ":)";
-  return out;
-}
-
-bool mishanina::Comparator::operator()(const DataStruct &left, const DataStruct &right)
-{
-  if (left.key1 != right.key1)
+  bool mishanina::Comparator::operator()(const DataStruct &left, const DataStruct &right)
   {
-    return left.key1 < right.key1;
+    if (left.key1 != right.key1)
+    {
+      return left.key1 < right.key1;
+    }
+    else if (left.key2 != right.key2)
+    {
+      return left.key2 < right.key2;
+    }
+    else
+    {
+      return left.key3.length() < right.key3.length();
+    }
   }
-  else if (left.key2 != right.key2)
-  {
-    return left.key2 < right.key2;
-  }
-  else
-  {
-    return left.key3.length() < right.key3.length();
-  }
-}
