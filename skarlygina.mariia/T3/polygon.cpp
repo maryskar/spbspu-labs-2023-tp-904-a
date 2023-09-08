33d720a277fffe8f77653eb84bd1d390435eac69
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-09 01:40:17 +0300

    add operators

--- skarlygina.mariia/T3/polygon.cpp
+++ skarlygina.mariia/T3/polygon.cpp
@@ -1,1 +1,31 @@
 #include "polygon.h"
+#include <iostream>
+
+bool skarlygina::operator==(const Polygon& lhs, const Polygon& rhs)
+{
+  return (lhs.points == rhs.points);
+}
+
+bool skarlygina::operator!=(const Polygon& lhs, const Polygon& rhs)
+{
+  return !(lhs == rhs);
+}
+
+std::istream& skarlygina::operator>>(std::istream& in, Polygon& polygon)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+}
+
+std::ostream& skarlygina::operator<<(std::ostream& out, const Polygon& polygon)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+}
+
