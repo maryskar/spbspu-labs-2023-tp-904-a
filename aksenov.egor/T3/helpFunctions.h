1f862ac7227284cecf9a98f5808a38f8c4977024
aksenovEA <aksenov200477@gmail.com>
2023-09-09 01:16:42 +0300

    add helpFunctions file, also a signature of getArea function

---
+++ aksenov.egor/T3/helpFunctions.h
@@ -0,0 +1,9 @@
+#ifndef HELPFUNCTIONS_H
+#define HELPFUNCTIONS_H
+#include <vector>
+#include "polygon.h"
+namespace aksenov
+{
+  std::vector< double > getArea(const std::vector<Polygon> &pol);
+}
+#endif
