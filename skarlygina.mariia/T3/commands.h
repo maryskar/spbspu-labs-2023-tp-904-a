2722993ce8ea038dd5874524845a60ff176a588b
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-08 16:06:30 +0300

    fix

--- skarlygina.mariia/T3/commands.h
+++ skarlygina.mariia/T3/commands.h
@@ -1,5 +1,6 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include "polygon.h"
 
 namespace skarlygina
 {
--- skarlygina.mariia/T3/main.cpp
+++ skarlygina.mariia/T3/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <exception>
+#include <fstream>
 #include "polygon.h"
 #include "commands.h"
 
 int main(int argc, char** argv)
 {
-  std::vector< skarlygina::Polygon > poligons;
+  if (argc != 2)
+  {
+    std::cerr << "Invalid number of arguments";
+    return 1;
+  }
+  std::ifstream fin(argv[1]);
+  if (!fin)
+  {
+    std::cerr << "File is not open";
+    return 1;
+  }
+  std::vector< skarlygina::Polygon > polygons;
   using vector_of_polygons = std::vector< skarlygina::Polygon >;
+  using iterator_istream = std::istream_iterator< skarlygina::Polygon >;
+  while (!fin.eof())
+  {
+    std::copy(iterator_istream(fin), iterator_istream(), std::back_inserter(polygons));
+    if (!fin)
+    {
+      fin.clear();
+      fin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
 
 }
