f000e457eba156b0bcf87e2fdc61cab627c0c477
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-09-08 21:39:04 +0300

    add operator>>

--- chulkov.leontii/Т3/polygon.cpp
+++ chulkov.leontii/Т3/polygon.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
+#include <iterator>
+#include <streamsguard.h>
 #include <iostruct.h>
 
 namespace chulkov {
@@ -14,4 +16,21 @@ namespace chulkov {
     in >> DelimiterIO{'('} >> point.x >> DelimiterIO{';'} >> point.y >> DelimiterIO{')'};
     return in;
   }
+
+  std::istream& operator>>(std::istream& in, Polygon& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    dest.points.clear();
+    size_t inpNum = 0;
+    in >> inpNum;
+    if (inpNum < 3) {
+      in.setstate(std::ios::failbit);
+    }
+    std::copy_n(std::istream_iterator< Point >(in), inpNum, std::back_inserter(dest.points));
+    return in;
+  }
+
 }
