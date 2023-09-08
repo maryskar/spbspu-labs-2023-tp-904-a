5328b1b7b2d5be0f46bb04cc9a1c46168873de03
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-09 00:40:38 +0300

    point and polygon

---
+++ skarlygina.mariia/T3/point.h
@@ -0,0 +1,12 @@
+#ifndef POINT_H
+#define POINT_H
+namespace skarlygina
+{
+  struct Point
+  {
+    int x, y;
+  };
+  std::istream& operator>>(std::istream&, Point&);
+  std::ostream& operator<<(std::ostream&, const Point&);
+}
+#endif
--- skarlygina.mariia/T3/polygon.h
+++ skarlygina.mariia/T3/polygon.h
@@ -2,21 +2,15 @@
 #define POLYGON_H
 #include <vector>
 #include <ios>
+#include "point.h"
 
 namespace skarlygina
 {
-  struct Point
-  {
-    size_t x, y;
-  };
   struct Polygon
   {
     std::vector< Point > points;
   };
-
-  std::istream& operator>>(std::istream&, Point&);
   std::istream& operator>>(std::istream&, Polygon&);
-  std::ostream& operator<<(std::ostream&, const Point&);
   std::ostream& operator<<(std::ostream&, const Polygon&);
 }
 
