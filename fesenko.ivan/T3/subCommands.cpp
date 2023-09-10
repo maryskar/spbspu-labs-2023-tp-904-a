b02d8c02a4009982a3908edc0a2414c0849bbb4f
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-10 16:06:47 +0300

    Add area_mean

--- fesenko.ivan/T3/subCommands.cpp
+++ fesenko.ivan/T3/subCommands.cpp
@@ -17,3 +17,11 @@ fesenko::ReturnType fesenko::area_even(data_t &data)
   std::copy_if(data.cbegin(), data.cend(), std::back_inserter(temp), isEven);
   return ReturnType(std::accumulate(temp.cbegin(), temp.cend(), 0.0, calcArea));
 }
+
+fesenko::ReturnType fesenko::area_mean(data_t &data)
+{
+  if (data.empty()) {
+    throw std::logic_error("Divide by zero");
+  }
+  return ReturnType(std::accumulate(data.cbegin(), data.cend(), 0.0, calcArea) / data.size());
+}
