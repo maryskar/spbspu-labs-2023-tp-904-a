91614144673eff10ef68892703800dc4b6674c02
KiraGelaz <kiragelaz@yandex.ru>
2023-09-11 22:36:03 +0300

    fix overloaded op

--- kryuchkova.kira/T3/polygon.cpp
+++ kryuchkova.kira/T3/polygon.cpp
@@ -7,18 +7,19 @@ namespace kryuchkova
   std::istream & operator>>( std::istream & in, Polygon & polygon)
   {
     std::istream::sentry sentry(in);
-    Point point;
-    in >> point;
-    polygon.points.push_back(point);
-    while (in)
+    if (!sentry)
     {
-      if (!sentry)
-      {
-        return in;
-      }
-      in >> Delimiter{' '} >> point;
-      polygon.points.push_back(point);
+      return in;
     }
+    size_t size = 0;
+    in >> size;
+    if (!in || size < 3)
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    polygon.points.clear();
+    std::copy_n(std::istream_iterator< Point >(in), size, std::back_inserter(polygon.points));
     return in;
   }
 }
