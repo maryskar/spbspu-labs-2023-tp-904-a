4bf84bc0b2bb7f6ff10f3a91fe8ea6fac1d57cba
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-11 15:16:40 +0300

    first command fixed with pre commands and help commands list

--- skarlygina.mariia/T3/commands.cpp
+++ skarlygina.mariia/T3/commands.cpp
@@ -1,1 +1,39 @@
 #include "commands.h"
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <iofmt_guard.h>
+#include "help_commands.h"
+
+void skarlygina::getArea(const std::vector< Polygon >& polys, std::istream& in, std::ostream& out);
+void skarlygina::getMax(const std::vector< Polygon >& polys, std::istream& in, std::ostream& out)
+{
+  if (polys.empty()) {
+    throw std::invalid_argument("There are no polygons");
+  }
+
+  std::string command = "";
+  in >> command;
+
+  iofmtguard guard(out);
+  out << std::fixed << std::setprecision(1);
+  if (command == "AREA")
+  {
+    out << maxArea(polys) << '\n';
+  }
+  else if (command == "VERTEXES")
+  {
+    out << maxVertexes(polys) << '\n';
+  }
+  else
+  {
+    throw std::invalid_argument("False command");
+  }
+}
+void skarlygina::getMin(const std::vector< Polygon >&, std::istream&, std::ostream&);
+void skarlygina::countFigures(const std::vector< Polygon >&, std::istream&, std::ostream&);
+void skarlygina::findPerms(const std::vector< Polygon >&, std::istream&, std::ostream&);
+void skarlygina::findSame(const std::vector< Polygon >&, std::istream&, std::ostream&);
+std::ostream& skarlygina::printInvalidCommand(std::ostream&);
---
+++ skarlygina.mariia/T3/help_commands.cpp
@@ -0,0 +1,61 @@
+#include "help_commands.h"
+#include <stdexcept>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include "iofmt_guard.h"
+
+namespace skarlygina
+{
+  struct PolygonsArea
+  {
+    PolygonsArea(skarlygina::Point point):
+      prev(point)
+    {}
+    double operator()(double area, const skarlygina::Point& current)
+    {
+      area += static_cast< double >(prev.x) * current.y;
+      area -= static_cast< double >(prev.y) * current.x;
+      prev = current;
+      return area;
+    }
+  private:
+    skarlygina::Point prev;
+  };
+}
+
+double findAreaPoly(const skarlygina::Polygon& polys, double area = 0.0)
+{
+  std::vector< skarlygina::Point > poly = polys.points;
+  skarlygina::PolygonsArea area_polygon_count(*poly.rbegin());
+  area += std::abs(std::accumulate(poly.begin(), poly.end(), 0.0, area_polygon_count)) / 2.0;
+  return area;
+}
+
+bool compareArea(const skarlygina::Polygon& left, const skarlygina::Polygon& right)
+{
+  return skarlygina::findAreaPoly(left) < skarlygina::findAreaPoly(right);
+}
+
+bool compareVert(const skarlygina::Polygon& left, const skarlygina::Polygon& right)
+{
+  return left.points.size() < right.points.size();
+}
+
+size_t getSize(const skarlygina::Polygon poly)
+{
+  return poly.points.size();
+}
+
+
+double skarlygina::maxArea(const std::vector< Polygon >& polys)
+{
+  auto element_max_area = std::max_element(polys.begin(), polys.end(), compareArea);
+  return findAreaPoly(*element_max_area);
+}
+
+size_t skarlygina::maxVertexes(const std::vector< Polygon >& polys)
+{
+  auto element_max_vert = std::max_element(polys.begin(), polys.end(), compareVert);
+  return getSize(*element_max_vert);
+}
