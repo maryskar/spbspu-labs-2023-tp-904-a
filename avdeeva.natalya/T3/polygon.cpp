8ceff9f430171a877b4500877741c6686d41257c
natavdeeva <scarletname@yandex.ru>
2023-09-12 00:04:22 +0300

    operator>> for Point

--- avdeeva.natalya/T3/polygon.cpp
+++ avdeeva.natalya/T3/polygon.cpp
@@ -1,5 +1,17 @@
 #include "polygon.h"
-std::istream & operator>>(std::istream & in, const Point & rhs)
+#include "iostructs.h.h"
+std::istream & avdeeva::operator>>(std::istream & in, const Point & rhs)
 {
-
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  Point input {0, 0};
+  in >> DelimiterIO{'('} >> input.x >> DelimiterIO{';'} >> input.y >> DelimiterIO{')'};
+  if (in)
+  {
+    rhs = input;
+  }
+  return in;
 }
