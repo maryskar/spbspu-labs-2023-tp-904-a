2b04d6a39a6366d7aa0b8814151bce38895de5a4
ArsDi <abstractlevl@gmail.com>
2023-09-12 17:37:03 +0300

    init

---
+++ dmitriev.arseny/T2/datasruct.h
@@ -0,0 +1,20 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <iostream>
+
+namespace dmitriev
+{
+
+	struct DataStruct
+	{
+		double key1;
+		long long key2;
+
+		std::string key3;
+	};
+
+
+}
+
+#endif
