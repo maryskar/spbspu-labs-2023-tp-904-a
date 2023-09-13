4f2b11e295c4b872fcfb087e62f85479c190ed97
ArsDi <abstractlevl@gmail.com>
2023-09-13 10:19:24 +0300

    main dasic realiz to understand

--- dmitriev.arseny/T2/main.cpp
+++ dmitriev.arseny/T2/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+
 
 int main()
 {
+  using data_t = int;
+  std::vector< data_t > vec;
+
+  std::copy(std::istream_iterator< data_t >(std::cin), std::istream_iterator< data_t >(), std::back_inserter(vec));
+  std::copy(vec.cbegin(), vec.cend(), std::ostream_iterator< data_t >(std::cout, "_"));
 
   return 0;
 }
