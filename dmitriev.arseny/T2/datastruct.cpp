f433a45e4a2706718240d8d88ac8082059fe7558
ArsDi <abstractlevl@gmail.com>
2023-09-13 12:38:46 +0300

    dataStruct first try inp realize

--- dmitriev.arseny/T2/datastruct.cpp
+++ dmitriev.arseny/T2/datastruct.cpp
@@ -1,4 +1,5 @@
 #include "datastruct.h"
+#include "customtypes.h"
 
 bool dmitriev::compareDataStructures(const DataStruct& lhs, const DataStruct& rhs)
 {
@@ -18,7 +19,42 @@ bool dmitriev::compareDataStructures(const DataStruct& lhs, const DataStruct& rh
 
 std::istream& dmitriev::operator>>(std::istream& inp, DataStruct& data)
 {
-  // TODO: вставьте здесь оператор return
+  std::istream::sentry sentry(inp);
+  if (!sentry)
+  {
+    return inp;
+  }
+
+
+  DataStruct input;
+  inp >> SeparatorIO{'('} >> SeparatorIO{':'};
+  for (size_t i = 0; i < 3; i++)
+  {
+    size_t number = 0;
+    inp >> LableIO{"key"} >> number;
+
+    switch (number)
+    {
+    case 1:
+      inp >> DoubleI{input.key1} >> SeparatorIO{':'};
+    case 2:
+      inp >> LongLongI{input.key2} >> SeparatorIO{':'};
+    case 3:
+      inp >> StringI{input.key3} >> SeparatorIO{':'};
+    default:
+      inp.setstate(std::ios::failbit);
+    }
+    if (!sentry)
+    {
+      return inp;
+    }
+  }
+  if (inp)
+  {
+    data = input;
+  }
+
+  return inp;
 }
 
 std::ostream& dmitriev::operator<<(std::ostream& inp, const DataStruct& data)
