de14c57d5294fe3fa2a0dc85ea446fbe1ece13fa
natavdeeva <scarletname@yandex.ru>
2023-09-14 22:44:16 +0300

    added comparators

--- avdeeva.natalya/T3/helpFunctions.cpp
+++ avdeeva.natalya/T3/helpFunctions.cpp
@@ -11,3 +11,11 @@ bool avdeeva::isNumOfVerts(const Polygon & pol, size_t num)
 {
   return size(pol) == num;
 }
+bool avdeeva::compArea(const Polygon & lhs, const Polygon & rhs)
+{
+  return (getArea(lhs) < getArea(rhs));
+}
+bool avdeeva::compSize(const Polygon & lhs, const Polygon & rhs)
+{
+  return (lhs.points.size() < rhs.points.size());
+}
--- avdeeva.natalya/T3/helpFunctions.h
+++ avdeeva.natalya/T3/helpFunctions.h
@@ -6,5 +6,7 @@ namespace avdeeva
   bool isOdd(const Polygon & pol);
   bool isEven(const Polygon & pol);
   bool isNumOfVerts(const Polygon & pol, size_t num);
+  bool compArea(const Polygon & lhs, const Polygon & rhs);
+  bool compSize(const Polygon & lhs, const Polygon & rhs);
 }
 #endif
