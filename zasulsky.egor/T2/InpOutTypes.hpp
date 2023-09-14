45eecb14dc8466bf2669267892cf5d1d5276016e
Засульский Егор <3756035@gmail.com>
2023-09-14 22:09:23 +0300

    adding new structs

---
+++ zasulsky.egor/T2/InpOutTypes.hpp
@@ -0,0 +1,40 @@
+#ifndef INPOUTTYPES_HPP
+#define INPOUTTYPES_HPP
+
+#include <iostream>
+#include <string>
+
+struct DelimiterIO
+{
+  char exp;
+};
+
+struct LabelIO
+{
+  std::string exp;
+};
+
+struct DblIO
+{
+  double& ref;
+};
+
+struct StringIO
+{
+  std::string& ref;
+};
+
+struct SllIO
+{
+  long long& ref;
+};
+
+std::istream& operator>>(std::istream& in, DelimiterIO&& ref);
+std::ostream& operator<<(std::ostream& out, const DelimiterIO& data);
+std::istream& operator>>(std::istream& in, DblIO&& ref);
+std::istream& operator>>(std::istream& in, StringIO&& ref);
+std::istream& operator>>(std::istream& in, SllIO&& ref);
+std::istream& operator>>(std::istream& in, LabelIO&& ref);
+
+#endif
+
