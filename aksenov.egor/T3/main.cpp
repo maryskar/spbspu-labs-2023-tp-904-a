5d48a88818ab844a872dd7b7d1bd17ca629a0319
aksenovEA <aksenov200477@gmail.com>
2023-09-14 19:59:07 +0300

    new idea for input

--- aksenov.egor/T3/main.cpp
+++ aksenov.egor/T3/main.cpp
@@ -6,10 +6,29 @@
 #include <limits>
 #include "polygon.h"
 #include "commandMap.h"
+bool getShapes(std::vector< aksenov::Polygon > &shapesArray, std::string fileName)
+{
+  std::ifstream in(fileName);
+  if (!in.is_open())
+  {
+    std::cerr << "There was error opening file.";
+    return false;
+  }
+  while (!in.eof())
+  {
+    std::copy(std::istream_iterator< aksenov::Polygon >(in), std::istream_iterator< aksenov::Polygon >(), std::back_inserter(shapesArray));
+    if (in.fail() && !in.eof())
+    {
+      in.clear();
+      in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  return true;
+}
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
+  /*if (argc != 2)
   {
     std::cerr << "Error" << "\n";
     return 1;
@@ -23,13 +42,22 @@ int main(int argc, char **argv)
       std::cerr << "Error" << "\n";
       return 1;
     }
+  }*/
+  if (argc != 2)
+  {
+    std::cerr << "Error" << "\n";
+    return 1;
   }
   std::vector< aksenov::Polygon > data;
+  if (!getShapes(data, argv[1]))
+  {
+    return 1;
+  }
   aksenov::Polygon polygon;
-  while (inF >> polygon)
+  /*while (inF >> polygon)
   {
     data.push_back(polygon);
-  }
+  }*/
   std::map< std::string, void (*)(std::istream&, const std::vector< aksenov::Polygon >&) > commands;
   aksenov::makeDict(commands);
   std::string command = "";
