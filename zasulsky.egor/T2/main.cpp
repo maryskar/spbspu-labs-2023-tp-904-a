4e7f252701a619c3619db4513621a00db99f0526
Засульский Егор <3756035@gmail.com>
2023-09-14 22:01:59 +0300

    adding libralies

--- zasulsky.egor/T2/main.cpp
+++ zasulsky.egor/T2/main.cpp
@@ -1,4 +1,11 @@
-
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include "DataStruct.hpp"
+#include "InpOutTypes.hpp"
+#include "comp.hpp"
 int main()
 {
 
