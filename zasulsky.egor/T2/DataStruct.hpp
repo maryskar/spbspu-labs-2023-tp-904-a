5aea52e1d8c166da99d16a7e1e047770d9d1cfd3
Засульский Егор <3756035@gmail.com>
2023-09-14 22:05:14 +0300

    new  struct

---
+++ zasulsky.egor/T2/DataStruct.hpp
@@ -0,0 +1,18 @@
+#ifndef data_t_H
+#define data_t_H
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+struct dataStruct
+{
+  double key1;
+  long long key2;
+  std::string key3;
+};
+
+std::istream& operator>>(std::istream& in, dataStruct& ref);
+std::ostream& operator<<(std::ostream& out, const dataStruct& ref);
+
+#endif
