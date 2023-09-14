d80feeb9a8c0d70436fa0a7861ccf8f34815df97
natavdeeva <scarletname@yandex.ru>
2023-09-14 21:15:23 +0300

    help functions for commands with calculating area

--- avdeeva.natalya/T3/helpFunctions.cpp
+++ avdeeva.natalya/T3/helpFunctions.cpp
@@ -1,1 +1,13 @@
 #include "helpFunctions.h"
+bool avdeeva::isOdd(const Polygon & pol)
+{
+  return (size(pol) % 2);
+}
+bool avdeeva::isEven(const Polygon & pol)
+{
+  return !(isOdd(pol));
+}
+bool avdeeva::isNumOfVerts(const Polygon & pol, size_t num)
+{
+  return size(pol) == num;
+}
--- avdeeva.natalya/T3/helpFunctions.h
+++ avdeeva.natalya/T3/helpFunctions.h
@@ -1,5 +1,10 @@
 #ifndef SPBSPU_LABS_2023_TP_904_A_HELPFUNCTIONS_H
 #define SPBSPU_LABS_2023_TP_904_A_HELPFUNCTIONS_H
-
-
+#include "polygon.h"
+namespace avdeeva
+{
+  bool isOdd(const Polygon & pol);
+  bool isEven(const Polygon & pol);
+  bool isNumOfVerts(const Polygon & pol, size_t num);
+}
 #endif
