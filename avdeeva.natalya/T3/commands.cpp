20d09e690505e37c5e09091c752c7f8f2e307c77
natavdeeva <scarletname@yandex.ru>
2023-09-14 21:21:37 +0300

    calculating areas function made

---
+++ avdeeva.natalya/T3/commands.cpp
@@ -0,0 +1,30 @@
+#include "commands.h"
+#include <algorithm>
+#include <numeric>
+#include "processFunctions.h"
+
+template< typename Pred >
+double calcArea(const std::deque< avdeeva::Polygon > & polygons, Pred p) {
+  std::deque< avdeeva::Polygon > filtPolygons;
+  std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(filtPolygons), p);
+  std::deque< double > values(filtPolygons.size());
+  std::transform(filtPolygons.begin(), filtPolygons.end(), values.begin(), avdeeva::getArea);
+  return std::accumulate(values.begin(), values.end(), 0.0);
+}
+double avdeeva::calcAreaOdd(const std::deque< Polygon > & polygons) {
+  return calcArea(polygons, isOdd);
+}
+
+double avdeeva::calcAreaEven(const std::deque< Polygon > & polygons) {
+  return calcArea(polygons, isEven);
+}
+
+double avdeeva::calcAreaMean(const std::deque< Polygon > & polygons) {
+  if (polygons.empty()) {
+    throw std::invalid_argument("Empty vector of polygons");
+  }
+  std::deque< double > values(polygons.size());
+  std::transform(polygons.begin(), polygons.end(), values.begin(), getArea);
+  double res = std::accumulate(values.begin(), values.end(), 0.0) / polygons.size();
+  return res;
+}
---
+++ avdeeva.natalya/T3/commands.h
@@ -0,0 +1,11 @@
+#ifndef SPBSPU_LABS_2023_TP_904_A_COMMANDS_H
+#define SPBSPU_LABS_2023_TP_904_A_COMMANDS_H
+#include <deque>
+#include "polygon.h"
+namespace avdeeva
+{
+  double calcAreaOdd(const std::deque< Polygon > & polygons);
+  double calcAreaEven(const std::deque< Polygon > & polygons);
+  double calcAreaMean(const std::deque< Polygon > & polygons);
+}
+#endif
