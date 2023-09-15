586f43ddbbc1e3b5e6d2f519ea3713219144b2da
natavdeeva <scarletname@yandex.ru>
2023-09-15 04:51:01 +0300

    count command out

--- avdeeva.natalya/T3/outCommand.cpp
+++ avdeeva.natalya/T3/outCommand.cpp
@@ -1,12 +1,38 @@
 #include "outCommand.h"
 #include <exception>
+#include <map>
+#include <string>
+#include <functional>
+#include "polygon.h"
 #include "commands.h"
+void avdeeva::countCommand(const std::deque< avdeeva::Polygon >& polygons, std::istream& in, std::ostream& out)
+{
+  std::string argument;
+  in >> argument;
+  std::map< std::string, std::function< size_t(const std::deque< avdeeva:: Polygon >) > > countCommands(
+    {
+      {"EVEN", counterEven},
+      {"ODD", counterOdd}
+    });
+  size_t count = 0;
+  try
+  {
+    count = countCommands.at(argument)(polygons);
+    out << count << '\n';
+  }
+  catch (const std::exception & e)
+  {
+    size_t verts = std::stoull(argument);
+    if (verts < 3)
+    {
+      throw std::logic_error("Polygons have more than 3 vertexes");
+    }
+    count = avdeeva::counterVertexes(polygons, verts);
+    out << count << '\n';
+  }
+}
 void avdeeva::rightshapesCommand(const std::deque< avdeeva::Polygon > & polygons, std::istream & in, std::ostream & out)
 {
-  if (!in)
-  {
-    throw std::invalid_argument("Incorrect parameter");
-  }
   size_t count = avdeeva::countRightShapes(polygons);
   out << count << '\n';
 }
@@ -14,10 +40,6 @@ void avdeeva::inframeCommand(const std::deque< avdeeva::Polygon > & polygons, st
 {
   avdeeva::Polygon input;
   in >> input;
-  if (!in)
-  {
-    throw std::invalid_argument("Incorrect parameter");
-  }
   bool isInFrame = avdeeva::isInFrame(polygons, input);
   if (isInFrame)
   {
