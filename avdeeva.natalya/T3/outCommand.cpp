1c6c41611b7242cd730e2833a9fcaf25ebabed7b
natavdeeva <scarletname@yandex.ru>
2023-09-15 04:21:37 +0300

    made inframe command out

--- avdeeva.natalya/T3/outCommand.cpp
+++ avdeeva.natalya/T3/outCommand.cpp
@@ -1,1 +1,20 @@
 #include "outCommand.h"
+#include "commands.h"
+void avdeeva::inframeCommand(const std::deque< avdeeva::Polygon > & polygons, std::istream & in, std::ostream & out)
+{
+  avdeeva::Polygon input;
+  in >> input;
+  if (!in)
+  {
+    throw std::invalid_argument("Incorrect parameter");
+  }
+  bool isInFrame = avdeeva::isInFrame(polygons, input);
+  if (isInFrame)
+  {
+    out << "<TRUE>" << "\n";
+  }
+  else
+  {
+    out << "<FALSE>" << "\n";
+  }
+}
