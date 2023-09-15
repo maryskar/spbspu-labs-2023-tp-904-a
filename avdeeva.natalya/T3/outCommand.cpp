441dbffef96b3c2c47358cad365d880d3dd2b4a2
natavdeeva <scarletname@yandex.ru>
2023-09-15 04:23:19 +0300

    rightshapes command out

--- avdeeva.natalya/T3/outCommand.cpp
+++ avdeeva.natalya/T3/outCommand.cpp
@@ -1,5 +1,14 @@
 #include "outCommand.h"
 #include "commands.h"
+void avdeeva::rightshapesCommand(const std::deque< avdeeva::Polygon >& polygons, std::istream& in, std::ostream& out)
+{
+  if (!in)
+  {
+    throw std::invalid_argument("Incorrect parameter");
+  }
+  size_t count = avdeeva::countRightShapes(polygons);
+  out << count << '\n';
+}
 void avdeeva::inframeCommand(const std::deque< avdeeva::Polygon > & polygons, std::istream & in, std::ostream & out)
 {
   avdeeva::Polygon input;
