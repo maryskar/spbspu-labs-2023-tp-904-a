c5391dc79c936c928e296806076174f7a1f87a79
Засульский Егор <3756035@gmail.com>
2023-09-16 17:24:10 +0300

    cg 14

--- zasulsky.egor/T2/main.cpp
+++ zasulsky.egor/T2/main.cpp
@@ -10,10 +10,9 @@ int main()
 {
   using itOut = std::ostream_iterator<dataStruct>;
   using itInp = std::istream_iterator<dataStruct>;
-  dataStruct data; 
   std::vector<dataStruct> vector;
- 
-  
+  dataStruct data;
+
   while (!std::cin.eof())
   {
     std::copy(itInp(std::cin), itInp(), std::back_inserter(vector));
