47c3c67bc4c6fcca1dfa85edf61e69c4fed3a4ab
ArsDi <abstractlevl@gmail.com>
2023-09-16 22:54:06 +0300

    destr s g realiz

--- dmitriev.arseny/T2/streamguard.cpp
+++ dmitriev.arseny/T2/streamguard.cpp
@@ -1,8 +1,15 @@
 #include "streamguard.h"
 
-dmitriev::StreamGuard::StreamGuard(std::basic_ios<char>& stream):
+dmitriev::StreamGuard::StreamGuard(std::basic_ios< char >& stream):
   m_stream(stream),
   m_fill(stream.fill()),
   m_precision(stream.precision()),
   m_flags(stream.flags())
 {}
+
+dmitriev::StreamGuard::~StreamGuard()
+{
+  m_stream.fill(m_fill);
+  m_stream.precision(m_precision);
+  m_stream.flags(m_flags);
+}
