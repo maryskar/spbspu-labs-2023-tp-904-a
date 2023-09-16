769487aae8cb31df1bfda56ad3049d26c1e7c3d4
Засульский Егор <3756035@gmail.com>
2023-09-16 21:56:23 +0300

    gg

--- zasulsky.egor/T2/main.cpp
+++ zasulsky.egor/T2/main.cpp
@@ -11,7 +11,7 @@ int main()
   using itOut = std::ostream_iterator<dataStruct>;
   using itInp = std::istream_iterator<dataStruct>;
   std::vector<dataStruct> vector;
-  dataStruct data
+  dataStruct data;
 
   while(!std::cin.eof())
   {
