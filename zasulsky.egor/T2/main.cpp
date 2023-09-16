37386ceac4dcb0ffacfc662f6ae5c82a205acbb5
Засульский Егор <3756035@gmail.com>
2023-09-16 17:14:31 +0300

    time out

--- zasulsky.egor/T2/main.cpp
+++ zasulsky.egor/T2/main.cpp
@@ -10,13 +10,13 @@ int main()
 {
   using itOut = std::ostream_iterator<dataStruct>;
   using itInp = std::istream_iterator<dataStruct>;
+  dataStruct data; 
   std::vector<dataStruct> vector;
-  dataStruct data;
-
+ 
+  
   while (!std::cin.eof())
   {
     std::copy(itInp(std::cin), itInp(), std::back_inserter(vector));
-
   }
   std::sort(vector.begin(), vector.end(), compare);
   std::copy(vector.begin(), vector.end(), itOut(std::cout));
