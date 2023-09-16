ddbf081dd49d64be4c659634fcd6dcf7c7b67364
Потапова <marina-potapova04@mail.ru>
2023-09-16 21:37:05 +0300

    add DataStruct

---
+++ potapova.marina/T2/dataStruct.h
@@ -0,0 +1,44 @@
+#include <string>
+
+namespace potapova
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  struct DelimiterIO
+  {
+    char sign;
+  };
+
+  struct UnsignedLongIntOctIO
+  {
+    unsigned long long& oct;
+  };
+
+  struct UnsignedLongIntBinIO
+  {
+    unsigned long long& bin;
+  };
+  
+  struct StringIO
+  {
+    std::string& str;
+  };
+
+  struct LabelIO
+  {
+    std::string value;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, UnsignedLongIntOctIO&& dest);
+  std::istream& operator>>(std::istream& in, UnsignedLongIntBinIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+}
