72d94a4210775f655a62af08a711be4a07afb397
Засульский Егор <3756035@gmail.com>
2023-09-16 21:29:04 +0300

    nooooo

--- zasulsky.egor/T2/main.cpp
+++ zasulsky.egor/T2/main.cpp
@@ -12,7 +12,6 @@ int main()
   using itInp = std::istream_iterator<dataStruct>;
   std::vector<dataStruct> vector;
   dataStruct data
-  
   while(!std::cin.eof())
   {
     std::copy(itInp(std::cin), itInp(), std::back_inserter(vector));
