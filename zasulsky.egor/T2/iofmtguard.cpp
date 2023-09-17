5a1028042920381e3bbc8ac8749aa86d5accbda5
Засульский Егор <3756035@gmail.com>
2023-09-17 23:48:10 +0300

    please no

--- zasulsky.egor/T2/iofmtguard.cpp
+++ zasulsky.egor/T2/iofmtguard.cpp
@@ -1,12 +1,12 @@
 #include "Iofmtguard.hpp"
-aksenov::iofmtguard::iofmtguard(std::basic_ios< char >& s) :
+iofmtguard::iofmtguard(std::basic_ios< char >& s) :
   s_(s),
   fill_(s.fill()),
   precision_(s.precision()),
   fmt_(s.flags())
 {}
 
-aksenov::iofmtguard::~iofmtguard()
+iofmtguard::~iofmtguard()
 {
   s_.fill(fill_);
   s_.precision(precision_);
