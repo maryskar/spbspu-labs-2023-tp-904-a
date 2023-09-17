38b0747cae4fe16281d33a87b1a0a7039cf7aa90
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-18 02:37:16 +0300

    implementation struct point & polygon & op in/out

--- mishanina.elena/T3/Polygon.h
+++ mishanina.elena/T3/Polygon.h
@@ -1,3 +1,21 @@
 #ifndef POLYGON_H
 #define POLYGON_H
+#include <vector>
+#include <istream>
+
+namespace mishanina
+{
+  struct Point
+  {
+    int x, y;
+  };
+  struct Polygon
+  {
+    std::vector<Point> points;
+  };
+  std::istream &operator>>(std::istream &in, Point &&data);
+  std::istream &operator>>(std::istream &in, Polygon &&data);
+  std::ostream &operator<<(std::ostream &out, const Point &data);
+  std::ostream &operator<<(std::ostream &out, const Polygon &data);
+}
 #endif
