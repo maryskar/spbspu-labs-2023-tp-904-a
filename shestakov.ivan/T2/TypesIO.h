b6b63095576aec400d51229967810b5db4723560
Ivan31122003 <Ivan31122003@mail.ru>
2023-09-17 15:31:06 +0300

    add new type for output

--- shestakov.ivan/T2/TypesIO.h
+++ shestakov.ivan/T2/TypesIO.h
@@ -20,7 +20,7 @@ namespace shestakov
   {
     char& ref;
   };
-  struct CmpLspIO
+  struct CmpLspI
   {
     std::complex< double >& ref;
   };
@@ -28,10 +28,15 @@ namespace shestakov
   {
     std::string& str;
   };
+  struct CmpLspO
+  {
+    std::complex< double > ref;
+  };
   std::istream& operator>>(std::istream& in, DelIO&& dest);
   std::istream& operator>>(std::istream& in, LabelIO&& dest);
   std::istream& operator>>(std::istream& in, CharLitIO&& dest);
-  std::istream& operator>>(std::istream& in, CmpLspIO&& dest);
+  std::istream& operator>>(std::istream& in, CmpLspI&& dest);
   std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::ostream& operator<<(std::ostream& out, const CmpLspO& dest);
 }
 #endif
