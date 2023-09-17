8ec5f97f7025c93fe7b9a3a886bc3f5cd35e1319
Slava <slavk.kazakov@gmail.com>
2023-09-17 12:48:16 +0300

    CG

--- kazakov.slava/T2/DataStructur.cpp
+++ kazakov.slava/T2/DataStructur.cpp
@@ -1,64 +1,65 @@
 #include"DataStructur.h"
 
-std::ostream &kazakov::operator<<(std::ostream& out, const kazakov::DataStructur& p)
+std::ostream& kazakov::operator<<(std::ostream& out, const kazakov::DataStructur& p)
 {
-    std::ostream::sentry sentry(out);
-    if (!sentry)
-    {
-        return out;
-    }
-    out << "(" << ":" << "key1" << " " << p.key1 << "ll" << ":";
-    out << "key2" << " " << "'" << p.key2 << "'" << ":";
-    out << "key3" << " " << "\"" << p.key3 << "\"" << ":" << ")";
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
     return out;
+  }
+  out << "(" << ":" << "key1" << " " << p.key1 << "ll" << ":";
+  out << "key2" << " " << "'" << p.key2 << "'" << ":";
+  out << "key3" << " " << "\"" << p.key3 << "\"" << ":" << ")";
+  return out;
 }
-std::istream &kazakov::operator>>(std::istream& in, kazakov::DataStructur& p)
+std::istream& kazakov::operator>>(std::istream& in, kazakov::DataStructur& p)
 {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
-    kazakov::DataStructur vrem;
-    in >> ExpectedSymbol{ '(' } >> ExpectedSymbol{ ':' };
-    std::string getin;
-    for (int i = 0; i < 3; i++)
-    {
-        in >> getin;
-        if (getin == "key1")
-        {
-            in >> vrem.key1;
-
-        }
-        if (getin == "key2")
-        {
-            in >> vrem.key2;
-        }
-        if (getin == "key3")
-        {
-            in >> vrem.key3;
-        }
-
-        in >> ExpectedSymbol{ ':' };
-    }
-    in >> ExpectedSymbol{ ')' };
-
-    if (in)
-    {
-        p = vrem;
-    }
-
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
     return in;
+  }
+  kazakov::DataStructur vrem;
+  in >> ExpectedSymbol{ '(' } >> ExpectedSymbol{ ':' };
+  std::string getin;
+  for (int i = 0; i < 3; i++)
+  {
+    in >> getin;
+    if (getin == "key1")
+    {
+      in >> vrem.key1;
+
+    }
+    if (getin == "key2")
+    {
+      in >> vrem.key2;
+    }
+    if (getin == "key3")
+    {
+      in >> vrem.key3;
+    }
+
+    in >> ExpectedSymbol{ ':' };
+  }
+  in >> ExpectedSymbol{ ')' };
+
+  if (in)
+  {
+    p = vrem;
+  }
+
+  return in;
 }
 bool kazakov::comparator(const kazakov::DataStructur& p1, const kazakov::DataStructur& p2)
 {
-    if (p1.key1 == p2.key1 and p1.key2 == p2.key2)
-    {
-        return p1.key3 < p2.key3;
-    }
-    if (p1.key1 == p2.key1)
-    {
-        return p1.key2 < p2.key2;
-    }
-    return p1.key1 < p2.key1;
+  if (p1.key1 == p2.key1 and p1.key2 == p2.key2)
+  {
+    return p1.key3 < p2.key3;
+  }
+  if (p1.key1 == p2.key1)
+  {
+    return p1.key2 < p2.key2;
+  }
+  return p1.key1 < p2.key1;
 }
+
--- kazakov.slava/T2/main.cpp
+++ kazakov.slava/T2/main.cpp
@@ -2,28 +2,31 @@
 #include <fstream>
 #include <algorithm>
 #include <iterator>
+#include <limits>
 #include"DataStructur.h"
 
 int main()
 {
-    std::vector<kazakov::DataStructur> v;
-    while (true)
+  std::vector< kazakov::DataStructur > v;
+  while (true)
+  {
+    std::copy(std::istream_iterator< kazakov::DataStructur >(std::cin),
+        std::istream_iterator< kazakov::DataStructur >(),
+        std::inserter(v, v.end())
+    );
+    if (std::cin.eof())
     {
-        std::copy(std::istream_iterator<kazakov::DataStructur>(std::cin),
-            std::istream_iterator<kazakov::DataStructur>(),
-            std::inserter(v, v.end()));
-        if (std::cin.eof())
-        {
-            break;
-        }
-        if (std::cin.fail())
-        {
-            std::cin.clear();
-            auto maxstream = std::numeric_limits<std::streamsize>::max();
-            std::cin.ignore(maxstream, ')');
-        }
+      break;
     }
-    std::sort(v.begin(), v.end(), kazakov::comparator);
-    std::copy(v.begin(), v.end(), std::ostream_iterator<kazakov::DataStructur>(std::cout, "\n"));
-    return 0;
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      auto maxstream = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(maxstream, ')');
+    }
+  }
+  std::sort(v.begin(), v.end(), kazakov::comparator);
+  std::copy(v.begin(), v.end(), std::ostream_iterator< kazakov::DataStructur >(std::cout, "\n"));
+  return 0;
 }
+
