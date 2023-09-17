933e610b1ecff150ea6f850f1e97a4c2583f0d68
Twink1eToes <aristarhov.ro@edu.spbstu.ru>
2023-09-17 20:08:01 +0300

    fix dataStruckt

--- aristarkhov.rostislav/T2/dataStruct.cpp
+++ aristarkhov.rostislav/T2/dataStruct.cpp
@@ -12,24 +12,22 @@ std::istream& aristarkhov::operator>>(std::istream& in, DataStruct& data)
   {
     return in;
   }
-  DataStruct input;
   in >> DelimiterIO{'('} >> DelimiterIO{':'};
   for (int i = 0; i < 3; i++)
   {
     std::string key{};
-    size_t num = 0;
-    in >> LabelIO{"key"} >> num;
-    if (key == "key1")
+    in >> LabelIO{"key"} >> key;
+    if (key == "1")
     {
-      in >> CharIO{input.key1};
+      in >> CharIO{data.key1};
     }
-    else if (key == "key2")
+    else if (key == "2")
     {
-      in >> RationalNumberIO{input.key2};
+      in >> RationalNumberIO{data.key2};
     }
-    else if (key == "key3")
+    else if (key == "3")
     {
-      in >> StringIO{input.key3};
+      in >> StringIO{data.key3};
     }
     else
     {
@@ -50,21 +48,24 @@ std::ostream& aristarkhov::operator<<(std::ostream& out, const DataStruct& data)
     return out;
   }
   StreamGuard streamGuard(out);
-  out << "(:key1 " << std::setprecision(1) << data.key1;
-  out << ":key2 \'" << std::setprecision(0) << "\'";
-  out << ":key3 \"" << std::quoted(data.key3) << "\":)";
+  out << "(:key1 \'" << data.key1 << '\'';
+  out << ":key2 (:N " << data.key2.first << ":D " << data.key2.second;
+  out << ":key3 \"" << data.key3 << "\":)";
   return out;
 }
 
 bool aristarkhov::compare(const DataStruct& first, const DataStruct& second)
 {
-  if (first.key1 == second.key1)
+  if (lhs.key1 != rhs.key1)
   {
-    if (first.key2 == second.key2)
-    {
-      return (first.key3 < second.key3);
-    }
-    return (first.key2 < second.key2);
+    return lhs.key1 < rhs.key1;
   }
-  return (first.key1 < second.key1);
+  else if (lhs.key2 != rhs.key2)
+  {
+    return lhs.key2 < rhs.key2;
+  }
+  else
+  {
+    return lhs.key3.length() < rhs.key3.length();
+  }
 }
