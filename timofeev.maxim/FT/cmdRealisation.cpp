c9eda3b9a730314e6ab1603418d0c74d9d93c84b
timofeev <maks.timofeev9834@gmail.com>
2023-09-17 17:17:06 +0300

    realisation for functions

--- timofeev.maxim/FT/cmdRealisation.cpp
+++ timofeev.maxim/FT/cmdRealisation.cpp
@@ -1,38 +1,53 @@
 #include "cmdRealisation.h"
+#include <vector>
+#include <map>
+#include <unordered_set>
 #include "HelpFunction.h"
 namespace timofeev
 {
+  void Createdict(std::istream& in, std::ostream&, dictsOfDicts& dicts)
+  {
+    std::string Name;
+    while (true)
+    {
+      std::cout << "Write \"name\" or \"stop\" to out: ";
+      in >> Name;
+      if (Name == "stop")
+      {
+        break;
+      }
+      std::map <std::string, std::vector<std::string>> translateDict;
+      dicts[Name] = translateDict;
+    }
+  }
   void printHelp(std::istream& in, std::ostream& out, dictsOfDicts &)
   {
-    out << "Select which command you need:" << "\n";
-    out << "Write down \"All\" to get full info:" << "\n";
-    out << "Write down \"List\" to get list of command:" << "\n";
+    out << "Select which command you need: " << "\n";
+    out << "Write down \"All\" to get full info: " << "\n";
+    out << "Write down \"List\" to get list of command: " << "\n";
     std::string tmp = "";
     in >> tmp;
     if (tmp == "All")
     {
-      out << "AddWord Rus - let you add word in Russian with English translation (rus + eng)\n";
-      out << "AddWord Eng - let you add word in English with Russian translation (eng + rus)\n";
+      out << "Create \"name\" - let you make new dictionary and use it\n";
+      out << "AddWord \"name\" - let you add word and translation chosen dictionary \n";
       out << "Exist - a command that will help you find out if a word is in a any dictionary ";
       out << "and in which one it is\n";
       out << "Delete All - delete all dictionaries\n";
-      out << "Delete Eng - delete English dictionary\n";
-      out << "Delete Rus - delete Russian dictionary\n";
-      out << "DeleteT Rus - delete English translation in Russian dictionary\n";
-      out << "DeleteT Eng - delete Russian translation in English dictionary\n";
+      out << "Delete \"name\" - delete chosen dictionary\n";
+      out << "DeleteT \"name\" - delete translation in chosen dictionary\n";
       out << "Print All - print all dictionaries\n";
-      out << "Print Eng - print English dictionary\n";
-      out << "Print Rus - print Russian dictionary\n";
-      out << "Translate Eng - show you translation of English word in Russian\n";
-      out << "Translate Rus - show you translation of Russian word in English\n";
+      out << "Print \"name\" - print chosen dictionary\n";
+      out << "DictList - show you list of dict names\n";
+      out << "Translate \"name\" - show you translation of word in chosen dictionary\n";
       out << "Common - show you common words in dictionaries\n";
     }
     else if (tmp == "List")
     {
-      out << "[AddWord Rus, " << "AddWord Eng, " << "Exist, " << "Delete All, " << "\n";
-      out << "Delete Eng, " << "Delete Rus, " << "DeleteT Rus, " << "DeleteT Eng, " << "\n";
-      out << "Print All, " << "Print Eng, " << "Print Rus, " << "\n";
-      out << "Translate Rus, " << "Translate Eng, " << "Common]" << "\n";
+      out << "[AddWord \"name\", "  << "Exist, " << "Delete All, " << "\n";
+      out << "Delete \"name\", " << "DeleteT \"name\", " << "\n";
+      out << "Print All, " << "Print \"name\", " << "\n";
+      out << "Translate \"name\", " << "Common, " << "DictList]" << "\n";
     }
     else
     {
@@ -44,17 +59,12 @@ namespace timofeev
   {
     std::string secPart = "";
     in >> secPart;
-    if (secPart == "Rus")
+    try
     {
-      dict& rusDict = dicts["Ru"];
-      readDict(in, out, rusDict);
+      dict& lilDict = dicts[secPart];
+      addInDict(in, out, lilDict);
     }
-    else if (secPart == "Eng")
-    {
-      dict& engDict = dicts["Eng"];
-      readDict(in, out, engDict);
-    }
-    else
+    catch (...)
     {
       throw std::logic_error("Wrong Command");
     }
@@ -64,17 +74,12 @@ namespace timofeev
   {
     std::string secPart = "";
     in >> secPart;
-    if (secPart == "Rus")
+    try
     {
-      dict& rusDict = dicts["Ru"];
-      delTrans(in, out, rusDict);
+      dict& dictionary = dicts[secPart];
+      delTrans(in, out, dictionary);
     }
-    else if (secPart == "Eng")
-    {
-      dict& engDict = dicts["Eng"];
-      delTrans(in, out, engDict);
-    }
-    else
+    catch(...)
     {
       throw std::logic_error("Wrong Command");
     }
@@ -84,52 +89,47 @@ namespace timofeev
   {
     std::string secPart = "";
     in >> secPart;
-    dict& rusDict = dicts["Ru"];
-    dict& engDict = dicts["Eng"];
-    if (secPart == "Rus")
+    try
     {
-      printDict(out, rusDict);
+      if (secPart == "All")
+      {
+        for (auto p: dicts)
+        {
+          printDict(out, p.second);
+        }
+      }
+      else
+      {
+        dict& dictionary = dicts[secPart];
+        printDict(out, dictionary);
+      }
     }
-    else if (secPart == "Eng")
-    {
-      printDict(out, engDict);
-    }
-    else if (secPart == "All")
-    {
-      out << "Russian dictionary:" << "\n";
-      printDict(out, rusDict);
-      out << "English dictionary:" << "\n";
-      printDict(out, engDict);
-    }
-    else
+    catch (...)
     {
       throw std::logic_error("Wrong Command");
     }
   }
 
-  void Delete(std::istream& in, std::ostream& out, dictsOfDicts& dicts)
+  void Delete(std::istream& in, std::ostream&, dictsOfDicts& dicts)
   {
     std::string secPart = "";
     in >> secPart;
-    dict& rusDict = dicts["Ru"];
-    dict& engDict = dicts["Eng"];
-    if (secPart == "Rus")
+    try
     {
-      delDict(rusDict);
-      out << "Done!" << "\n";
+      if (secPart == "All")
+      {
+        for (auto p: dicts)
+        {
+          delDict(p.second);
+        }
+      }
+      else
+      {
+        dict& dictionary = dicts[secPart];
+        delDict(dictionary);;
+      }
     }
-    else if (secPart == "Eng")
-    {
-      delDict(engDict);
-      out << "Done!" << "\n";
-    }
-    else if (secPart == "All")
-    {
-      delDict(rusDict);
-      delDict(engDict);
-      out << "Done!" << "\n";
-    }
-    else
+    catch (...)
     {
       throw std::logic_error("Wrong Command");
     }
@@ -140,68 +140,77 @@ namespace timofeev
     std::string secPart = "";
     std::string word = "";
     in >> secPart;
-    if (secPart == "Rus")
+    try
     {
       in >> word;
-      dict& rusDict = dicts["Ru"];
-      findTrans(out, word, rusDict);
+      dict& dictionary = dicts[secPart];
+      findTrans(out, word, dictionary);
     }
-    else if (secPart == "Eng")
+    catch (...)
     {
-      in >> word;
-      dict& engDict = dicts["Eng"];
-      findTrans(out, word, engDict);
+      throw std::logic_error("Wrong Command");
     }
   }
   void Exist(std::istream& in, std::ostream& out, dictsOfDicts& dicts)
   {
     std::string word;
     in >> word;
-    dict& rusDict = dicts["Ru"];
-    dict& engDict = dicts["Eng"];
-    if (findWord(word, rusDict) && findWord(word, engDict))
+    bool flag = false;
+    try
     {
-      out << word << " In both Dicts";
+      if (dicts.empty())
+      {
+        out << "No dictionaries" << "\n";
+        return;
+      }
+      for (auto p: dicts)
+      {
+        if (findWord(word, p.second))
+        {
+          out << word << " in " << p.first;
+          flag = true;
+        }
+      }
+      if (!flag)
+      {
+        out << "No matches" << "\n";
+      }
     }
-    else if (findWord(word, engDict))
+    catch (...)
     {
-      out << word << " In English Dict";
-    }
-    else if (findWord(word, rusDict))
-    {
-      out << word << " In Russian Dict";
-    }
-    else
-    {
-      out << "No matches";
+      throw std::logic_error("Wrong Command");
     }
   }
 
+  void printDictList(std::istream&, std::ostream& out, dictsOfDicts& dicts)
+  {
+    out << "Names of Lists: " << "\n";
+    for (const auto& entry : dicts) {
+      const std::string& listName = entry.first;
+      out << listName << "\n";
+    }
+  }
   void Common(std::istream&, std::ostream& out, dictsOfDicts& dicts)
   {
-    dict& rusDict = dicts["Ru"];
-    dict& engDict = dicts["Eng"];
-    size_t count = 0;
-    for (const auto &i: rusDict)
+    std::unordered_set< std::string > unique;
+    for (const auto& names: dicts)
     {
-      for (const auto &y: engDict)
+      const std::map< std::string, std::vector< std::string > >& dict = names.second;
+      for (const auto& word: dict)
       {
-        for (const std::string &word: i.second)
+        const std::vector< std::string >& wordList = word.second;
+        for (const std::string& translation: wordList)
         {
-          for (const std::string &tmp: y.second)
+          if (unique.count(translation) > 0)
           {
-            if (i.first == tmp && y.first == word)
-            {
-              out << i.first << " " << y.first << "\n";
-              count++;
-            }
+            out << word.first << translation << "\n";
+          }
+          else
+          {
+            unique.insert(translation);
           }
         }
       }
-    }
-    if (count == 0)
-    {
-      out << "No matching";
     }
   }
 }
--- timofeev.maxim/FT/cmdRealisation.h
+++ timofeev.maxim/FT/cmdRealisation.h
@@ -4,11 +4,13 @@
 #include "Commands.h"
 namespace timofeev
 {
+  void printDictList(std::istream&, std::ostream& out, dictsOfDicts& dicts);
+  void Createdict(std::istream& in, std::ostream&, dictsOfDicts& dicts);
   void printHelp(std::istream& in, std::ostream& out, dictsOfDicts&);
   void AddWord(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
   void DeleteT(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
   void Print(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
-  void Delete(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
+  void Delete(std::istream& in, std::ostream&, dictsOfDicts& dicts);
   void Translate(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
   void Exist(std::istream& in, std::ostream& out, dictsOfDicts& dicts);
   void Common(std::istream&, std::ostream& out, dictsOfDicts& dicts);
