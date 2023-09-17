70616dc2eae6cd3327c6d66055ae3d724041c786
Потапова <marina-potapova04@mail.ru>
2023-09-17 17:26:27 +0300

    add input and output in main

--- potapova.marina/T2/main.cpp
+++ potapova.marina/T2/main.cpp
@@ -1,5 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "dataStruct.h"
+
+using namespace potapova;
+
 int main()
 {
+  std::vector< DataStruct > data;
+  std::copy(
+    std::istream_iterator< DataStruct >(std::cin),
+    std::istream_iterator< DataStruct >(),
+    std::back_inserter(data)
+  );
 
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
   return 0;
 }
