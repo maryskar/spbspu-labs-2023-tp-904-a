4ed3b5ec807cf49300a535132170b949588c9ed2
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-18 03:32:30 +0300

    create func get area

--- mishanina.elena/T3/commands.cpp
+++ mishanina.elena/T3/commands.cpp
@@ -1,5 +1,28 @@
 #include "commands.h"
+#include <iomanip>
+#include "extra_commands.h"
 
 namespace mishanina{
+//Расчёт суммы площади фигур с чётным количеством вершин
+  void printAreaEven(const std::vector<Polygon>& polygons, std::ostream& out){
+//    mishanina::iofmtguard iofmtguard(out);
+    out << std::fixed << std::setprecision(1) << mishanina::getAreaIf(polygons, isEven) << '\n';
+  }
+//Расчёт суммы площади фигур с нечётным количеством вершин
+  void printAreaOdd(const std::vector<Polygon>& polygons, std::ostream& out){
+    out << std::fixed << std::setprecision(1) << mishanina::getAreaIf(polygons, isOdd) << '\n';
+  }
+//Расчёт среднего площадей фигур
+  void printAreaMean(const std::vector<Polygon>& polygons, std::ostream& out){
+    if (polygons.empty()) {
+      throw std::invalid_argument("ERROR : polygons are empty");
+    }
+    std::vector<double> areas(polygons.size());
+    std::transform(polygons.begin(), polygons.end(), areas.begin(), mishanina::getArea);
 
+    double sum = std::accumulate(areas.begin(), areas.end(), 0.0);
+    double mean = sum / polygons.size();
+//    mishanina::iofmtguard iofmtguard(out);
+    out << std::fixed << std::setprecision(1) << mean << '\n';
+  }
 }
--- mishanina.elena/T3/commands.h
+++ mishanina.elena/T3/commands.h
@@ -1,7 +1,11 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include "Polygon.h"
 
 namespace mishanina{
+  void printAreaEven(const std::vector<Polygon>& polygons, std::ostream& out);
+  void printAreaOdd(const std::vector<Polygon>& polygons, std::ostream& out);
+  void printAreaMean(const std::vector<Polygon>& polygons, std::ostream& out);
 
 }
 #endif
