b3745a6d4a957521297b1904b84e52738168f88a
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-18 03:14:10 +0300

    main create vector & sstream

--- mishanina.elena/T3/main.cpp
+++ mishanina.elena/T3/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
-
+#include <sstream>
+#include <limits>
+#include "Polygon.h"
 int main()
 {
-  std::cout << "hello world\n";
+  using namespace mishanina;
+  std::stringstream input{
+    "3 (0;0) (1;1) (1;0)\n"
+    "2 (0;0) (1;1)\n"
+    "\n"
+    "4 (0;0) (1;0) (1;1) (0;1)\n"
+  };
+  std::vector<Polygon> pol;
+  while (!input.eof())
+  {
+    std::copy(std::istream_iterator<Polygon>(input),
+              std::istream_iterator<Polygon>(),
+              std::back_inserter(pol));
+    if (input.fail())
+    {
+      input.clear();
+      input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+  for (Polygon &pl: pol)
+  {
+    std::cout << "point\n";
+    for (auto &point: pl.points)
+    {
+      std::cout << "x = " << point.x << "\ty = " << point.y << '\n';
+    }
+  }
   return 0;
 }
