4e6603230914c789699bf265d98bbe97dac4fc87
Vagina Daria <dvagina@inbox.ru>
2023-09-18 17:03:40 +0300

    fixed commands.h

--- vagina.daria/T3/commands.h
+++ vagina.daria/T3/commands.h
@@ -5,23 +5,6 @@
 
 namespace vagina
 {
-  void areaEven(const std::vector< Polygon >& dest, std::ostream& out);
-  void areaOdd(const std::vector< Polygon >& dest, std::ostream& out);
-  void areaMean(const std::vector< Polygon >& dest, std::ostream& out);
-  void areaVertexes(const std::vector< Polygon >& dest, std::ostream& out, std::size_t param);
-  void maxArea(const std::vector < Polygon >& dest, std::ostream& out);
-  void maxVertexes(const std::vector < Polygon >& dest, std::ostream& out);
-  void minArea(const std::vector < Polygon >& dest, std::ostream& out);
-  void minVertexes(const std::vector < Polygon >& dest, std::ostream& out);
-  void countEven(const std::vector < Polygon >& dest, std::ostream& out);
-  void countOdd(const std::vector < Polygon >& dest, std::ostream& out);
-  void countVertexes(const std::vector < Polygon >& dest, std::ostream& out, std::size_t param);
-  void rects(const std::vector < Polygon >& dest, std::ostream& out);
-  void perms(const std::vector < Polygon >& dest, std::ostream& out, std::istream& in);
-  bool isEven(const Polygon& pol);
-  bool isOdd(const Polygon& pol);
-  bool isCountOfVertexes(const Polygon& pol, std::size_t param);
-  bool isPerm(const Polygon& lhs, const Polygon& rhs);
   void messageInvalidCommand(std::ostream& out);
   class DictionaryOfCommands
   {
