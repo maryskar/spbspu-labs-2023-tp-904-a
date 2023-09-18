d2ab0d2708016629e06e09f44434d435a991ae1f
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-18 12:53:17 +0300

    &

--- skarlygina.mariia/T2/struct_io.h
+++ skarlygina.mariia/T2/struct_io.h
@@ -26,7 +26,7 @@ namespace skarlygina
 
   struct DBLsciO
   {
-    const double ref;
+    const double& ref;
   };
 
   struct ULLhexI
@@ -36,7 +36,7 @@ namespace skarlygina
 
   struct ULLhexO
   {
-    const unsigned long long ref;
+    const unsigned long long& ref;
   };
 
   std::istream& operator>>(std::istream& in, DelimiterSep&& dest);
