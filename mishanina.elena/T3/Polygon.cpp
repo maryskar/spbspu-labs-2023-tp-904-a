2c9e3487bcf5ea020dadcd89bedb0e0a251325d3
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-18 03:14:27 +0300

    polygon fix

--- mishanina.elena/T3/Polygon.cpp
+++ mishanina.elena/T3/Polygon.cpp
@@ -1,1 +1,38 @@
 #include "Polygon.h"
+namespace mishanina{
+
+//  std::istream &operator>>(std::istream &in, Point &point)
+//  {
+//    std::istream::sentry sentry(in);
+//    if (!sentry) {
+//      return in;
+//    }
+//    return in >> DelimiterIO{ '(' } >> point.x >> DelimiterIO{ ';' } >> point.y >> DelimiterIO{ ')' };
+//  }
+
+//  std::istream &operator>>(std::istream &in, Polygon &data){
+//    std::istream::sentry sentry(in);
+//    if (!sentry) {
+//      return in;
+//    }
+//    std::size_t count = 0;
+//    in >> count;
+//    if (count < 3) {
+//      in.setstate(std::ios::failbit);
+//    }
+//    Polygon pol;
+//    std::copy_n(std::istream_iterator<Point>(in), count, std::back_inserter(pol.points));
+//    if (in) {
+//      pol = data;
+//    }
+//    return in;
+//  }
+
+  std::ostream &operator<<(std::ostream &out, const Point &data){
+
+  }
+  std::ostream &operator<<(std::ostream &out, const Polygon &data){
+
+  }
+
+}
--- mishanina.elena/T3/Polygon.h
+++ mishanina.elena/T3/Polygon.h
@@ -2,7 +2,9 @@
 #define POLYGON_H
 #include <vector>
 #include <istream>
-
+#include <iterator>
+#include <algorithm>
+#include "iodel.h"
 namespace mishanina
 {
   struct Point
@@ -13,9 +15,39 @@ namespace mishanina
   {
     std::vector<Point> points;
   };
-  std::istream &operator>>(std::istream &in, Point &&data);
-  std::istream &operator>>(std::istream &in, Polygon &&data);
+  std::istream &operator>>(std::istream &in, Point &point);
+  std::istream &operator>>(std::istream &in, Polygon &data);
   std::ostream &operator<<(std::ostream &out, const Point &data);
   std::ostream &operator<<(std::ostream &out, const Polygon &data);
 }
+std::istream &mishanina::operator>>(std::istream &in, Point &point)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  return in >> DelimiterIO{ '(' } >> point.x >> DelimiterIO{ ';' } >> point.y >> DelimiterIO{ ')' };
+}
+
+std::istream &mishanina::operator>>(std::istream &in, Polygon &data){
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  std::size_t count = 0;
+  in >> count;
+  if (count < 3) {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  Polygon polygon;
+  polygon.points.resize(count);
+  std::copy_n(std::istream_iterator<Point>(in), count, std::begin(polygon.points));
+  if (in) {
+    data = polygon;
+  }
+  return in;
+}
+
+
 #endif
