14ba7ecfabd862d939d15f394a479171e4b5d4fe
Ivan31122003 <Ivan31122003@mail.ru>
2023-09-19 16:43:47 +0300

    fix cg

--- shestakov.ivan/T2/get_abs_complex.cpp
+++ shestakov.ivan/T2/get_abs_complex.cpp
@@ -1,6 +1,7 @@
 #include "get_abs_complex.h"
 
-namespace shestakov{
+namespace shestakov
+{
   double getAbsComplex(std::complex< double > complex_number)
   {
     return std::sqrt(pow(complex_number.real(), 2.0) + pow(complex_number.imag(), 2.0));
