8171f95c8a9054cb88dafe46c57a0decc4355447
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-20 13:53:30 +0300

    fix include

--- mishanina.elena/T3/commands.cpp
+++ mishanina.elena/T3/commands.cpp
@@ -1,14 +1,17 @@
 #include "commands.h"
 #include <iomanip>
 #include "extra_commands.h"
+#include <iofmtguard.h>
 namespace mishanina
 {
   void printAreaEven(const std::vector<Polygon> &polygons, std::ostream &out)
   {
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getAreaIf(polygons, isEven) << '\n';
   }
   void printAreaOdd(const std::vector<Polygon> &polygons, std::ostream &out)
   {
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getAreaIf(polygons, isOdd) << '\n';
   }
   void printAreaMean(const std::vector<Polygon> &polygons, std::ostream &out)
@@ -17,6 +20,7 @@ namespace mishanina
     {
       throw std::invalid_argument("ERROR : polygons are empty");
     }
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getTotalArea(polygons) / polygons.size() << '\n';
   }
   void printAreaNumOfVertexes(const std::vector<Polygon> &polygons, std::ostream &out, std::size_t num)
@@ -26,6 +30,7 @@ namespace mishanina
       throw std::invalid_argument("ERROR : invalid parameter");
     }
     std::vector<Polygon> polygonsNum = getPolygonsWithNumOfVertexes(polygons, num);
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getTotalArea(polygonsNum) << '\n';
   }
   void printMaxArea(const std::vector<Polygon> &polygons, std::ostream &out)
@@ -34,6 +39,7 @@ namespace mishanina
     {
       throw std::invalid_argument("ERROR : polygons are empty");
     }
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getMaxMinArea(polygons, true) << '\n';
   }
   void printMaxVertexes(const std::vector<Polygon> &polygons, std::ostream &out)
@@ -42,6 +48,7 @@ namespace mishanina
     {
       throw std::invalid_argument("ERROR : polygons are empty");
     }
+    iofmtguard iofmtguard(out);
     out << getMaxMinVert(polygons, true) << '\n';
   }
   void printMinArea(const std::vector<Polygon> &polygons, std::ostream &out)
@@ -50,6 +57,7 @@ namespace mishanina
     {
       throw std::invalid_argument("ERROR : polygons are empty");
     }
+    iofmtguard iofmtguard(out);
     out << std::fixed << std::setprecision(1) << getMaxMinArea(polygons, false) << '\n';
   }
   void printMinVertexes(const std::vector<Polygon> &polygons, std::ostream &out)
@@ -58,14 +66,17 @@ namespace mishanina
     {
       throw std::invalid_argument("ERROR : polygons are empty");
     }
+    iofmtguard iofmtguard(out);
     out << getMaxMinVert(polygons, false) << '\n';
   }
   void printCountEven(const std::vector<Polygon> &polygons, std::ostream &out)
   {
+    iofmtguard iofmtguard(out);
     out << getCountIf(polygons, isEven) << '\n';
   }
   void printCountOdd(const std::vector<Polygon> &polygons, std::ostream &out)
   {
+    iofmtguard iofmtguard(out);
     out << getCountIf(polygons, isOdd) << '\n';
   }
   void printCountNumOfVertexes(const std::vector<Polygon> &polygons, std::ostream &out, std::size_t num)
@@ -75,10 +86,12 @@ namespace mishanina
       throw std::invalid_argument("ERROR : invalid parameter");
     }
     std::vector<Polygon> polNum = getPolygonsWithNumOfVertexes(polygons, num);
+    iofmtguard iofmtguard(out);
     out << polNum.size() << '\n';
   }
   void printRects(const std::vector<Polygon>& polygons, std::ostream& out)
   {
+    iofmtguard iofmtguard(out);
     out << getCountIf(polygons, isRectangle) << '\n';
   }
   void printRmecho(std::vector<Polygon>& polygons, std::ostream& out, std::istream& in)
@@ -93,8 +106,7 @@ namespace mishanina
     auto end = std::unique(polygons.begin(), polygons.end(), pred);
     std::size_t res = std::distance(end, polygons.end());
     polygons.erase(end, polygons.end());
-//    iofmtguard iofmtguard(out);
-    out << std::fixed << std::setprecision(1);
-    out << res << "\n";
+    iofmtguard iofmtguard(out);
+    out << std::fixed << std::setprecision(1) << res << "\n";
   }
 }
