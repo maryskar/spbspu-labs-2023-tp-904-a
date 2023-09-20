6cba98d2d87e64ba09b4952e9e1d41f39df636dd
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-20 13:11:31 +0300

    Polygon & Point & op

--- mishanina.elena/T3/Polygon.cpp
+++ mishanina.elena/T3/Polygon.cpp
@@ -1,38 +1,42 @@
 #include "Polygon.h"
-namespace mishanina{
-
-//  std::istream &operator>>(std::istream &in, Point &point)
-//  {
-//    std::istream::sentry sentry(in);
-//    if (!sentry) {
-//      return in;
-//    }
-//    return in >> DelimiterIO{ '(' } >> point.x >> DelimiterIO{ ';' } >> point.y >> DelimiterIO{ ')' };
-//  }
-
-//  std::istream &operator>>(std::istream &in, Polygon &data){
-//    std::istream::sentry sentry(in);
-//    if (!sentry) {
-//      return in;
-//    }
-//    std::size_t count = 0;
-//    in >> count;
-//    if (count < 3) {
-//      in.setstate(std::ios::failbit);
-//    }
-//    Polygon pol;
-//    std::copy_n(std::istream_iterator<Point>(in), count, std::back_inserter(pol.points));
-//    if (in) {
-//      pol = data;
-//    }
-//    return in;
-//  }
-
-  std::ostream &operator<<(std::ostream &out, const Point &data){
-
+std::istream &mishanina::operator>>(std::istream &in, Point &point)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
   }
-  std::ostream &operator<<(std::ostream &out, const Polygon &data){
-
+  Point input{0, 0};
+  in >> DelimiterIO{'('} >> input.x >> DelimiterIO{';'} >> input.y >> DelimiterIO{')'};
+  if (in)
+  {
+    point = input;
   }
-
+  return in;
+}
+std::istream &mishanina::operator>>(std::istream &in, Polygon &data)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  std::size_t count = 0;
+  in >> count;
+  if (count < 3)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  Polygon pol;
+  pol.points.reserve(count);
+  std::copy_n(std::istream_iterator<Point>(in), count, std::back_inserter(pol.points));
+  if (in)
+  {
+    data.points.swap(pol.points);
+  }
+  return in;
+}
+bool mishanina::operator==(const Point &left, const Point &right)
+{
+  return (left.x == right.x && left.y == right.y);
 }
