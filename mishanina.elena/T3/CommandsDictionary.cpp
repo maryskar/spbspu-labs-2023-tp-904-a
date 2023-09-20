ca70ca7bc3d58cc51351acb85d2f633db11d46b9
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2023-09-20 13:23:45 +0300

    dict_out

--- mishanina.elena/T3/CommandsDictionary.cpp
+++ mishanina.elena/T3/CommandsDictionary.cpp
@@ -1,1 +1,24 @@
 #include "CommandsDictionary.h"
+mishanina::CommandDictionary::CommandDictionary()
+{
+  dict_out.insert({"AREA EVEN", printAreaEven});
+  dict_out.insert({"AREA ODD", printAreaOdd});
+  dict_out.insert({"AREA MEAN", printAreaMean});
+}
+void mishanina::CommandDictionary::doCommandOut(std::string &cmd, const vect_pol &pols, std::ostream &out) const
+{
+  auto func = dict_out.at(cmd);
+  func(pols, out);
+}
+void mishanina::CommandDictionary::doCommand(vect_pol &pols, std::string &cmd, const CommandDictionary &cmds,
+                                             std::istream &in,
+                                             std::ostream &out)
+{
+  try
+  {
+    doCommandOut(cmd, pols, out);
+    return;
+  } catch (const std::out_of_range &e)
+  {
+  }
+}
--- mishanina.elena/T3/CommandsDictionary.h
+++ mishanina.elena/T3/CommandsDictionary.h
@@ -2,28 +2,22 @@
 #define COMMANDS_DICTIONARY_H
 #include <map>
 #include "commands.h"
-namespace mishanina{
+namespace mishanina
+{
   using vect_pol = std::vector<Polygon>;
-  using commPolygon = void(*)(const vect_pol&, std::ostream&);
-  class CommandDictionary {
+  using cmd_out = void (*)(const vect_pol &, std::ostream &);
+  class CommandDictionary
+  {
   public:
     CommandDictionary();
-//    ~CommandDictionary() = default;
-//    std::string inputCommand(std::istream& in);
-    void doCommand(
-      vect_pol& pols,
-      std::string& cmd,
-      const CommandDictionary& cmds,
-      std::istream& in,
-      std::ostream& out
-    );
+    void doCommand(vect_pol &pols,
+                   std::string &cmd,
+                   const CommandDictionary &cmds,
+                   std::istream &in,
+                   std::ostream &out);
   private:
-    std::map< std::string, commPolygon > dict_pol;
-//    std::map< std::string, commVertex > dict_ver;
-//    std::map< std::string, commRmecho > dict_rm;
-    void doCommandPol(std::string& cmd, const vect_pol& pols, std::ostream& out) const;
-//    void doCommandVer(std::string& cmd, const vect_pol& pols, std::ostream& out, size_t num) const;
-//    void doCommandRm(std::string& cmd, vect_pol& pols, std::ostream& out, std::istream& in) const;
+    std::map<std::string, cmd_out> dict_out;
+    void doCommandOut(std::string &cmd, const vect_pol &pols, std::ostream &out) const;
   };
 }
 #endif
