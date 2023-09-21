0d54aa1fad55980dd4ebc7d40e4bd2b39b189f2c
Ivan31122003 <Ivan31122003@mail.ru>
2023-09-21 13:15:52 +0300

    fix build

--- shestakov.ivan/T3/main.cpp
+++ shestakov.ivan/T3/main.cpp
@@ -13,20 +13,12 @@
 int main(int argc, char ** argv)
 {
   using namespace shestakov;
-  /*
-   * if (argc != 2)
+  if (argc != 2)
   {
     std::cerr << "Not enough CL parameters\n";
     return 1;
   }
   std::ifstream input(argv[1]);
-   */
-  std::stringstream input(
-  "3 (3;3) (1;1) (1;3)\n"
-  "4 (0;0) (1;0) (1;1) (0;1)\n"
-  "4 (0;0) (1;0) (1;1) (0;1)\n"
-  "3 (3;3) (1;1) (1;3)\n"
-    );
   std::vector< Polygon > polygons;
   auto maxstream = std::numeric_limits< std::streamsize >::max();
   while (!input.eof())
