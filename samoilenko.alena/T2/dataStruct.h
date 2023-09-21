ac099060beecfe1284bafe40eb047d6f0e1283d5
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-09-21 17:59:44 +0300

    files dataStruct added, struct dataStruct added

---
+++ samoilenko.alena/T2/dataStruct.h
@@ -0,0 +1,15 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+#include <complex>
+
+namespace samoilenko
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+}
+
+#endif
