78290e5fbcaea73c22531d0bba49003a2d455433
Ivan31122003 <Ivan31122003@mail.ru>
2023-09-21 20:42:08 +0300

    fix

--- shestakov.ivan/T3/main.cpp
+++ shestakov.ivan/T3/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char ** argv)
     {
       if (polygons.size() == 0)
       {
-        throw;
+        throw std::logic_error("");
       }
       doCommand(polygons, std::cin, std::cout, cmd);
     }
