470100de534c338aacb9e8a97f79c8d99360ccfb
ArsDi <abstractlevl@gmail.com>
2023-09-22 16:48:11 +0300

    3 first printers init

--- dmitriev.arseny/T3/commands.h
+++ dmitriev.arseny/T3/commands.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef COMMANDS
+#define COMMANDS
+
+#include "polygon.h"
+
+namespace dmitriev
+{
+  std::ostream& printEvenArea(std::ostream& out, std::vector< Polygon > data);
+  std::ostream& printOddArea(std::ostream& out, std::vector< Polygon > data);
+  std::ostream& printNSizeArea(std::ostream& out, std::vector< Polygon > data, size_t number);
+
+   
+}
+
+#endif
