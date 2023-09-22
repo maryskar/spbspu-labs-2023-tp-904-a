7016701e533a20f7da288269ce7a5febb3ffade2
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-09-22 15:46:53 +0300

    added checking for status of stream and clear and ignore if it has errors

--- samoilenko.alena/T2/main.cpp
+++ samoilenko.alena/T2/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include <deque>
+#include <limits>
 #include "dataStruct.h"
 
 int main()
 {
   std::deque< samoilenko::DataStruct > data;
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n')
+    }
+  }
 }
