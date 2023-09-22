59f7b9cc2b61dc721cde0923a7e45dc0970caece
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-09-22 15:23:16 +0300

    added private fields in class iofmtguard and constructor is ready

--- samoilenko.alena/T2/scopeguard.cpp
+++ samoilenko.alena/T2/scopeguard.cpp
@@ -1,3 +1,8 @@
 #include "scopeguard.h"
 
-samoilenko::iofmtguard::iofmtguard()
+samoilenko::iofmtguard::iofmtguard(std::basic_ios< char >& s):
+  s_ (s),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  fmt_(s.flags())
+{}                                                              
--- samoilenko.alena/T2/scopeguard.h
+++ samoilenko.alena/T2/scopeguard.h
@@ -8,6 +8,11 @@ namespace samoilenko
   public:
     explicit iofmtguard(std::basic_ios< char >& s);
     ~iofmtguard();
+  private:
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags fmt_;
   };
 }
 
