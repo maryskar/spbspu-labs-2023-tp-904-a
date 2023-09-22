98e2c58ed724fd9d22eb1106ff061830407ab2d1
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-09-22 15:49:50 +0300

    added pseudonym for istream iterator

--- samoilenko.alena/T2/main.cpp
+++ samoilenko.alena/T2/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <deque>
 #include <limits>
+#include <iterator>
 #include "dataStruct.h"
 
 int main()
 {
+  using iter_of_inp = std::istream_iterator< samoilenko::DataStruct >;
   std::deque< samoilenko::DataStruct > data;
   while (!std::cin.eof())
   {
     if (std::cin.fail())
     {
       std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n')
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
 }
