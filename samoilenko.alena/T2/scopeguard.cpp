3c8d7cd9fb792734e4dcd4f197aec4f90a38442d
alenasamojlenko1 <alenasamojlenko1@gmail.com>
2023-09-22 15:26:08 +0300

    destrustor in iofmtguard is ready

--- samoilenko.alena/T2/scopeguard.cpp
+++ samoilenko.alena/T2/scopeguard.cpp
@@ -5,4 +5,11 @@ samoilenko::iofmtguard::iofmtguard(std::basic_ios< char >& s):
   fill_(s.fill()),
   precision_(s.precision()),
   fmt_(s.flags())
-{}                                                              
+{}
+
+samoilenko::iofmtguard::~iofmtguard()
+{
+  s_.fill(fill_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+}
