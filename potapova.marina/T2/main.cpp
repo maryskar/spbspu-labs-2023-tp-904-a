2d73db36226002e618fc1a0997471a39910f6a15
Потапова <marina-potapova04@mail.ru>
2023-09-25 21:56:35 +0300

    [r] Change functions on methods in std::copy in output

--- potapova.marina/T2/main.cpp
+++ potapova.marina/T2/main.cpp
@@ -13,12 +13,12 @@ int main()
   std::vector< DataStruct > data;
   while (!std::cin.eof())
   {
-    std::copy(
+     std::copy(
       std::istream_iterator< DataStruct >(std::cin),
       std::istream_iterator< DataStruct >(),
       std::back_inserter(data)
     );
-    if (std::cin.fail())
+    if (!std::cin)
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
@@ -28,8 +28,8 @@ int main()
   std::sort(data.begin(), data.end(), Comparator());
 
   std::copy(
-    std::begin(data),
-    std::end(data),
+    data.cbegin(),
+    data.cend(),
     std::ostream_iterator< DataStruct >(std::cout, "\n")
   );
   return 0;
