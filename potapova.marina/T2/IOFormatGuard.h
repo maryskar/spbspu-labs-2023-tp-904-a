9d1a9dc0c6e74964974847daf408295df790a538
Потапова <marina-potapova04@mail.ru>
2023-09-25 22:08:55 +0300

    [f] Remove initialisation of constructor and destructor in IOFormatGuard.h

--- potapova.marina/T2/IOFormatGuard.h
+++ potapova.marina/T2/IOFormatGuard.h
@@ -6,19 +6,8 @@ namespace potapova
   class IOFormatGuard
   {
     public:
-      IOFormatGuard(std::basic_ios< char >& stream):
-        stream_(stream),
-        fill_character_(stream.fill()),
-        format_flags_(stream.flags())
-      {
-
-      }
-
-      ~IOFormatGuard()
-      {
-        stream_.fill(fill_character_);
-        stream_.flags(format_flags_);
-      }
+      IOFormatGuard(std::basic_ios< char >& stream);
+      ~IOFormatGuard();
     private:
       std::basic_ios< char >& stream_;
       char fill_character_;
