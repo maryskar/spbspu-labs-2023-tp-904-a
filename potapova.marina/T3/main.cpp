08123f018d4b8e7dc21375dc143ce131b1f14437
Потапова <marina-potapova04@mail.ru>
2023-10-03 00:25:21 +0300

    [+] Add input of file in main

--- potapova.marina/T3/main.cpp
+++ potapova.marina/T3/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include "dataStruct.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+  using namespace potapova;
+  std::ifstream input_file;
+  if (argc != 2)
+  {
+    std::cerr << "Incorrect number of arguments\n";
+    return 1;
+  }
+  else if (argc == 2)
+  {
+    input_file.open(argv[1]);
+    if (!input_file.is_open())
+    {
+      std::cerr << "Failed to open file\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cerr << "No file to open\n";
+    return 1;
+  }
+
   return 0;
 }
