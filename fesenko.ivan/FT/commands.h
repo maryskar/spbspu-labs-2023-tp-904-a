5c5b86cb07536af004d88b7ce49a5d4b9d78a36b
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-10-05 00:45:40 +0300

    Defined class Commands and declare commands

--- fesenko.ivan/FT/commands.h
+++ fesenko.ivan/FT/commands.h
@@ -12,9 +12,9 @@ namespace fesenko
     using hash_t = std::unordered_map< std::string, std::forward_list< size_t > >;
     using data_t = std::unordered_map< std::string, hash_t >;
     Commands();
-    void make(const std::string &command, const std::string dict_name, std::istream &in, std::ostream &out);
+    void make(const std::string &command, data_t &data, std::istream &in, std::ostream &out);
    private:
-    using commands1 = std::function< void(const data_t &, std::istream &) >;
+    using commands1 = std::function< void(data_t &, std::istream &) >;
     using commands2 = std::function< std::ostream &(const data_t &, std::istream &, std::ostream &) >;
     std::unordered_map< std::string, commands1 > type1;
     std::unordered_map< std::string, commands2 > type2;
