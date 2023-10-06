39808b6a869d30e9978f0dc8051a3efff2dea238
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-10-06 13:31:29 +0300

    Complete read_file command

---
+++ fesenko.ivan/FT/subCommands.cpp
@@ -0,0 +1,41 @@
+#include "subCommands.h"
+#include <string>
+#include <cctype>
+#include <fstream>
+#include <stdexcept>
+
+void fesenko::read_file_cmd(data_t &data, std::istream &in)
+{
+  std::string dict_name = "";
+  std::string file_name = "";
+  in >> dict_name >> file_name;
+  if (!in) {
+    throw std::invalid_argument("Wrong input");
+  }
+  std::ifstream fin(file_name);
+  if (!fin.is_open()) {
+    throw std::invalid_argument("Can`t open the file");
+  }
+  size_t counter = 0;
+  std::string line = "";
+  while (std::getline(fin, line)) {
+    counter++;
+    std::string word = "";
+    for (size_t i = 0; i < line.size(); i++) {
+      char c = line[i];
+      if (isalnum(c)) {
+        if (isupper(c)) {
+          c = tolower(c);
+        }
+        word += c;
+      } else if (!word.empty()){
+        data.at(dict_name).insert(std::make_pair(word, counter));
+        word = "";
+      }
+    }
+    if (!word.empty()){
+      data.at(dict_name).insert(std::make_pair(word, counter));
+      word = "";
+    }
+  }
+}
