9afbaa2a2772cb681c7ff89a482e42b267b28c00
Мария <skarlygina04@yandex.ru>
2023-10-07 22:12:35 +0300

    find word func

--- skarlygina.mariia/FT/commands.h
+++ skarlygina.mariia/FT/commands.h
@@ -11,9 +11,9 @@ namespace skarlygina
 
 
 	void analyze(std::string&, Dicts_t&);
-	void printDict(std::string&, Dicts_t&);
-	void writelnFile(std::string&, Dicts_t&);
-	void printWord(std::string&, Dicts_t&);
+	void printDict(std::string&, const Dicts_t&);
+	void writelnFile(std::string&, const Dicts_t&);
+	void printWord(std::string&, const Dicts_t&);
 	void deleteWord(std::string&, Dicts_t&);
 	void deleteDict(std::string&, Dicts_t&);
 	void merge(std::string&, Dicts_t&);
