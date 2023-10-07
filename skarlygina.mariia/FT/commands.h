a26c567db83c3d54c9d5bec29843c05ce45443b4
Мария <skarlygina04@yandex.ru>
2023-10-07 21:59:42 +0300

    new types`

--- skarlygina.mariia/FT/commands.h
+++ skarlygina.mariia/FT/commands.h
@@ -1,8 +1,15 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 #include <string>
+#include <list>
+#include <map>
 namespace skarlygina
 {
+	using List_t = std::list< size_t >;
+	using Dict_t = std::map<std::string, List_t>;
+	using Dicts_t = std::map<std::string, Dict_t>;
+
+
 	void analyze(std::string&, Dicts_t&);
 	void printDict(std::string&, Dicts_t&);
 	void writelnFile(std::string&, Dicts_t&);
