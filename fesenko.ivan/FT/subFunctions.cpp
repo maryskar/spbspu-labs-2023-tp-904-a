2eac6573a798581f3458b3254d363bc9fb54d960
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-10-08 17:10:34 +0300

    Add command to parse string to words

--- fesenko.ivan/FT/subFunctions.cpp
+++ fesenko.ivan/FT/subFunctions.cpp
@@ -1,4 +1,28 @@
 #include "subFunctions.h"
+#include <cctype>
+
+std::forward_list< std::string > fesenko::parse_line(std::string line)
+{
+  std::forward_list< std::string > list;
+  if (line.empty()) {
+    return list;
+  }
+  std::string word = "";
+  for (size_t i = 0; i < line.size(); i++) {
+    char c = line[i];
+    if (std::isalnum(c)) {
+      word += std::tolower(c);;
+    } else if (!word.empty()){
+      list.push_front(word);
+      word = "";
+    }
+  }
+  if (!word.empty()){
+    list.push_front(word);
+    word = "";
+  }
+  return list;
+}
 
 void fesenko::print_word(const hash_t &hash, std::string word, std::ostream &out)
 {
--- fesenko.ivan/FT/subFunctions.h
+++ fesenko.ivan/FT/subFunctions.h
@@ -6,6 +6,7 @@
 namespace fesenko
 {
   using hash_t = std::unordered_map< std::string, std::forward_list< size_t > >;
+  std::forward_list< std::string > parse_line(std::string line);
   void print_word(const hash_t &hash, std::string word, std::ostream &out);
 }
 #endif
