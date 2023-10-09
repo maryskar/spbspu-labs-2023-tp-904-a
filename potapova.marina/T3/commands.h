ba445efee1f846935848cfff4b8df95f29ff8235
Потапова <marina-potapova04@mail.ru>
2023-10-09 21:31:38 +0300

    [+] Add all the commands

--- potapova.marina/T3/commands.h
+++ potapova.marina/T3/commands.h
@@ -4,10 +4,74 @@
 #include <deque>
 #include "dataStruct.h"
 
+#include <deque>
+
 namespace potapova
 {
-  double calculatePolygonArea(const Polygon& polygon);
-  double calculateAverageArea(const std::deque< Polygon >& polygons);
+  void getSumAreasPolygonsOdd(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getSumAreasPolygonsEven(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getAverageArea(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getSumOfAreasWithPointCounts(const std::deque< Polygon >& polygons,
+      std::istream& in,
+      std::ostream& out,
+      std::ostream&);
+
+  void getMaxArea(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getMaxPoints(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getMinArea(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void getMaxPoints(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void countPolygonsWithOddPointsNum(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void countPolygonsWithEvenPointsNum(const std::deque< Polygon >& polygons,
+      std::istream&,
+      std::ostream& out,
+      std::ostream&);
+
+  void countPolygonsWithPointsNum(const std::deque< Polygon >& polygons,
+      std::istream& in,
+      std::ostream& out,
+      std::ostream&);
+
+  void removePolygonDuplicates(std::deque< Polygon >& polygons,
+      std::istream& in,
+      std::ostream& out,
+      std::ostream&);
+
+  void checkPolygonInFrame(std::deque< Polygon >& polygons,
+      std::istream& in,
+      std::ostream& out,
+      std::ostream&);
 }
 
 #endif
