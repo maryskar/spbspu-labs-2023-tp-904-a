4453b32df9cd47b5beea153ebcf4292eace6bb64
Потапова <marina-potapova04@mail.ru>
2023-10-13 22:15:28 +0300

    [f] Fix unordered_map, remove global variables

--- potapova.marina/T3/commandsMap.cpp
+++ potapova.marina/T3/commandsMap.cpp
@@ -1,29 +1,38 @@
 #include "commandsMap.h"
 #include <functional>
+#include <unordered_map>
+#include <string>
 #include "commands.h"
 
 namespace potapova
 {
-  std::unordered_map< std::string, commandFunc >
-  non_changing_commands =
-  {
-    {"AREA EVEN", getSumAreasPolygonsEven},
-    {"AREA ODD", getSumAreasPolygonsOdd},
-    {"AREA MEAN", getAverageArea},
-    {"AREA NUM_OF_VERTEXES", getSumOfAreasWithPointCounts},
-    {"MAX AREA", getMaxArea},
-    {"MAX VERTEXES", getMaxPoints},
-    {"MIN AREA", getMinArea},
-    {"MIN VERTEXES", getMaxPoints},
-    {"COUNT ODD", countPolygonsWithOddPointsNum},
-    {"COUNT EVEN", countPolygonsWithEvenPointsNum},
-    {"COUNT NUM_OF_VERTEXES", countPolygonsWithPointsNum}
-  };
+  template < typename Polygons >
+  using CommandFunc = std::function< void(Polygons&, std::istream&, std::ostream&, std::ostream&) >;
 
-  std::unordered_map< std::string, commandFunc >
-  changing_commands =
+  std::unordered_map< std::string, CommandFunc< const std::deque< Polygon > > > getNonChangingCommands()
   {
-    {"RMECHO", removePolygonDuplicates},
-    {"INFRAME", checkPolygonInFrame}
-  };
+    return std::unordered_map< std::string, CommandFunc< const std::deque< Polygon > > >
+    {
+      {"AREA EVEN", getSumAreasPolygonsEven},
+      {"AREA ODD", getSumAreasPolygonsOdd},
+      {"AREA MEAN", getAverageArea},
+      {"AREA NUM_OF_VERTEXES", getSumOfAreasWithPointCounts},
+      {"MAX AREA", getMaxArea},
+      {"MAX VERTEXES", getMaxPoints},
+      {"MIN AREA", getMinArea},
+      {"MIN VERTEXES", getMaxPoints},
+      {"COUNT ODD", countPolygonsWithOddPointsNum},
+      {"COUNT EVEN", countPolygonsWithEvenPointsNum},
+      {"COUNT NUM_OF_VERTEXES", countPolygonsWithPointsNum},
+      {"INFRAME", checkPolygonInFrame}
+    };
+  }
+
+  std::unordered_map< std::string, CommandFunc< std::deque< Polygon > > > getChangingCommands()
+  {
+    return std::unordered_map< std::string, CommandFunc< std::deque< Polygon > > >
+    {
+      {"RMECHO", removePolygonDuplicates}
+    };
+  }
 }
--- potapova.marina/T3/commandsMap.h
+++ potapova.marina/T3/commandsMap.h
@@ -1,18 +1,6 @@
 #ifndef COMMANDSMAP_H
 #define COMMANDSMAP_H
 
-#include <unordered_map>
-#include "dataStruct.h"
 
-namespace potapova
-{
-  using commandFunc = std::function< void(const std::deque< Polygon >&, std::istream&, std::ostream&, std::ostream&) >;
-
-  std::unordered_map< std::string, commandFunc >
-  extern non_changing_commands;
-
-  std::unordered_map< std::string, commandFunc >
-  extern changing_commands;
-}
 
 #endif
