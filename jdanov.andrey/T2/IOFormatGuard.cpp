18ff66eb708550d22527410fdf083a72529647a9
TyToh <tarasgoncharuk9@gmail.com>
2023-10-13 15:31:02 +0300

    Fix CG 3

--- jdanov.andrey/T2/IOFormatGuard.cpp
+++ jdanov.andrey/T2/IOFormatGuard.cpp
@@ -1,18 +1,15 @@
 #include "IOFormatGuard.h"
 
-namespace zhdanov
+IOFormatGuard::IOFormatGuard(std::basic_ios< char >& stream) :
+  referenceToStream_(stream),
+  fill_(stream.fill()),
+  precision_(stream.precision()),
+  formatFlags_(stream.flags())
 {
-  IOFormatGuard::IOFormatGuard(std::basic_ios< char >& stream) :
-  	referenceToStream_(stream),
-  	fill_(stream.fill()),
-  	precision_(stream.precision()),
-  	formatFlags_(stream.flags())
-  {
-  }
-  IOFormatGuard::~IOFormatGuard()
-  {
-  	referenceToStream_.fill(fill_);
-  	referenceToStream_.precision(precision_);
-  	referenceToStream_.flags(formatFlags_);
-  }
+}
+IOFormatGuard::~IOFormatGuard()
+{
+  referenceToStream_.fill(fill_);
+  referenceToStream_.precision(precision_);
+  referenceToStream_.flags(formatFlags_);
 }
