8f492cce34ff2023e03ca41c2e5a9156742fb8d1
Мария <skarlygina04@yandex.ru>
2023-10-14 23:29:52 +0300

    fixed tabs

--- skarlygina.mariia/FT/main.cpp
+++ skarlygina.mariia/FT/main.cpp
@@ -5,25 +5,25 @@
 
 int main()
 {
-	using command_t = std::function< void(std::string&, skarlygina::Dicts_t&) >;
+  using command_t = std::function< void(std::string&, skarlygina::Dicts_t&) >;
   using const_command_t = std::function< void(std::string&, const skarlygina::Dicts_t&) >;
 
   std::map< std::string, command_t > commands
   {
-    {"analyze", skarlygina::analyze},
-    {"deleteWord", skarlygina::deleteWord},
-    {"merge", skarlygina::merge},
-    {"rename", skarlygina::rename},
-    {"intersect", skarlygina::intersect},
-    {"deleteDict", skarlygina::deleteDict},
-    {"complement", skarlygina::complement},
+  {"analyze", skarlygina::analyze},
+  {"deleteWord", skarlygina::deleteWord},
+  {"merge", skarlygina::merge},
+  {"rename", skarlygina::rename},
+  {"intersect", skarlygina::intersect},
+  {"deleteDict", skarlygina::deleteDict},
+  {"complement", skarlygina::complement},
   };
 
   std::map< std::string, const_command_t > const_commands
   {
-    {"printWord", skarlygina::printWord},
-    {"printDict", skarlygina::printDict},
-    {"writeInFile", skarlygina::writeInFile},
+  {"printWord", skarlygina::printWord},
+  {"printDict", skarlygina::printDict},
+  {"writeInFile", skarlygina::writeInFile},
   };
 
   try
