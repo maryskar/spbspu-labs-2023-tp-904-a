78769bd756b23d01853854200c48b8fe86c90bf3
aksenovEA <aksenov200477@gmail.com>
2023-10-15 23:53:34 +0300

    new rightshapes 2

--- aksenov.egor/T3/commands.cpp
+++ aksenov.egor/T3/commands.cpp
@@ -84,41 +84,69 @@ namespace aksenov
     }
   }
 
-    void doSame(std::istream &inp, const std::vector< Polygon > &pol)
-    {
-      if (pol.size() == 0 || pol.size() == 1)
-      {
-        throw std::invalid_argument("empty pol");
-      }
-      Polygon polygon;
-      inp >> polygon;
-      auto sort = std::bind(isEqual, std::placeholders::_1, polygon.points.size());
-      std::vector< Polygon > sorted;
-      std::copy_if(pol.begin(),pol.end(),sorted.begin(), sort);
-      auto func = std::bind(isSame, std::placeholders::_1, polygon);
-      auto counter = std::count_if(sorted.begin(), sorted.end(), func);
-      std::cout << counter << "\n";
-    }
-
-  bool isRightShape(const aksenov::Polygon& polygon)
+  void doSame(std::istream &inp, const std::vector< Polygon > &pol)
   {
-    if (polygon.points.size() != 4)
+    if (pol.size() == 0 || pol.size() == 1)
+    {
+      throw std::invalid_argument("empty pol");
+    }
+    Polygon polygon;
+    inp >> polygon;
+    auto sort = std::bind(isEqual, std::placeholders::_1, polygon.points.size());
+    std::vector< Polygon > sorted;
+    std::copy_if(pol.begin(),pol.end(),sorted.begin(), sort);
+    auto func = std::bind(isSame, std::placeholders::_1, polygon);
+    auto counter = std::count_if(sorted.begin(), sorted.end(), func);
+    std::cout << counter << "\n";
+  }
+
+  double getDistance(const Point &p1, const Point &p2)
+  {
+    return std::sqrt(std::pow(p2.x - p1.x, 2) + std::pow(p2.y - p1.y, 2));
+  }
+
+  Polygon createAngle(int &i, const Polygon &polygon)
+  {
+    Polygon angle;
+    auto iter = polygon.points.begin() + i;
+    angle.points.insert(angle.points.cbegin(), iter, iter + 3);
+    i++;
+    return angle;
+  }
+
+  std::vector< Polygon > createVectorOfAngles(const Polygon &polygon)
+  {
+    Polygon tmp(polygon);
+    tmp.points.push_back(polygon.points.at(0));
+    tmp.points.push_back(polygon.points.at(1));
+    std::vector< Polygon > angles(polygon.points.size());
+    int i = 0;
+    auto bindFunction = std::bind(createAngle, i, tmp);
+    std::generate(angles.begin(), angles.end(), bindFunction);
+    return angles;
+  }
+
+  bool isRightAngle(const Polygon &angle)
+  {
+    if (angle.points.size() != 3)
     {
       return false;
     }
-    int dx1 = polygon.points[1].x - polygon.points[0].x;
-    int dy1 = polygon.points[1].y - polygon.points[0].y;
-    int dx2 = polygon.points[2].x - polygon.points[1].x;
-    int dy2 = polygon.points[2].y - polygon.points[1].y;
-    int dx3 = polygon.points[3].x - polygon.points[2].x;
-    int dy3 = polygon.points[3].y - polygon.points[2].y;
-    return (dx1 * dx2 + dy1 * dy2 == 0) && (dx2 * dx3 + dy2 * dy3 == 0);
+    double ab = getDistance(angle.points.at(0), angle.points.at(1));
+    double bc = getDistance(angle.points.at(1), angle.points.at(2));
+    double ca = getDistance(angle.points.at(2), angle.points.at(0));
+    return std::sqrt(ab * ab + bc * bc) == ca;
+  }
+
+  bool isThereRightAngle(const Polygon &polygon)
+  {
+    auto angles = createVectorOfAngles(polygon);
+    return std::any_of(angles.begin(), angles.end(), isRightAngle);
   }
 
   void doRightshapes(std::istream &, const std::vector< Polygon > &pol)
     {
-      std::vector< Polygon > rightShapes;
-      std::copy_if(pol.begin(), pol.end(), std::back_inserter(rightShapes), isRightShape);
-      std::cout << rightShapes.size();
+      size_t count = std::count_if(pol.begin(), pol.end(), isThereRightAngle);
+      std::cout << count;
     }
   }
