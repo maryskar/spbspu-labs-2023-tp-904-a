3c0546fdac5f6ac004095cb8b870071e276faa58
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-10-17 14:50:33 +0300

    remove get and putback

--- fesenko.ivan/T3/commands.cpp
+++ fesenko.ivan/T3/commands.cpp
@@ -1,14 +1,15 @@
 #include "commands.h"
+#include <string>
 #include <messages.h>
 #include "subCommands.h"
 
 fesenko::Commands::Commands():
   type1(),
-  type2()
+  type2(),
+  type3()
 {
   type1.insert(std::make_pair("AREA", &area_vertexes));
   type1.insert(std::make_pair("COUNT", &count_vertexes));
-  type1.insert(std::make_pair("INFRAME", &inframe));
 
   type2.insert(std::make_pair("AREA ODD", &area_odd));
   type2.insert(std::make_pair("AREA EVEN", &area_even));
@@ -20,28 +21,27 @@ fesenko::Commands::Commands():
   type2.insert(std::make_pair("COUNT ODD", &count_odd));
   type2.insert(std::make_pair("COUNT EVEN", &count_even));
   type2.insert(std::make_pair("RECTS", &rects));
+
+  type3.insert(std::make_pair("INFRAME", &inframe));
 }
 
 void fesenko::Commands::make(const std::string &command1, data_t &data, std::istream &in, std::ostream &out)
 {
   try {
-    char c1 = ' ';
-    in.get(c1);
-    if (c1 != ' ') {
+    if (type3.find(command1) != type3.end()) {
+      type3.at(command1)(data, in, out) << "\n";
+      return;
+    } else if (type2.find(command1) != type2.end()) {
       type2.at(command1)(data, out) << "\n";
       return;
-    } else {
-      char c2;
-      in.get(c2);
-      in.putback(c2);
-      if (std::isdigit(c2)) {
-        type1.at(command1)(data, in, out) << "\n";
-        return;
-      }
     }
     std::string command2 = "";
     in >> command2;
-    type2.at(command1 + " " + command2)(data, out) << "\n";
+    if (std::isdigit(command2[0])) {
+      type1.at(command1)(data, std::stoull(command2), out) << "\n";
+    } else {
+      type2.at(command1 + " " + command2)(data, out) << "\n";
+    }
   } catch (...) {
     outInvalidCommandMessage(out);
     out << "\n";
--- fesenko.ivan/T3/commands.h
+++ fesenko.ivan/T3/commands.h
@@ -14,10 +14,12 @@ namespace fesenko
     Commands();
     void make(const std::string &command1, data_t &data, std::istream &in, std::ostream &out);
    private:
-    using commands1 = std::function< std::ostream &(const data_t &, std::istream &, std::ostream &) >;
+    using commands1 = std::function< std::ostream &(const data_t &, size_t, std::ostream &) >;
     using commands2 = std::function< std::ostream &(const data_t &, std::ostream &) >;
+    using commands3 = std::function< std::ostream &(const data_t &, std::istream &, std::ostream &) >;
     std::map< std::string, commands1 > type1;
     std::map< std::string, commands2 > type2;
+    std::map< std::string, commands3 > type3;
   };
 }
 #endif
