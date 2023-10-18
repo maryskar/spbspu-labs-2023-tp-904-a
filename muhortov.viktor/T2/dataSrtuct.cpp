99a4fb7c27be7834ea23374c6c465596967e7b51
muhortov viktor <viktormuhortov26@gmail.com>
2023-10-18 20:09:45 +0300

    complete datastruct

--- muhortov.viktor/T2/dataSrtuct.cpp
+++ muhortov.viktor/T2/dataSrtuct.cpp
@@ -1,1 +1,13 @@
 #include "dataSrtuct.hpp"
+
+namespace muhortov {
+  bool comparator::operator()(const muhortov::DataStruct &lhs, const muhortov::DataStruct &rhs) const {
+    if (std::abs(lhs.key1) == std::abs(rhs.key1)) {
+      if (lhs.key2 == rhs.key2) {
+        return lhs.key3.length() < rhs.key3.length();
+      }
+      return lhs.key2 < rhs.key2;
+    }
+    return std::abs(lhs.key1) < std::abs(rhs.key1);
+  }
+}
--- muhortov.viktor/T2/dataSrtuct.hpp
+++ muhortov.viktor/T2/dataSrtuct.hpp
@@ -4,10 +4,14 @@
 #include <string>
 
 namespace muhortov {
-  struct DataStruct{
+  struct DataStruct {
     std::complex< double > key1;
     std::pair< long long, unsigned long long > key2;
     std::string key3;
+  };
+
+  struct comparator {
+    bool operator()(const DataStruct &lhs, const DataStruct &rhs) const;
   };
 }
 
