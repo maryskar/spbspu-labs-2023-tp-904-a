b667ad2e0ad272e955337566bbd6130f57e63593
muhortov viktor <viktormuhortov26@gmail.com>
2023-10-18 22:16:31 +0300

    complete main

--- muhortov.viktor/T2/main.cpp
+++ muhortov.viktor/T2/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include <deque>
+#include "dataSrtuct.h"
+#include "io.h"
 
-int main()
-{
+using namespace muhortov;
 
+int main() {
+  using iteratorInput = std::istream_iterator< dataStruct >;
+  auto iteratorOutput = iteratorInput();
+  const std::streamsize maxStreamSize = std::numeric_limits< std::streamsize >::max();
+  std::deque< dataStruct > data;
+  while (!std::cin.eof()) {
+    std::copy(iteratorInput(std::cin), iteratorOutput, std::back_inserter(data));
+    if (!std::cin) {
+      std::cin.clear();
+      std::cin.ignore(maxStreamSize, '\n');
+    }
+  }
+
+  std::sort(data.begin(), data.end(), comparator);
+  std::copy(data.begin(), data.end(). std::ostream_iterator< dataStruct>(std::cout, "\n"));
+  return 0;
 }
