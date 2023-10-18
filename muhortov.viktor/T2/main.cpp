deab17b3f854bc396d0a6f9f69a4c0e12574899f
muhortov viktor <viktormuhortov26@gmail.com>
2023-10-19 00:32:55 +0300

    delete test cout

--- muhortov.viktor/T2/main.cpp
+++ muhortov.viktor/T2/main.cpp
@@ -12,10 +12,8 @@ int main() {
   using iteratorInput = std::istream_iterator< dataStruct >;
   const std::streamsize maxStreamSize = std::numeric_limits< std::streamsize >::max();
   std::deque< dataStruct > data;
-  std::cout << "help\n";
   while (!std::cin.eof()) {
     std::copy(iteratorInput(std::cin), iteratorInput(), std::back_inserter(data));
-    std::cout << "help\n";
     if (!std::cin) {
       std::cin.clear();
       std::cin.ignore(maxStreamSize, '\n');
