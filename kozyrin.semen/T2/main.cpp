47cd3e62e0adb04138b4cc5e9c2fbeaede63a84e
Kozyrin Semen <garbagemilitia@gmail.com>
2023-10-20 11:33:10 +0300

    removing test stuff

--- kozyrin.semen/T2/main.cpp
+++ kozyrin.semen/T2/main.cpp
@@ -4,24 +4,21 @@
 #include <limits>
 #include <deque>
 #include "datastruct.h"
-#include <sstream>
 using namespace kozyrin;
 
 int main() {
   using ist_iter = std::istream_iterator< DataStruct >;
   using ost_iter = std::ostream_iterator< DataStruct >;
   std::deque< DataStruct > data;
-  std::stringstream yes("(:key2 10ull:key1 5.45e-2:key3 \"Data\":)");
-  //while (!std::cin.eof())
-  //{
+  while (!std::cin.eof())
+  {
     if (std::cin.fail())
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
-    std::copy(ist_iter(yes), ist_iter(), std::back_inserter(data));
-  //}
+    std::copy(ist_iter(std::cin), ist_iter(), std::back_inserter(data));
+  }
   std::sort(data.begin(), data.end(), comp);
   std::copy(std::begin(data), std::end(data), ost_iter(std::cout, "\n"));
-  std::cout << data.size();
 }
