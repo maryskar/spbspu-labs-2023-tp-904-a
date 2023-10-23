febb6c30945d48a507643dba4e4e7ce11eae0119
Twink1eToes <aristarhov.ro@edu.spbstu.ru>
2023-10-24 00:57:12 +0300

    add areaEven and Odd

--- aristarkhov.rostislav/T3/command.cpp
+++ aristarkhov.rostislav/T3/command.cpp
@@ -1,8 +1,8 @@
-#include "commands.h"
+#include "command.h"
 #include <cmath>
 #include <functional>
-#include <vector>
 #include <algorithm>
+#include <streamGuard.h>
 
 namespace aristarkhov
 {
@@ -28,5 +28,37 @@ namespace aristarkhov
     area = std::abs(area / 2);
 
     return area;
+  }
+
+  bool isEven(const Polygon& polygon)
+  {
+    return polygon.points.size() % 2 == 0;
+  }
+
+  bool isOdd(const Polygon& polygon)
+  {
+    return !isEven(polygon);
+  }
+  double sumArea(double cur, const Polygon& polygon)
+  {
+    return cur + getArea(polygon);
+  }
+
+  void areaEven(const std::vector< Polygon >& polygons, std::ostream& out)
+  {
+    std::vector< Polygon > even_polygons;
+    std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(even_polygons), isEven);
+    StreamGuard iofmtguard(out);
+    out << std::fixed << std::setprecision(1)
+      << std::accumulate(even_polygons.begin(), even_polygons.end(), 0.0, sumArea) << '\n';
+  }
+
+  void areaOdd(const std::vector< Polygon >& polygons, std::ostream& out)
+  {
+    std::vector< Polygon > odd_polygons;
+    std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(odd_polygons), isOdd);
+    StreamGuard iofmtguard(out);
+    out << std::fixed << std::setprecision(1);
+    out << std::accumulate(odd_polygons.begin(), odd_polygons.end(), 0.0, sumArea) << "\n";
   }
 }
--- aristarkhov.rostislav/T3/command.h
+++ aristarkhov.rostislav/T3/command.h
@@ -1,12 +1,16 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
+#ifndef COMMAND_H
+#define COMMAND_H
 
 #include "polygon.h"
+#include <vector>
 
 namespace aristarkhov
 {
   size_t getVerticesCount(const Polygon& polygon);
   double getArea(const Polygon& polygon);
+
+  void areaEven(const std::vector< Polygon >& polygons, std::ostream& ostream);
+  void areaOdd(const std::vector< Polygon >& polygons, std::ostream& ostream);
 
 }
 
