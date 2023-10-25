b09314a353a8b69043a2a84d26a463ef6f78e898
Мария <skarlygina04@yandex.ru>
2023-10-25 12:50:03 +0300

    fixed headers

--- skarlygina.mariia/T3/help_structures.h
+++ skarlygina.mariia/T3/help_structures.h
@@ -1,42 +1,13 @@
 #ifndef HELP_STRUCTURES_H
 #define HELP_STRUCTURES_H
 #include "polygon.h"
-#include "help_commands.h"
 namespace skarlygina
 {
   struct PolygonsArea
   {
-    PolygonsArea(Point point):
-      prev(point)
-    {}
-    double operator()(double area, const Point& current)
-    {
-      area += static_cast< double >(prev.x) * current.y;
-      area -= static_cast< double >(prev.y) * current.x;
-      prev = current;
-      return area;
-    }
+    PolygonsArea(Point point);
+    double operator()(double area, const Point& current);
   private:
     Point prev;
   };
-
-  template< class Condition >
-  struct AreaCondition
-  {
-    AreaCondition(Condition cond):
-      condition(cond)
-    {}
-
-    double operator()(double area, const Polygon& poly)
-    {
-      if (condition(poly))
-      {
-        return area + findAreaPoly(poly);
-      }
-      return area;
-    }
-  private:
-    Condition condition;
-  };
 }
-#endif
